bf380ce95896d7e67dc2e2e3f349b41a76389621
Linkarzu <linkarzu@gmail.com>
2024-02-01 12:21:28 -0600

    added oil.nvim for testing

---
+++ neovim/nvim-lazyvim/lua/plugins/oil-nvim.lua
@@ -0,0 +1,21 @@
+-- Use `-` to open oil, that's the default command, see the mapping below
+
+-- From outside neovim, you can SSH to hosts using
+-- -- nvim oil-ssh://docker1/
+-- This takes you to the root dir
+-- -- nvim oil-ssh://docker1//
+
+-- From inside neovim you can SSH to hosts using
+-- -- First type `:` to enter command mode, then enter
+-- -- e oil-ssh://docker1/
+
+-- https://github.com/stevearc/oil.nvim
+return {
+  "stevearc/oil.nvim",
+  opts = {},
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+  },
+}

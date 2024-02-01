e62f73f4a75419b8a8267058b086f8618ad080d5
Linkarzu <linkarzu@gmail.com>
2024-02-01 16:49:31 -0600

    set follow_current_file to false

--- neovim/nvim-lazyvim/lua/plugins/neotree.lua
+++ neovim/nvim-lazyvim/lua/plugins/neotree.lua
@@ -5,6 +5,12 @@ return {
       -- I swapped these 2
       { "<leader>e", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
       { "<leader>E", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
+    opts = {
+      filesystem = {
+        -- Had to disable this option, because when I open neotree it was
+        -- jumping around to random dirs when I opened a dir
+        follow_current_file = { enabled = false },
+      },
     },
   },
 }

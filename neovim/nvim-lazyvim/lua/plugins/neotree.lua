065313c42e8a640f06d47e09699cd53697b48034
Linkarzu <linkarzu@gmail.com>
2024-02-01 16:50:13 -0600

    added r mapping for Neotree reveal

--- neovim/nvim-lazyvim/lua/plugins/neotree.lua
+++ neovim/nvim-lazyvim/lua/plugins/neotree.lua
@@ -1,3 +1,5 @@
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
@@ -5,6 +7,15 @@ return {
       -- I swapped these 2
       { "<leader>e", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
       { "<leader>E", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
+      -- New mapping for spacebar+r to reveal in NeoTree
+      {
+        "<leader>r",
+        function()
+          vim.cmd("Neotree reveal")
+        end,
+        desc = "Reveal current file in NeoTree",
+      },
+    },
     opts = {
       filesystem = {
         -- Had to disable this option, because when I open neotree it was

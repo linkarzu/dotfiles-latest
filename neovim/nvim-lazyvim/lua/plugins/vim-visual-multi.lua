73a9bfa5310dfeebcdb90f8fea5677f24a827baf
Linkarzu <linkarzu@gmail.com>
2024-02-05 03:57:43 -0600

    added instructions to select up to a word

--- neovim/nvim-lazyvim/lua/plugins/vim-visual-multi.lua
+++ neovim/nvim-lazyvim/lua/plugins/vim-visual-multi.lua
@@ -2,7 +2,7 @@
 
 -- In macos disable Settings - Keyboard - Keyboard Shortcuts
 -- Disable both 'Mission Control' shortcuts that use ctrl+down and ctrl+up
--- As those are used to create multiple cursors
+-- As those are used by the plugin to create vertical cursors
 
 -- select words with Ctrl-N
 -- create cursors vertically with Ctrl-Down/Ctrl-Up
@@ -14,11 +14,25 @@
 -- start insert mode with i,a,I,
 
 -- INSTRUCTIONS:
+
+-- Help:
+-- :help visual-multi
+
+-- To select up to a word in multiple lines (similar to vfd)
+-- Place on the first word and do 'ctrl+down' to create vertical cursors
+-- Then change to extend mode with 'tab'
+-- Then type 'g/' and enter what you want to match, a word or symbol
+-- Press enter and your text will be selected
+
 -- Put yourself on the 1st letter of the word you want to edit, press ctrl+n
 -- keep pressing 'n' again to select the word on the below lines
 -- to skip the current word press 'q' and will continue to next
 -- Then press 'e' or 'w' to move to the right to select the entire word
 -- Hit C to change the word, type the new word, hit escape
+
+-- You can also first select a word in visual mode, then do ctrl+n and keep
+-- pressing 'n' to select the same words
+-- press 'q' to skip workds
 
 return {
   -- add gruvbox

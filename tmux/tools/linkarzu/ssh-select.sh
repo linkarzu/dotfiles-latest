10d3840214d8af8f60a4416d3274ffd8b00b14c5
Linkarzu <linkarzu@gmail.com>
2024-02-05 12:55:37 -0600

    added karabiner mapping to session name

--- tmux/tools/linkarzu/ssh-select.sh
+++ tmux/tools/linkarzu/ssh-select.sh
@@ -2,22 +2,33 @@
 
 # Filename: ~/github/dotfiles-latest/tmux/tools/linkarzu/ssh-select.sh
 
-# Define the path to your file containing SSH host aliases and their keys
-HOSTS_FILE="$HOME/github/dotfiles-latest/tmux/tools/linkarzu/ssh-hosts.txt"
+# NOTE:
+# Remember to make this file executable
 
-# Ensure the HOSTS_FILE exists
-if [ ! -f "$HOSTS_FILE" ]; then
-	echo "SSH hosts file not found at $HOSTS_FILE"
+# Define the path to your file containing SSH host aliases and their keys
+hosts_file="$HOME/github/dotfiles-latest/tmux/tools/linkarzu/ssh-hosts.sh"
+
+# Ensure the hosts_file exists
+if [ ! -f "$hosts_file" ]; then
+	tmux display-message -d 3000 "ssh hosts file not found"
+	sleep 3
 	exit 1
 fi
 
-FZF_HEADER=$'- The letter in () like (j) is just to remember the karabiner mapping
-- Hosts with () will have it removed before SSHing to them
-- You can also have hosts without the () and they will work fine'
+# Source the SSH hosts mappings
+# Sent to dev null in case there are hosts added without mappings
+# I.E 'docker2' instead of 'docker3="j"'
+source $hosts_file >/dev/null 2>&1
+
+fzf_header=$'- The letter after the '=' is just to remember my karabiner mappings
+- Hosts with '=' will have it removed before SSHing to them
+- You can also have hosts without the '=' and they will work fine'
 
 # Use fzf to select a host, showing both the host and its key
-# selected=$(cat "$HOSTS_FILE" | fzf --height=40% --reverse)
-selected=$(cat "$HOSTS_FILE" | grep -v '^#' | fzf --height=40% --reverse --header="$FZF_HEADER" --prompt="Type or select SSH host: ")
+selected=$(cat "$hosts_file" | grep -v '^#' | fzf --height=40% --reverse --header="$fzf_header" --prompt="Type or select SSH host: ")
+# Debugging
+# tmux display-message -d 3000 "selected: $selected"
+# sleep 3
 
 # Exit if no selection is made
 if [[ -z $selected ]]; then
@@ -25,14 +36,34 @@ if [[ -z $selected ]]; then
 	exit 0
 fi
 
-# Extract just the host name from the selection (before the '(')
-SSH_NAME=$(echo "$selected" | awk -F " [(]" '{print $1}')
+# Extract the variable name from the selection
+ssh_name=$(echo "$selected" | cut -d'=' -f1)
 
-# Add 'ssh' to beginning of name for easier identification
-SELECTED_NAME="ssh-$SSH_NAME"
+selected_after_tr=$(basename "$ssh_name" | tr '.-' '__')
 
-if ! tmux has-session -t=$SELECTED_NAME 2>/dev/null; then
-	tmux new-session -s "$SELECTED_NAME" -d "ssh $SSH_NAME"
+# NOTE: If you don't want to use the 'ssh-hosts.sh' file, just rename
+# that 'ssh-hosts.sh' file to something else (or delete it)
+# This file adds a letter at the end of my session to remind me of the karabiner shortcut
+mappings_file=$hosts_file
+if [ -f "$mappings_file" ]; then
+	# source "$mappings_file"
+	# Get the value of the variable whose name matches $base_selected
+	mapping_value="${!selected_after_tr}"
+	# If mapping value is not empty
+	if [ -n "$mapping_value" ]; then
+		selected_name="SSH-${selected_after_tr}-${mapping_value}"
+	else
+		# If the mapping value is empty
+		selected_name="SSH-${selected_after_tr}"
+	fi
+else
+	selected_name="SSH-${selected_after_tr}"
 fi
+
+# If a tmux session with the desired name does not already exist, create it in detached mode
+if ! tmux has-session -t=$selected_name 2>/dev/null; then
+	tmux new-session -s "$selected_name" -d "ssh $ssh_name"
+fi
+
 # Switch to the tmux session with the name derived from the selected directory
-tmux switch-client -t $SELECTED_NAME
+tmux switch-client -t $selected_name
--- tmux/tools/linkarzu/tmux-sshonizer-agen.sh
+++ tmux/tools/linkarzu/tmux-sshonizer-agen.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
-# https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/scripts/tmux-sessionizer
-# Filename: ~/github/dotfiles-latest/tmux/tools/tmux-sessionizer.sh
-# Remember to make this file executable
 # Filename: ~/github/dotfiles-latest/tmux/tools/linkarzu/tmux-sshonizer-agen.sh
+
+# https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/scripts/tmux-sessionizer
+
+# NOTE:
+# Remember to make this file executable
 
 # Check if one argument is being provided
 if [[ $# -eq 1 ]]; then
 	# Use the provided argument as the selected directory
-	SSH_NAME=$1
+	ssh_name=$1
 elif [[ $# -eq 0 ]]; then
 	# Explicitly specified 0 in case someone decides to pass more than 1 argument
 	# Call the ssh-select script which will open fzf to select a host
 	~/github/dotfiles-latest/tmux/tools/linkarzu/ssh-select.sh
 	# Debugging, uncomment below if you need to see what's being selected
-	# tmux display-message -d 10000 "Directory selected via fzf: $SSH_NAME"
+	# tmux display-message -d 10000 "Directory selected via fzf: $ssh_name"
 else
 	# This will hopefully catch your attention
 	tmux display-message -d 500 "This script expects zero or one argument."
@@ -24,13 +26,31 @@ else
 	tmux display-message -d 5000 "This script expects zero or one argument."
 fi
 
-# Add 'ssh' to beginning of name for easier identification
-# SELECTED_NAME=$SSH_NAME
-SELECTED_NAME="ssh-$SSH_NAME"
-# Debugging below
-# tmux display-message -d 3000 "SSH_NAME: $SSH_NAME"
-# sleep 3
-# tmux display-message -d 3000 "SELECTED_NAME: $SELECTED_NAME"
+# # Add 'ssh' to beginning of name for easier identification
+# # selected_name=$ssh_name
+# selected_name="SSH-$ssh_name"
+# # Debugging below
+# # tmux display-message -d 3000 "ssh_name: $ssh_name"
+# # sleep 3
+# # tmux display-message -d 3000 "selected_name: $selected_name"
+
+# replace '.' and '-' with '_'
+# I had some dirs with '-' and couldn't get the value of the corresponding var
+# in the mappings file because it was interpreting the '-'
+selected_after_tr=$(basename "$ssh_name" | tr '.-' '__')
+
+# NOTE: If you don't want to use the 'karabiner-mappings.sh' file, just rename
+# that 'karabiner-mappings.sh' file to something else (or delete it)
+# This file adds a letter at the end of my session to remind me of the karabiner shortcut
+mappings_file="$HOME/github/dotfiles-latest/tmux/tools/linkarzu/ssh-hosts.sh"
+if [ -f "$mappings_file" ]; then
+	source "$mappings_file"
+	# Get the value of the variable whose name matches $base_selected
+	mapping_value="${!selected_after_tr}"
+	selected_name="SSH-${selected_after_tr}-${mapping_value}"
+else
+	selected_name="SSH-$ssh_name"$selected_after_tr
+fi
 
 ###############################################################################
 # I commented this section, uncomment if needed
@@ -42,20 +62,20 @@ SELECTED_NAME="ssh-$SSH_NAME"
 # tmux_running=$(pgrep tmux)
 # # If tmux is not running and the TMUX environment variable is not set, start a new tmux session with the selected directory
 # if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-# 	# I included quotes in "$SSH_NAME" because wasn't changing to dirs that have a space
+# 	# I included quotes in "$ssh_name" because wasn't changing to dirs that have a space
 # 	# Like the iCloud dir
-# 	tmux new-session -s $SSH_NAME_name -c "$SSH_NAME"
+# 	tmux new-session -s $ssh_name_name -c "$ssh_name"
 # 	exit 0
 # fi
 
 ###############################################################################
 
 # If a tmux session with the desired name does not already exist, create it in detached mode
-if ! tmux has-session -t=$SELECTED_NAME 2>/dev/null; then
-	# I included quotes in "$SSH_NAME" because wasn't changing to dirs that have a space
+if ! tmux has-session -t=$selected_name 2>/dev/null; then
+	# I included quotes in "$ssh_name" because wasn't changing to dirs that have a space
 	# Like the iCloud dir
-	tmux new-session -s "$SELECTED_NAME" -d "ssh $SSH_NAME"
+	tmux new-session -s "$selected_name" -d "ssh $ssh_name"
 fi
 
 # Switch to the tmux session with the name derived from the selected directory
-tmux switch-client -t $SELECTED_NAME
+tmux switch-client -t $selected_name

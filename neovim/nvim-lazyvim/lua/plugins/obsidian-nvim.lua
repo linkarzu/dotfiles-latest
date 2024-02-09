c09b838c30c723c3a16fc6f5bc76ccafa3b35e94
Linkarzu <linkarzu@gmail.com>
2024-02-08 18:57:11 -0600

    added obsidian.nvim

---
+++ neovim/nvim-lazyvim/lua/plugins/obsidian-nvim.lua
@@ -0,0 +1,48 @@
+-- https://github.com/epwalsh/obsidian.nvim
+
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+  --   "BufReadPre path/to/my-vault/**.md",
+  --   "BufNewFile path/to/my-vault/**.md",
+  -- },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+
+    -- see below for full list of optional dependencies 👇
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "obsidian_main",
+        path = "~/github/obsidian_main",
+      },
+      -- {
+      --   name = "work",
+      --   path = "~/vaults/work",
+      -- },
+    },
+
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "250-daily",
+      -- folder = "notes/dailies",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y-%m-%d",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- alias_format = "YYYY/Qo/MM-MMM/WW/YYYY-MM-DD-dddd",
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = nil,
+    },
+
+    -- see below for full list of options 👇
+  },
+}

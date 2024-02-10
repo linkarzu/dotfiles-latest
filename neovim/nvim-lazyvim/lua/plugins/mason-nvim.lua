83cf3028d9db2bb571a6ef6b27b07b683613d9ad
Linkarzu <linkarzu@gmail.com>
2024-02-10 09:49:54 -0600

    Declared mason plugin and added "markdown-toc"

---
+++ neovim/nvim-lazyvim/lua/plugins/mason-nvim.lua
@@ -0,0 +1,9 @@
+-- https://github.com/williamboman/mason.nvim
+return {
+  "williamboman/mason.nvim",
+  opts = function(_, opts)
+    opts.ensure_installed = opts.ensure_installed or {}
+    -- marksman and markdownlint come by default in the lazyvim config
+    vim.list_extend(opts.ensure_installed, { "markdownlint", "marksman", "markdown-toc" })
+  end,
+}

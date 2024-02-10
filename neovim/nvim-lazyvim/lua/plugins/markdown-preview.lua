096a4c663db64216b52b84046455fbfd9d11d87f
Linkarzu <linkarzu@gmail.com>
2024-02-10 09:49:25 -0600

    changed markdown preview keymap to <leader>mp

---
+++ neovim/nvim-lazyvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,11 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  keys = {
+    {
+      "<leader>mp",
+      ft = "markdown",
+      "<cmd>MarkdownPreviewToggle<cr>",
+      desc = "Markdown Preview",
+    },
+  },
+}

8832a05aa86fa775b4070f644e5bb7a1afc3d806
Linkarzu <linkarzu@gmail.com>
2024-02-14 05:57:30 -0600

    moves cursor not to the corner, but a bit down

--- hammerspoon/cursor_escape.lua
+++ hammerspoon/cursor_escape.lua
@@ -1,22 +1,22 @@
 -- Function to move the cursor to the top-left corner of the screen
 function moveCursorToCorner()
-  hs.mouse.absolutePosition(hs.geometry.point(0, 0))
+	hs.mouse.absolutePosition(hs.geometry.point(0, 100))
 end
 
 -- Function to simulate pressing the Escape key
 function pressEscapeKey()
-  hs.eventtap.keyStroke({}, "escape")
+	hs.eventtap.keyStroke({}, "escape")
 end
 
 -- Combined Function to handle application focus change events
 function applicationWatcher(appName, eventType, appObject)
-  if eventType == hs.application.watcher.activated then
-    if appName == "Alacritty" then
-      moveCursorToCorner()
-      -- elseif appName == "Google Chrome" or appName == "Safari" or appName == "YouTube" then
-      --   pressEscapeKey()
-    end
-  end
+	if eventType == hs.application.watcher.activated then
+		if appName == "Alacritty" then
+			moveCursorToCorner()
+			-- elseif appName == "Google Chrome" or appName == "Safari" or appName == "YouTube" then
+			--   pressEscapeKey()
+		end
+	end
 end
 
 -- Create a new application watcher

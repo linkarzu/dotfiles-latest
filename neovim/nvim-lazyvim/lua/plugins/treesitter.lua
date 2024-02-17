ea5778d530e1fec054db787f9e475a5102118ccd
Linkarzu <linkarzu@gmail.com>
2024-02-17 14:13:34 -0600

    added sql syntax highlighting

---
+++ neovim/nvim-lazyvim/lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+-- https://github.com/nvim-treesitter/nvim-treesitter
+-- SQL wasn't sohwing in my codeblocks when working with .md files, that's
+-- how I found out it was missing from treesitter
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "sql",
+      },
+    },
+  },
+}

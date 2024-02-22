05f621d6e4860dd93688f788f36b8bd6fe5899cf
Linkarzu <linkarzu@gmail.com>
2024-02-22 09:53:12 -0600

    adding wrapping at 80 chars

--- neovim/nvim-lazyvim/lua/config/options.lua
+++ neovim/nvim-lazyvim/lua/config/options.lua
@@ -9,9 +9,6 @@
 -- Modified version of the bar that also shows the hostname
 vim.opt.winbar = "%=" .. vim.fn.systemlist("hostname")[1] .. "            %m %f"
 
--- Shows colorcolumn that helps me with markdown guidelines
-vim.opt.colorcolumn = "80"
-
 -- If set to 0 it shows all the symbols in a file, like bulletpoints and
 -- codeblock languages, obsidian.nvim works better with 1 or 2
 vim.opt.conceallevel = 1
@@ -23,3 +20,32 @@ vim.opt.conceallevel = 1
 -- Keeps my cursor in the middle whenever possible
 -- This didn't work as expected, but the `stay-centered.lua` plugin did the trick
 -- vim.opt.scrolloff = 999
+
+-- When text reaches this limit, it automatically wraps to the next line.
+-- This WILL NOT auto wrap existing lines, or if you paste a long line into a
+-- file it will not wrap it as well
+-- https://www.reddit.com/r/neovim/comments/1av26kw/i_tried_to_figure_it_out_but_i_give_up_how_do_i/
+vim.opt.textwidth = 80
+
+-- Above option applies the setting to ALL file types, if you want to apply it
+-- to specific files only
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "markdown",
+--   -- pattern = {"python", "javascript", "html"},
+--   callback = function()
+--     vim.opt_local.textwidth = 80
+--   end,
+-- })
+
+-- Shows colorcolumn that helps me with markdown guidelines. This applies to ALL
+-- file types
+vim.opt.colorcolumn = "80"
+
+-- -- To apply it to markdown files only
+-- vim.api.nvim_create_autocmd("BufWinEnter", {
+--   pattern = { "*.md" },
+--   callback = function()
+--     vim.opt.colorcolumn = "80"
+--     vim.opt.textwidth = 80
+--   end,
+-- })

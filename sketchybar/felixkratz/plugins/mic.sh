96b30d3b32dbf1f792f3679401203ce9c1507f19
Linkarzu <linkarzu@gmail.com>
2024-02-24 16:08:19 -0600

    updatd icon when volume between 0 and 100

--- sketchybar/felixkratz/plugins/mic.sh
+++ sketchybar/felixkratz/plugins/mic.sh
@@ -26,7 +26,7 @@ else
 	if [[ $MIC_VOLUME -eq 0 ]]; then
 		sketchybar -m --set mic label="$MIC_NAME $MIC_VOLUME" icon= icon.color=$RED label.color=$RED
 	elif [[ $MIC_VOLUME -gt 0 && $MIC_VOLUME -lt 100 ]]; then
-		sketchybar -m --set mic label="$MIC_NAME $MIC_VOLUME" icon= icon.color=$ORANGE label.color=$ORANGE
+		sketchybar -m --set mic label="$MIC_NAME $MIC_VOLUME" icon= icon.color=$ORANGE label.color=$ORANGE
 	elif [[ $MIC_VOLUME -eq 100 ]]; then
 		sketchybar -m --set mic label="$MIC_NAME $MIC_VOLUME" icon= icon.color=$WHITE label.color=$WHITE
 	fi

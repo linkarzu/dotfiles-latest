8ba462517b07a0b14a3e1609258d7bfccb9756f4
Linkarzu <linkarzu@gmail.com>
2024-02-26 05:05:09 -0600

    initial commit

---
+++ brew/00-base/basic-user.sh
@@ -0,0 +1,17 @@
+# User that wants to install just the basic stuff
+
+tap "homebrew/bundle"
+tap "homebrew/services"
+
+brew "wget"
+brew "git"
+brew "tree"
+cask "raindropio"
+cask "google-chrome"
+cask "spotify"
+cask "hot"
+brew "htop"
+cask "google-drive"
+cask "raycast"
+cask "setapp"
+brew "switchaudio-osx"

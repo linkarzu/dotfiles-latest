b709872c3aee4ae2629a3c5dc4cb53336056b3ad
Linkarzu <linkarzu@gmail.com>
2024-02-27 05:39:07 -0600

    added healine6 purple

--- neovim/nvim-lazyvim/lua/plugins/headlines.lua
+++ neovim/nvim-lazyvim/lua/plugins/headlines.lua
@@ -10,6 +10,7 @@ return {
     vim.cmd([[highlight Headline3 guibg=#a56106 guifg=white]])
     vim.cmd([[highlight Headline4 guibg=#7e0000 guifg=white]])
     vim.cmd([[highlight Headline5 guibg=#1e0b7b guifg=white]])
+    vim.cmd([[highlight Headline6 guibg=#560b7b guifg=white]])
     vim.cmd([[highlight CodeBlock guibg=#09090d]])
     -- vim.cmd [[highlight Dash guibg=#D19A66 gui=bold]]
 
@@ -19,7 +20,7 @@ return {
         fat_headlines = true,
         fat_headline_upper_string = "▃",
         fat_headline_lower_string = "-",
-        headline_highlights = { "Headline1", "Headline2", "Headline3", "Headline4", "Headline5" },
+        headline_highlights = { "Headline1", "Headline2", "Headline3", "Headline4", "Headline5", "Headline6" },
         -- Additional configurations as needed
       },
       -- Add configurations for other file types if necessary

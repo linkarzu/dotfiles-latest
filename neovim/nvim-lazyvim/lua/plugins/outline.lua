93b2c6a1494d950078f3a245cee57a4f89de45cc
Linkarzu <linkarzu@gmail.com>
2024-02-29 13:22:43 -0600

    added autofold_depth = 1 as a comment

--- neovim/nvim-lazyvim/lua/plugins/outline.lua
+++ neovim/nvim-lazyvim/lua/plugins/outline.lua
@@ -14,9 +14,9 @@ return {
   opts = {
     symbol_folding = {
       -- Unfold entire symbol tree by default with false, otherwise enter a
-      -- number starting from 0
+      -- number starting from 1
       autofold_depth = false,
-      -- autofold_depth = 0,
+      -- autofold_depth = 1,
     },
   },
 }

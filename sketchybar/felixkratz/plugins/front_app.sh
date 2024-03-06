b18e41e39308e2562866bacd73f3306de698283c
Linkarzu <linkarzu@gmail.com>
2024-03-06 17:07:52 -0600

    renamed root dir

--- sketchybar/felixkratz/plugins/front_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Filename: ~/github/dotfiles-latest/sketchybar/felixkratz/plugins/front_app.sh
-
-if [ "$SENDER" = "front_app_switched" ]; then
-	sketchybar --set $NAME label="$INFO" icon.background.image="app.$INFO"
-fi

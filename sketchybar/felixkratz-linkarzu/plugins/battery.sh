611f49d3be3c1e64ddce4af29841b47ccafaf027
Christian Arzu <linkarzu@gmail.com>
2024-03-13 06:50:24 -0600

    added battery percentage

--- sketchybar/felixkratz-linkarzu/plugins/battery.sh
+++ sketchybar/felixkratz-linkarzu/plugins/battery.sh
@@ -1,33 +1,32 @@
-#!/bin/bash
+#!/bin/sh
 
-source "$CONFIG_DIR/icons.sh"
-source "$CONFIG_DIR/colors.sh"
+PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
+CHARGING="$(pmset -g batt | grep 'AC Power')"
 
-BATTERY_INFO="$(pmset -g batt)"
-PERCENTAGE=$(echo "$BATTERY_INFO" | grep -Eo "\d+%" | cut -d% -f1)
-CHARGING=$(echo "$BATTERY_INFO" | grep 'AC Power')
-
-if [ $PERCENTAGE = "" ]; then
-  exit 0
+if [ "$PERCENTAGE" = "" ]; then
+	exit 0
 fi
 
-DRAWING=on
-COLOR=$WHITE
-case ${PERCENTAGE} in
-  9[0-9]|100) ICON=$BATTERY_100; DRAWING=off
-  ;;
-  [6-8][0-9]) ICON=$BATTERY_75; DRAWING=off
-  ;;
-  [3-5][0-9]) ICON=$BATTERY_50
-  ;;
-  [1-2][0-9]) ICON=$BATTERY_25; COLOR=$ORANGE
-  ;;
-  *) ICON=$BATTERY_0; COLOR=$RED
+case "${PERCENTAGE}" in
+9[0-9] | 100)
+	ICON=""
+	;;
+[6-8][0-9])
+	ICON=""
+	;;
+[3-5][0-9])
+	ICON=""
+	;;
+[1-2][0-9])
+	ICON=""
+	;;
+*) ICON="" ;;
 esac
 
-if [[ $CHARGING != "" ]]; then
-  ICON=$BATTERY_CHARGING
-  DRAWING=off
+if [[ "$CHARGING" != "" ]]; then
+	ICON=""
 fi
 
-sketchybar --set $NAME drawing=$DRAWING icon="$ICON" icon.color=$COLOR
+# The item invoking this script (name $NAME) will get its icon and label
+# updated with the current battery status
+sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}"

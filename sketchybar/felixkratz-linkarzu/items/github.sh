0441bd54ca5324d18836382ec694229198fe8982
Linkarzu <linkarzu@gmail.com>
2024-03-14 10:20:47 -0600

    set label.padding_left=2

--- sketchybar/felixkratz-linkarzu/items/github.sh
+++ sketchybar/felixkratz-linkarzu/items/github.sh
@@ -6,7 +6,7 @@ POPUP_CLICK_SCRIPT="sketchybar --set \$NAME popup.drawing=toggle"
 
 github_bell=(
 	padding_right=4
-	label.padding_left=1
+	label.padding_left=2
 	update_freq=180
 	icon=$BELL
 	icon.font="$FONT:Bold:15.0"

e2524cd9e7d7394f05a437986361d28a890b5dd7
Linkarzu <linkarzu@gmail.com>
2024-03-14 09:35:26 -0600

    draw the battery, only if a battery is present

--- sketchybar/felixkratz-linkarzu/plugins/battery.sh
+++ sketchybar/felixkratz-linkarzu/plugins/battery.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+BATTERY_INFO="$(pmset -g batt)"
 PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
 CHARGING="$(pmset -g batt | grep 'AC Power')"
+
+# Check for the absence of "InternalBattery" to determine no battery present
+# My mac mini doesn't have a battery, and it was leaving a blank space where the
+# battery was supposed to be
+if ! echo "$BATTERY_INFO" | grep -q "InternalBattery"; then
+	sketchybar --set "$NAME" drawing=off
+else
+	sketchybar --set "$NAME" drawing=on
+fi
 
 if [ "$PERCENTAGE" = "" ]; then
 	exit 0

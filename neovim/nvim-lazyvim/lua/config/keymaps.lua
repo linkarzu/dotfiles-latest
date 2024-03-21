338b68fd0728584a736ccdf0fe05bfc8af663619
Linkarzu <linkarzu@gmail.com>
2024-03-21 09:15:33 -0600

    execute GO and BASH files in a TMUX pane on the right, intead of vim split
    Executing the file in a tmux pane, allows me to easily select the text if I need to for copying and pasting into my notes

--- neovim/nvim-lazyvim/lua/config/keymaps.lua
+++ neovim/nvim-lazyvim/lua/config/keymaps.lua
@@ -54,35 +54,78 @@ vim.keymap.set("n", "<leader>fx", '<cmd>!chmod +x "%"<CR>', { silent = true, des
 vim.keymap.set("n", "<leader>fX", '<cmd>!chmod -x "%"<CR>', { silent = true, desc = "Remove executable flag" })
 -- vim.keymap.set("n", "<leader>fX", "<cmd>!chmod -x %<CR>", { silent = true, desc = "Remove executable flag" })
 
--- If this is a bash script, make it executable, and execute it in a split pane on the right
+-- If this is a bash script, make it executable, and execute it in a tmux pane on the right
+-- Using a tmux pane allows me to easily select text
 -- Had to include quotes around "%" because there are some apple dirs that contain spaces, like iCloud
 vim.keymap.set("n", "<leader>cb", function()
   local file = vim.fn.expand("%") -- Get the current file name
   local first_line = vim.fn.getline(1) -- Get the first line of the file
   if string.match(first_line, "^#!/") then -- If first line contains shebang
     local escaped_file = vim.fn.shellescape(file) -- Properly escape the file name for shell commands
-    vim.cmd("!chmod +x " .. escaped_file) -- Make the file executable
-    vim.cmd("vsplit") -- Split the window vertically
-    vim.cmd("terminal " .. escaped_file) -- Open terminal and execute the file
-    vim.cmd("startinsert") -- Enter insert mode, recommended by echasnovski on Reddit
+
+    -- Execute the script on a tmux pane on the right. On my mac I use zsh, so
+    -- running this script with bash to not execute my zshrc file after
+    -- vim.cmd("silent !tmux split-window -h -l 70 'bash -c \"" .. escaped_file .. "; exec bash\"'")
+    vim.cmd(
+      "silent !tmux split-window -h -l 70 'bash -c \""
+        .. escaped_file
+        .. "; echo; echo Press any key to exit...; read -n 1; exit\"'"
+    )
   else
     vim.cmd("echo 'Not a script. Shebang line not found.'")
   end
-end, { desc = "Execute bash script in pane on the right" })
+end, { desc = "BASH, execute file" })
 
--- If this is a .go file, execute it in a split pane on the right
+-- -- If this is a bash script, make it executable, and execute it in a split pane on the right
+-- -- Had to include quotes around "%" because there are some apple dirs that contain spaces, like iCloud
+-- vim.keymap.set("n", "<leader>cb", function()
+--   local file = vim.fn.expand("%") -- Get the current file name
+--   local first_line = vim.fn.getline(1) -- Get the first line of the file
+--   if string.match(first_line, "^#!/") then -- If first line contains shebang
+--     local escaped_file = vim.fn.shellescape(file) -- Properly escape the file name for shell commands
+--     vim.cmd("!chmod +x " .. escaped_file) -- Make the file executable
+--     vim.cmd("vsplit") -- Split the window vertically
+--     vim.cmd("terminal " .. escaped_file) -- Open terminal and execute the file
+--     vim.cmd("startinsert") -- Enter insert mode, recommended by echasnovski on Reddit
+--   else
+--     vim.cmd("echo 'Not a script. Shebang line not found.'")
+--   end
+-- end, { desc = "Execute bash script in pane on the right" })
+
+-- If this is a .go file, execute it in a tmux pane on the right
+-- Using a tmux pane allows me to easily select text
 -- Had to include quotes around "%" because there are some apple dirs that contain spaces, like iCloud
 vim.keymap.set("n", "<leader>cg", function()
   local file = vim.fn.expand("%") -- Get the current file name
   if string.match(file, "%.go$") then -- Check if the file is a .go file
     local escaped_file = vim.fn.shellescape(file) -- Properly escape the file name for shell commands
-    vim.cmd("vsplit") -- Split the window vertically
-    vim.cmd("terminal go run " .. escaped_file) -- Run the file in a terminal pane
-    vim.cmd("startinsert") -- Enter insert mode in the terminal
+    local command_to_run = "go run " .. escaped_file
+    -- `-l 70` specifies the size of the tmux pane, in this case 70 columns
+    local cmd = "silent !tmux split-window -h -l 70 'sh -c \"echo "
+      .. command_to_run
+      .. "; echo" -- Add a blank line
+      .. "; "
+      .. command_to_run
+      .. "; echo; echo; echo Press enter to exit...; read _\"'"
+    vim.cmd(cmd)
   else
     vim.cmd("echo 'Not a Go file.'") -- Notify the user if the file is not a Go file
   end
-end, { desc = "Execute Go file in pane on the right" })
+end, { desc = "GOLANG, execute file" })
+
+-- -- If this is a .go file, execute it in a split pane on the right
+-- -- Had to include quotes around "%" because there are some apple dirs that contain spaces, like iCloud
+-- vim.keymap.set("n", "<leader>cg", function()
+--   local file = vim.fn.expand("%") -- Get the current file name
+--   if string.match(file, "%.go$") then -- Check if the file is a .go file
+--     local escaped_file = vim.fn.shellescape(file) -- Properly escape the file name for shell commands
+--     vim.cmd("split") -- Split the window vertically
+--     vim.cmd("terminal go run " .. escaped_file) -- Run the file in a terminal pane
+--     vim.cmd("startinsert") -- Enter insert mode in the terminal
+--   else
+--     vim.cmd("echo 'Not a Go file.'") -- Notify the user if the file is not a Go file
+--   end
+-- end, { desc = "Execute Go file in pane on the right" })
 
 -- Paste file path with the wordname Filename: first
 vim.keymap.set("n", "<leader>fp", function()

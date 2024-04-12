e5cc702ae653d71ccd2bd0f46926f202f28880f8
Linkarzu <linkarzu@gmail.com>
2024-04-11 18:09:45 -0600

    added bufexplorer plugin, testing it out

---
+++ neovim/nvim-lazyvim/lua/plugins/bufexplorer.lua
@@ -0,0 +1,27 @@
+-- Filename: ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/bufexplorer.lua
+-- ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/bufexplorer.lua
+--
+-- https://github.com/jlanzarotta/bufexplorer
+
+return {
+  {
+    "jlanzarotta/bufexplorer",
+    -- To load the plugin at startup, I set lazy to false
+    lazy = false,
+    -- Here you can configure other options for the plugin found in the customization section
+    -- https://github.com/jlanzarotta/bufexplorer/blob/20f0440948653b5482d555a35a432135ba46a26d/doc/bufexplorer.txt#L132
+    -- When I open bufexplorer I want to see relative paths, not absolute ones
+    config = function()
+      vim.g.bufExplorerShowRelativePath = 1
+    end,
+    -- In case you want to add keymaps, I probably won't use this one, but
+    -- leaving it there
+    keys = {
+      {
+        "<leader>bz",
+        "<cmd>BufExplorer<cr>",
+        desc = "Open bufexplorer",
+      },
+    },
+  },
+}

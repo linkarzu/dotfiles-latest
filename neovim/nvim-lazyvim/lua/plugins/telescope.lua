24893bb4fb7995737e0bb12ab73e4d23edaabe0d
Linkarzu <linkarzu@gmail.com>
2024-04-11 18:09:33 -0600

    leader+space alt buffer, leader+bb bufexplorer

--- neovim/nvim-lazyvim/lua/plugins/telescope.lua
+++ neovim/nvim-lazyvim/lua/plugins/telescope.lua
@@ -1,3 +1,6 @@
+-- Filename: ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/telescope.lua
+-- ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/telescope.lua
+
 -- https://github.com/nvim-telescope/telescope.nvim
 -- http://www.lazyvim.org/plugins/editor#telescopenvim-optional
 
@@ -23,6 +26,23 @@ return {
           require("telescope.builtin").find_files({})
         end,
         desc = "Find Files (root dir)",
+      },
+      -- I tried disabling this keymap, but I couldn't
+      -- So fuck it, I'm adding the keymap that I need here, which is to
+      -- alternate between the last 2 files
+      --
+      -- I also tried overwriting this keymap in keymaps.lua but it was always
+      -- overriden by this telescope.lua file
+      {
+        "<leader><space>",
+        "<cmd>e #<cr>",
+        desc = "Switch to Other Buffer",
+      },
+      -- This is used to open the BufExplorer plugin
+      {
+        "<leader>bb",
+        "<cmd>BufExplorer<cr>",
+        desc = "Open bufexplorer",
       },
     },
   },

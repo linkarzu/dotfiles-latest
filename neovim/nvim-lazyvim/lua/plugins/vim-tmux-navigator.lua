2a86a7c48d14698dcb988ff2489da8892208e8ad
Linkarzu <linkarzu@gmail.com>
2024-05-14 17:53:49 -0600

    neovim vim-tmux-navigator, added plugin to switch between neovim and tmux panes

---
+++ neovim/nvim-lazyvim/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,28 @@
+-- Filename: ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/vim-tmux-navigator.lua
+-- ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/vim-tmux-navigator.lua
+
+-- https://github.com/christoomey/vim-tmux-navigator
+--
+-- This plugin allows me to switch between neovim and tmux panes using
+-- ctrl+vim-motions, for it to work with tmux panes, you also need to install
+-- the same plugin in tmux
+
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+}

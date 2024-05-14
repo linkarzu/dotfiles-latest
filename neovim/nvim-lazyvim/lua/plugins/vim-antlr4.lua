bdf1c3299e1b1da828fa879e70ae4a5b9c51dda7
Linkarzu <linkarzu@gmail.com>
2024-05-14 17:53:10 -0600

    neovim vim-antlr4.lua added plugin

---
+++ neovim/nvim-lazyvim/lua/plugins/vim-antlr4.lua
@@ -0,0 +1,12 @@
+-- Filename: ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/vim-antlr4.lua
+-- ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/vim-antlr4.lua
+--
+-- I found a link to this plugin here
+-- https://github.com/nvim-treesitter/nvim-treesitter/issues/2282#issuecomment-1132043902
+--
+-- Here's the plugin itself
+-- https://github.com/Joakker/vim-antlr4
+
+return {
+  { "Joakker/vim-antlr4" },
+}

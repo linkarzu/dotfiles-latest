262729fb3b0f3792193b3c42579e702a42dda5f6
Linkarzu <linkarzu@gmail.com>
2024-06-02 14:02:40 -0600

    neovim leap.nvim initial commit

---
+++ neovim/nvim-lazyvim/lua/plugins/leap.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "ggandor/leap.nvim",
+    enabled = true,
+    keys = {
+      { "s", mode = { "n", "x", "o" }, desc = "Leap Forward to" },
+      { "S", mode = { "n", "x", "o" }, desc = "Leap Backward to" },
+      -- { "gs", mode = { "n", "x", "o" }, desc = "Leap from Windows" },
+    },
+    config = function(_, opts)
+      local leap = require("leap")
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      leap.add_default_mappings(true)
+      vim.keymap.del({ "x", "o" }, "x")
+      vim.keymap.del({ "x", "o" }, "X")
+      vim.keymap.del({ "n", "v", "x", "o" }, "gs")
+    end,
+  },
+}

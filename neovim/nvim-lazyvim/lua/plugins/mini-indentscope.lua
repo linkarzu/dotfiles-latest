d3982befd18235597f9360b41b7295d40fa79da3
Linkarzu <linkarzu@gmail.com>
2024-06-03 17:34:46 -0600

    neovim mini-indentscope initial commit

---
+++ neovim/nvim-lazyvim/lua/plugins/mini-indentscope.lua
@@ -0,0 +1,38 @@
+-- https://github.com/echasnovski/mini.indentscope
+--
+-- Filename: ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/mini-indentscope.lua
+-- ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/mini-indentscope.lua
+--
+-- This plugins shows you a vertical colored line that allows you to see the
+-- scope of your indentations
+
+return {
+  "echasnovski/mini.indentscope",
+  version = false, -- wait till new 0.7.0 release to put it back on semver
+  event = "LazyFile",
+  opts = {
+    -- symbol = "▏",
+    symbol = "│",
+    options = { try_as_border = true },
+  },
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      },
+      callback = function()
+        vim.b.miniindentscope_disable = true
+      end,
+    })
+  end,
+}

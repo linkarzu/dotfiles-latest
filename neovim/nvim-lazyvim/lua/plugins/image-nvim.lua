3f79f768059b230a058241f9938a1ffea70f4bae
Linkarzu <linkarzu@gmail.com>
2024-06-03 17:32:12 -0600

    neovim image.nvim render images in html and css files

--- neovim/nvim-lazyvim/lua/plugins/image-nvim.lua
+++ neovim/nvim-lazyvim/lua/plugins/image-nvim.lua
@@ -44,11 +44,17 @@ return {
     config = function()
       require("image").setup({
         backend = "kitty",
+        kitty_method = "normal",
         integrations = {
+          -- Notice these are the settings for markdown files
           markdown = {
             enabled = true,
             clear_in_insert_mode = false,
+            -- Set this to false if you don't want to render images coming from
+            -- a URL
             download_remote_images = true,
+            -- Change this if you would only like to render the image where the
+            -- cursor is at
             only_render_image_at_cursor = false,
             -- markdown extensions (ie. quarto) can go here
             filetypes = { "markdown", "vimwiki" },
@@ -60,17 +66,28 @@ return {
             only_render_image_at_cursor = false,
             filetypes = { "norg" },
           },
+          -- This is disabled by default
+          -- Detect and render images referenced in HTML files
+          -- Make sure you have an html treesitter parser installed
+          -- ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/treesitter.lua
           html = {
-            enabled = false,
+            enabled = true,
           },
+          -- This is disabled by default
+          -- Detect and render images referenced in CSS files
+          -- Make sure you have a css treesitter parser installed
+          -- ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/treesitter.lua
           css = {
-            enabled = false,
+            enabled = true,
           },
         },
         max_width = nil,
         max_height = nil,
         max_width_window_percentage = nil,
-        max_height_window_percentage = 20,
+        -- This is what I changed to make my images look smaller, like a
+        -- thumbnail, the default value is 50
+        max_height_window_percentage = 10,
+        -- max_height_window_percentage = 40,
         -- toggles images when windows are overlapped
         window_overlap_clear_enabled = false,
         window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },

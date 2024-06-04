5d44e99f50833dbd976d82e20b3655c1df61f6a7
Linkarzu <linkarzu@gmail.com>
2024-06-04 13:54:37 -0600

    neovim image.nvim added newlines between sections

--- neovim/nvim-lazyvim/lua/plugins/image-nvim.lua
+++ neovim/nvim-lazyvim/lua/plugins/image-nvim.lua
@@ -84,18 +84,23 @@ return {
         max_width = nil,
         max_height = nil,
         max_width_window_percentage = nil,
+
         -- This is what I changed to make my images look smaller, like a
         -- thumbnail, the default value is 50
         max_height_window_percentage = 10,
         -- max_height_window_percentage = 40,
+
         -- toggles images when windows are overlapped
         window_overlap_clear_enabled = false,
         window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+
         -- auto show/hide images when the editor gains/looses focus
         editor_only_render_when_focused = true,
+
         -- auto show/hide images in the correct tmux window
         -- In the tmux.conf add `set -g visual-activity off`
         tmux_show_only_in_active_window = true,
+
         -- render image files as images when opened
         hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
       })

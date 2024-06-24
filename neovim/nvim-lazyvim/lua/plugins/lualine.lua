11f20ddf4775b19f8aa81b53866eaf32f1c7d3f7
Linkarzu <linkarzu@gmail.com>
2024-06-24 15:42:10 -0600

    neovim lualine removed time and updated colors to match Eldritch

--- neovim/nvim-lazyvim/lua/plugins/lualine.lua
+++ neovim/nvim-lazyvim/lua/plugins/lualine.lua
@@ -1,8 +1,19 @@
+-- Filename: ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/lualine.lua
+-- ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/lualine.lua
+--
+-- I think I grabbed the config from this plugin using Folke's lazyvim.org and
+-- just made some changes on the top
+-- http://www.lazyvim.org/plugins/ui#lualinenvim
+
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
   opts = function(_, opts)
-    local fg_color = "#36454F" -- Foreground color for the text
+    local fg_color = "#212337" -- Foreground color for the text
+
+    -- Disable lualine_z section which shows the time
+    -- http://www.lazyvim.org/plugins/ui#lualinenvim
+    opts.sections.lualine_z = {}
 
     -- Function to determine file permissions and appropriate background color
     local function get_permissions_color()
@@ -13,8 +24,8 @@ return {
         local permissions = vim.fn.getfperm(file)
         -- Check only the first three characters for 'rwx' to determine owner permissions
         local owner_permissions = permissions:sub(1, 3)
-        -- Green for owner 'rwx', blue otherwise
-        return permissions, owner_permissions == "rwx" and "#50fa7b" or "#0099ff"
+        -- green for owner 'rwx', blue otherwise
+        return permissions, owner_permissions == "rwx" and "#37f499" or "#04d1f9"
       end
     end
 
@@ -23,14 +34,14 @@ return {
     local function decide_color()
       local hostname = vim.fn.systemlist("hostname")[1]
       local last_char = hostname:sub(-1)
-      local bg_color = "#A6AAF1" -- Default pink
+      local bg_color = "#a48cf2" -- Default pink
 
       if last_char == "1" then
-        bg_color = "#0DFFAE"
+        bg_color = "#37f499"
       elseif last_char == "2" then
-        bg_color = "#FF6200"
+        bg_color = "#f16c75"
       elseif last_char == "3" then
-        bg_color = "#DBF227"
+        bg_color = "#f7c67f"
       end
 
       return bg_color

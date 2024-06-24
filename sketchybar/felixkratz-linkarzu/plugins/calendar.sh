b600aff8e2ca6052817ce39d89421300cfc4c5aa
Linkarzu <linkarzu@gmail.com>
2024-06-24 16:13:36 -0600

    sketchybar calendar.sh added year

--- sketchybar/felixkratz-linkarzu/plugins/calendar.sh
+++ sketchybar/felixkratz-linkarzu/plugins/calendar.sh
@@ -4,4 +4,4 @@
 # sketchybar --set $NAME icon="$(date '+%a %d. %b')" label="$(date '+%H:%M')"
 
 # In case you don't want to show the time
-sketchybar --set $NAME icon="$(date '+%a %d. %b')"
+sketchybar --set $NAME icon="$(date '+%a %d. %b %Y')"

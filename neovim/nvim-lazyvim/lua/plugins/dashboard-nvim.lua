d5c4557a32b76e7c703ba5830ee095fd0a51828f
Linkarzu <linkarzu@gmail.com>
2024-06-28 04:36:29 -0600

    neovim dashboard-nvim.lua removed items I never use

--- neovim/nvim-lazyvim/lua/plugins/dashboard-nvim.lua
+++ neovim/nvim-lazyvim/lua/plugins/dashboard-nvim.lua
@@ -12,11 +12,12 @@ return {
   ██║ ╚████║███████╗╚██████╔╝██████╔╝███████╗██║  ██║██║ ╚████║
   ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝
 
-[linkarzu.com]
+[Linkarzu.com]
 
     ]]
 
-    logo = string.rep("\n", 8) .. logo .. "\n\n"
+    -- logo = string.rep("\n", 8) .. logo .. "\n\n"
+    logo = string.rep("\n", 8) .. logo
 
     local opts = {
       theme = "doom",
@@ -29,14 +30,14 @@ return {
         header = vim.split(logo, "\n"),
         -- stylua: ignore
         center = {
-          { action = 'lua LazyVim.pick()()',                           desc = " Find File",       icon = " ", key = "f" },
-          { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
-          { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = " ", key = "r" },
-          { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "g" },
-          { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
+          -- { action = 'lua LazyVim.pick()()',                           desc = " Find File",       icon = " ", key = "f" },
+          -- { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
+          -- { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = " ", key = "r" },
+          -- { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "g" },
+          -- { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
           { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
-          { action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
-          { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
+          -- { action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
+          -- { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
           { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
         },
         footer = function()
@@ -48,7 +49,9 @@ return {
     }
 
     for _, button in ipairs(opts.config.center) do
-      button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+      -- -- The default padding is 43, but I changed it to 15
+      -- button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+      button.desc = button.desc .. string.rep(" ", 15 - #button.desc)
       button.key_format = "  %s"
     end
 

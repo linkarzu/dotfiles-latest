b8f28ba0191e489e4bc4f9468795b362f97387bc
Linkarzu <linkarzu@gmail.com>
2024-06-28 12:01:10 -0600

    neovim bullets.lua initial commit, helps with bulletpoints

---
+++ neovim/nvim-lazyvim/lua/plugins/bullets.lua
@@ -0,0 +1,17 @@
+-- Filename: ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/bullets.lua
+-- ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/bullets.lua
+--
+-- This plugin automatically adds bulletpoints on the next line respecting
+-- indentation
+-- In markdown or a text file start a bulleted list using - or *. Press return
+-- to go to the next line, a new list item will be created.
+--
+-- When in insert mode, you can increase indentation with ctrl+t and decrease it
+-- with ctrl+d
+--
+-- By default its enabled on filetypes 'markdown', 'text', 'gitcommit', 'scratch'
+-- https://github.com/bullets-vim/bullets.vim
+
+return {
+  "bullets-vim/bullets.vim",
+}

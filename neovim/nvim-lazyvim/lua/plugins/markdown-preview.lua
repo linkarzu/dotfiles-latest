b982b7515ecb7b80b2393e8c506b863fe00fc07f
Linkarzu <linkarzu@gmail.com>
2024-07-01 07:36:21 -0600

    neovim plugins markdown-preview.lua added link to repo

--- neovim/nvim-lazyvim/lua/plugins/markdown-preview.lua
+++ neovim/nvim-lazyvim/lua/plugins/markdown-preview.lua
@@ -1,3 +1,9 @@
+-- Filename: ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/markdown-preview.lua
+-- ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/markdown-preview.lua
+--
+-- Link to github repo
+-- https://github.com/iamcco/markdown-preview.nvim
+
 return {
   "iamcco/markdown-preview.nvim",
   keys = {

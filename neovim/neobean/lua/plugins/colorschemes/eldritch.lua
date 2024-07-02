deff7ec73084a1263f9deee9e3f5cb51e227fe7d
Linkarzu <linkarzu@gmail.com>
2024-07-02 14:05:26 -0600

    neovim eldritch.lua updated spell colors

--- neovim/neobean/lua/plugins/colorschemes/eldritch.lua
+++ neovim/neobean/lua/plugins/colorschemes/eldritch.lua
@@ -21,14 +21,32 @@ return {
       -- I do the line below to change the color of bold text
       highlights["@markup.strong"] = { fg = "#f265b5", bold = true }
 
-      -- Trying to change the spell underline color
-      -- vim.cmd("highlight SpellBad cterm=undercurl gui=undercurl guisp=#37f499")
-      -- highlights["@spell"] = { undercurl = true, sp = "#37f499" }
-      -- highlights.SpellBad = { undercurl = true, sp = "#37f499" }
-      highlights.SpellBad = { sp = "#37f499", undercurl = true }
-      highlights.SpellCap = { sp = "#37f499", undercurl = true }
-      highlights.SpellLocal = { sp = "#37f499", undercurl = true }
-      highlights.SpellRare = { sp = "#37f499", undercurl = true }
+      -- Change the spell underline color
+      --
+      -- Every time you change an undercurl setting here, make sure to kill the tmux
+      -- session or you won't see the changes
+      --
+      -- For this to work in kitty, you need to add some configs to your
+      -- tmux.conf file, go to that file and look for "Undercurl support"
+      --
+      -- You could also set these to bold or italic if you wanted, example:
+      -- highlights.SpellBad = { sp = "#37f499", undercurl = true, bold = true, italic = true }
+      --
+      highlights.SpellBad = { sp = "#f16c75", undercurl = true, bold = true, italic = true }
+      highlights.SpellCap = { sp = "#f1fc79", undercurl = true, bold = true, italic = true }
+      highlights.SpellLocal = { sp = "#ebfafa", undercurl = true, bold = true, italic = true }
+      highlights.SpellRare = { sp = "#a48cf2", undercurl = true, bold = true, italic = true }
+
+      -- highlights.SpellBad = { sp = "#f16c75", undercurl = true }
+      -- highlights.SpellCap = { sp = "#f16c75", undercurl = true }
+      -- highlights.SpellLocal = { sp = "#f16c75", undercurl = true }
+      -- highlights.SpellRare = { sp = "#f16c75", undercurl = true }
+
+      -- My headings are this color, so this is not a good idea
+      -- highlights.SpellBad = { sp = "#f16c75", undercurl = true, fg = "#37f499" }
+      -- highlights.SpellCap = { sp = "#f16c75", undercurl = true, fg = "#37f499" }
+      -- highlights.SpellLocal = { sp = "#f16c75", undercurl = true, fg = "#37f499" }
+      -- highlights.SpellRare = { sp = "#f16c75", undercurl = true, fg = "#37f499" }
 
       -- These colors are used by mini-files.lua to show git changes
       highlights.MiniDiffSignAdd = { fg = "#f1fc79", bold = true }

94ec0fbf9ec8fb8e44f6ad2ba6de1302b3fd6f0e
Linkarzu <linkarzu@gmail.com>
2024-07-03 19:04:46 -0600

    neovim keymap delete current file with confirmation <leader>fD

--- neovim/neobean/lua/config/keymaps.lua
+++ neovim/neobean/lua/config/keymaps.lua
@@ -1155,6 +1155,52 @@ vim.keymap.set("n", "<leader>;;", function()
   vim.cmd("startinsert!")
   -- vim.api.nvim_win_set_cursor(0, { row, #heading })
 end, { desc = "[P]H4 heading and date" })
+-- Function to delete the current file with confirmation
+local function delete_current_file()
+  local current_file = vim.fn.expand("%:p")
+  if current_file and current_file ~= "" then
+    -- Check if trash utility is installed
+    if vim.fn.executable("trash") == 0 then
+      vim.api.nvim_echo({
+        { "- Trash utility not installed. Make sure to install it first\n", "ErrorMsg" },
+        { "- In macOS run `brew install trash`\n", nil },
+      }, false, {})
+      return
+    end
+    -- Prompt for confirmation before deleting the file
+    vim.ui.input({
+      prompt = "Type 'del' to delete the file '" .. current_file .. "': ",
+    }, function(input)
+      if input == "del" then
+        -- Delete the file using trash app
+        local success, _ = pcall(function()
+          vim.fn.system({ "trash", vim.fn.fnameescape(current_file) })
+        end)
+        if success then
+          vim.api.nvim_echo({
+            { "File deleted from disk:\n", "Normal" },
+            { current_file, "Normal" },
+          }, false, {})
+          -- Close the buffer after deleting the file
+          vim.cmd("bd!")
+        else
+          vim.api.nvim_echo({
+            { "Failed to delete file:\n", "ErrorMsg" },
+            { current_file, "ErrorMsg" },
+          }, false, {})
+        end
+      else
+        vim.api.nvim_echo({
+          { "File deletion canceled.", "Normal" },
+        }, false, {})
+      end
+    end)
+  else
+    vim.api.nvim_echo({
+      { "No file to delete", "WarningMsg" },
+    }, false, {})
+  end
+end
 
 vim.keymap.set("n", "<leader>uu", function()
   local date = os.date("%Y-%m-%d-%A")
@@ -1169,6 +1215,10 @@ vim.keymap.set("n", "<leader>uu", function()
   vim.cmd("startinsert!")
   -- vim.api.nvim_win_set_cursor(0, { row, #heading })
 end, { desc = "[P]H5 heading and date" })
+-- Keymap to delete the current file
+vim.keymap.set("n", "<leader>fD", function()
+  delete_current_file()
+end, { desc = "[P]Delete current file" })
 
 vim.keymap.set("n", "<leader>ii", function()
   local date = os.date("%Y-%m-%d-%A")

00052a49eb373f15ac17a25a3a78995d402aa6f4
Linkarzu <linkarzu@gmail.com>
2024-07-25 11:54:36 -0600

    neovim highlights.lua changed markdown heading colors to match markdown.nvim

--- neovim/neobean/lua/config/highlights.lua
+++ neovim/neobean/lua/config/highlights.lua
@@ -5,9 +5,22 @@ local color3_bg = "#04d1f9"
 local color4_bg = "#a48cf2"
 local color5_bg = "#f1fc79"
 local color6_bg = "#f7c67f"
-vim.cmd(string.format([[highlight @markup.heading.1.markdown cterm=bold gui=bold guifg=%s]], color1_bg))
-vim.cmd(string.format([[highlight @markup.heading.2.markdown cterm=bold gui=bold guifg=%s]], color2_bg))
-vim.cmd(string.format([[highlight @markup.heading.3.markdown cterm=bold gui=bold guifg=%s]], color3_bg))
-vim.cmd(string.format([[highlight @markup.heading.4.markdown cterm=bold gui=bold guifg=%s]], color4_bg))
-vim.cmd(string.format([[highlight @markup.heading.5.markdown cterm=bold gui=bold guifg=%s]], color5_bg))
-vim.cmd(string.format([[highlight @markup.heading.6.markdown cterm=bold gui=bold guifg=%s]], color6_bg))
+local color_fg = "#323449"
+vim.cmd(
+  string.format([[highlight @markup.heading.1.markdown cterm=bold gui=bold guifg=%s guibg=%s]], color_fg, color1_bg)
+)
+vim.cmd(
+  string.format([[highlight @markup.heading.2.markdown cterm=bold gui=bold guifg=%s guibg=%s]], color_fg, color2_bg)
+)
+vim.cmd(
+  string.format([[highlight @markup.heading.3.markdown cterm=bold gui=bold guifg=%s guibg=%s]], color_fg, color3_bg)
+)
+vim.cmd(
+  string.format([[highlight @markup.heading.4.markdown cterm=bold gui=bold guifg=%s guibg=%s]], color_fg, color4_bg)
+)
+vim.cmd(
+  string.format([[highlight @markup.heading.5.markdown cterm=bold gui=bold guifg=%s guibg=%s]], color_fg, color5_bg)
+)
+vim.cmd(
+  string.format([[highlight @markup.heading.6.markdown cterm=bold gui=bold guifg=%s guibg=%s]], color_fg, color6_bg)
+)

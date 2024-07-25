d1a27342726e3f985cd0efa22d7d590b41c8d9ae
Linkarzu <linkarzu@gmail.com>
2024-07-25 10:14:33 -0600

    neovim markdown.lua added comments on how to update highlights when hover over headings

--- neovim/neobean/lua/plugins/markdown.lua
+++ neovim/neobean/lua/plugins/markdown.lua
@@ -2,7 +2,13 @@
 -- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/markdown.lua
 
 -- https://github.com/MeanderingProgrammer/markdown.nvim
--- lamw25wmal
+--
+-- When I hover over markdown headings, this plugins goes away, so I need to
+-- edit the default highlights
+-- I tried adding this as an autocommand, in the options.lua
+-- file, also in the markdownl.lua file, but the highlights kept being overriden
+-- so the inly way is the only way I was able to make it work was loading it
+-- after the config.lazy in the init.lua file lamw25wmal
 
 -- Define color variables
 local color1_bg = "#f265b5"
@@ -12,6 +18,7 @@ local color4_bg = "#a48cf2"
 local color5_bg = "#f1fc79"
 local color6_bg = "#f7c67f"
 local color_fg = "#323449"
+-- local color_sign = "#ebfafa"
 
 return {
   "MeanderingProgrammer/markdown.nvim",
@@ -30,12 +37,12 @@ return {
 
       -- Highlight for the heading and sign icons (symbol on the left)
       -- I have the sign disabled for now, so this makes no effect
-      vim.cmd(string.format([[highlight Headline1Fg cterm=bold gui=bold guifg=%s]], color_fg)),
-      vim.cmd(string.format([[highlight Headline2Fg cterm=bold gui=bold guifg=%s]], color_fg)),
-      vim.cmd(string.format([[highlight Headline3Fg cterm=bold gui=bold guifg=%s]], color_fg)),
-      vim.cmd(string.format([[highlight Headline4Fg cterm=bold gui=bold guifg=%s]], color_fg)),
-      vim.cmd(string.format([[highlight Headline5Fg cterm=bold gui=bold guifg=%s]], color_fg)),
-      vim.cmd(string.format([[highlight Headline6Fg cterm=bold gui=bold guifg=%s]], color_fg)),
+      vim.cmd(string.format([[highlight Headline1Fg cterm=bold gui=bold guifg=%s]], color1_bg)),
+      vim.cmd(string.format([[highlight Headline2Fg cterm=bold gui=bold guifg=%s]], color2_bg)),
+      vim.cmd(string.format([[highlight Headline3Fg cterm=bold gui=bold guifg=%s]], color3_bg)),
+      vim.cmd(string.format([[highlight Headline4Fg cterm=bold gui=bold guifg=%s]], color4_bg)),
+      vim.cmd(string.format([[highlight Headline5Fg cterm=bold gui=bold guifg=%s]], color5_bg)),
+      vim.cmd(string.format([[highlight Headline6Fg cterm=bold gui=bold guifg=%s]], color6_bg)),
 
       backgrounds = {
         "Headline1Bg",

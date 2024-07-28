d9c6c6095f3d6ee6aa790c826ccbc78756dfef33
Linkarzu <linkarzu@gmail.com>
2024-07-28 06:01:14 -0600

    neovim mason disabled tailwindcss-language-server temporarily

--- neovim/neobean/lua/plugins/mason-nvim.lua
+++ neovim/neobean/lua/plugins/mason-nvim.lua
@@ -9,7 +9,8 @@ return {
     -- I installed "templ" and other LSPS, these requirements can be found in
     -- the TEMPL docs https://templ.guide/commands-and-tools/ide-support/#neovim--050
     -- The templ command must be in your system path for the LSP to be able to start
-    vim.list_extend(opts.ensure_installed, { "templ", "html-lsp", "htmx-lsp", "tailwindcss-language-server" })
+    -- vim.list_extend(opts.ensure_installed, { "templ", "html-lsp", "htmx-lsp", "tailwindcss-language-server" })
+    vim.list_extend(opts.ensure_installed, { "templ", "html-lsp", "htmx-lsp" })
     -- marksman and markdownlint come by default in the lazyvim config
     --
     -- I installed markdown-toc as I use to to automatically create and upate

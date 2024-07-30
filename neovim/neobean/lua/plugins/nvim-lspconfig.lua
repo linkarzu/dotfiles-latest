96ad1f7c98669504ebd1c25dbd948598194e5571
Linkarzu <linkarzu@gmail.com>
2024-07-30 17:46:50 -0600

    neovim nvim-lspconfig.lua added intelephense as phpactor not working

--- neovim/neobean/lua/plugins/nvim-lspconfig.lua
+++ neovim/neobean/lua/plugins/nvim-lspconfig.lua
@@ -2,16 +2,30 @@
 -- Filename: ~/github/dotfiles-latest/neovim/neobean/lua/plugins/nvim-lspconfig.lua
 --
 -- https://github.com/neovim/nvim-lspconfig
---
--- This disables inlay hints
--- When programming in Go, these made my experience feel like shit, because were
--- very intrusive and I never got used to them.
---
--- Folke has a keymap to toggle inaly hints with <leader>uh
+
+local lsp = "intelephense"
 
 return {
   "neovim/nvim-lspconfig",
   opts = {
+
+    -- This disables inlay hints
+    -- When programming in Go, these made my experience feel like shit, because were
+    -- very intrusive and I never got used to them.
+    --
+    -- Folke has a keymap to toggle inaly hints with <leader>uh
     inlay_hints = { enabled = false },
+
+    servers = {
+      -- phpactor = {
+      --   enabled = lsp == "phpactor",
+      -- },
+      intelephense = {
+        enabled = lsp == "intelephense",
+      },
+      [lsp] = {
+        enabled = true,
+      },
+    },
   },
 }

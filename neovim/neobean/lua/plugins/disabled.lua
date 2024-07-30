84a2235098652eb2df3bdf753277a1767f062cf7
Linkarzu <linkarzu@gmail.com>
2024-07-30 17:38:54 -0600

    neovim disabled.lua added filename on top

--- neovim/neobean/lua/plugins/disabled.lua
+++ neovim/neobean/lua/plugins/disabled.lua
@@ -1,3 +1,6 @@
+-- Filename: ~/github/dotfiles-latest/neovim/neobean/lua/plugins/disabled.lua
+-- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/disabled.lua
+
 return {
   -- If your search ends with an 'a' you enter insert mode and start modifying shit in the file, it sucks
   { "folke/flash.nvim", enabled = false },

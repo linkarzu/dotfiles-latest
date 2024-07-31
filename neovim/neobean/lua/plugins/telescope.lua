5a863ef48f58e780ec64bc09201f7d05b027e61b
Linkarzu <linkarzu@gmail.com>
2024-07-31 12:49:02 -0600

    neovim telescope.lua added find_command to sort by modification date (not working)

--- neovim/neobean/lua/plugins/telescope.lua
+++ neovim/neobean/lua/plugins/telescope.lua
@@ -9,6 +9,16 @@ return {
     opts = function()
       return {
         defaults = {
+          -- I'm adding this `find_command` based on this reddit discussion
+          -- https://www.reddit.com/r/neovim/comments/1egczrs/comment/lfsotjx/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+          -- However, it doesn't work, also tried without the setup function
+          require("telescope").setup({
+            pickers = {
+              find_files = {
+                find_command = { "rg", "--files", "--sortr=modified" },
+              },
+            },
+          }),
           -- When I search for stuff in telescope, I want the path to be shown
           -- first, this helps in files that are very deep in the tree and I
           -- cannot see their name.

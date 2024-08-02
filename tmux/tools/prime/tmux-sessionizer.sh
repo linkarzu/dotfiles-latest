0e9b3e5e68d454f390b736b31e877fcc31b73009
Linkarzu <linkarzu@gmail.com>
2024-08-02 05:33:07 -0600

    tmux tmux-sessionizer added /mnt dir for /containerdata nfs volume

--- tmux/tools/prime/tmux-sessionizer.sh
+++ tmux/tools/prime/tmux-sessionizer.sh
@@ -9,31 +9,31 @@
 
 # Check if one argument is being provided
 if [[ $# -eq 1 ]]; then
-	# Use the provided argument as the selected directory
-	selected=$1
+  # Use the provided argument as the selected directory
+  selected=$1
 elif [[ $# -eq 0 ]]; then
-	# Explicitly specified 0 in case someone decides to pass more than 1 argument
-	# Use 'find' to list directories in specified paths and 'fzf' for interactive selection
-	selected=$(find ~/github "$HOME/Library/Mobile Documents/com~apple~CloudDocs/github" -mindepth 1 -maxdepth 1 -type d | fzf)
-	# Prime's example below
-	# selected=$(find ~/work/builds ~/projects ~/ ~/work ~/personal ~/personal/yt -mindepth 1 -maxdepth 1 -type d | fzf)
+  # Explicitly specified 0 in case someone decides to pass more than 1 argument
+  # Use 'find' to list directories in specified paths and 'fzf' for interactive selection
+  selected=$(find ~/github "$HOME/Library/Mobile Documents/com~apple~CloudDocs/github" "/System/Volumes/Data/mnt" -mindepth 1 -maxdepth 1 -type d | fzf)
+  # Prime's example below
+  # selected=$(find ~/work/builds ~/projects ~/ ~/work ~/personal ~/personal/yt -mindepth 1 -maxdepth 1 -type d | fzf)
 
-	# Debugging, uncomment below if you need to see what's being selected
-	# tmux display-message -d 10000 "Directory selected via fzf: $selected"
+  # Debugging, uncomment below if you need to see what's being selected
+  # tmux display-message -d 10000 "Directory selected via fzf: $selected"
 else
-	# This will hopefully catch your attention
-	tmux display-message -d 500 "This script expects zero or one argument."
-	sleep 1
-	tmux display-message -d 500 "This script expects zero or one argument."
-	sleep 1
-	tmux display-message -d 5000 "This script expects zero or one argument."
+  # This will hopefully catch your attention
+  tmux display-message -d 500 "This script expects zero or one argument."
+  sleep 1
+  tmux display-message -d 500 "This script expects zero or one argument."
+  sleep 1
+  tmux display-message -d 5000 "This script expects zero or one argument."
 fi
 
 # Exit the script if no directory is selected
 if [[ -z $selected ]]; then
-	# Debugging
-	# tmux display-message -d 5000 "No directory selected. Exiting."
-	exit 0
+  # Debugging
+  # tmux display-message -d 5000 "No directory selected. Exiting."
+  exit 0
 fi
 
 # replace '.' and '-' with '_'
@@ -46,19 +46,19 @@ selected_after_tr=$(basename "$selected" | tr '.-' '__')
 # This file adds a letter at the end of my session to remind me of the karabiner shortcut
 mappings_file="$HOME/github/dotfiles-latest/tmux/tools/prime/karabiner-mappings.sh"
 if [ -f "$mappings_file" ]; then
-	source "$mappings_file"
-	base_selected=$(basename "$selected_after_tr")
-	current_username=$(whoami)
-	if [[ "$base_selected" == "$current_username" ]]; then
-		# If selected_after_tr equals current username, append "-h"
-		selected_name="${base_selected}-${username_suffix}"
-	else
-		# Get the value of the variable whose name matches base_selected
-		mapping_value="${!base_selected}"
-		selected_name="${base_selected}-${mapping_value}"
-	fi
+  source "$mappings_file"
+  base_selected=$(basename "$selected_after_tr")
+  current_username=$(whoami)
+  if [[ "$base_selected" == "$current_username" ]]; then
+    # If selected_after_tr equals current username, append "-h"
+    selected_name="${base_selected}-${username_suffix}"
+  else
+    # Get the value of the variable whose name matches base_selected
+    mapping_value="${!base_selected}"
+    selected_name="${base_selected}-${mapping_value}"
+  fi
 else
-	selected_name=$selected_after_tr
+  selected_name=$selected_after_tr
 fi
 
 # # Debug line to see what session name is being generated
@@ -83,9 +83,9 @@ fi
 
 # If a tmux session with the desired name does not already exist, create it in detached mode
 if ! tmux has-session -t=$selected_name 2>/dev/null; then
-	# I included quotes in "$selected" because wasn't changing to dirs that have a space
-	# Like the iCloud dir
-	tmux new-session -ds $selected_name -c "$selected"
+  # I included quotes in "$selected" because wasn't changing to dirs that have a space
+  # Like the iCloud dir
+  tmux new-session -ds $selected_name -c "$selected"
 fi
 
 # Switch to the tmux session with the name derived from the selected directory

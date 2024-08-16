6b4c54f13d87d977ba211e5f3e334f037e8e976a
Linkarzu <linkarzu@gmail.com>
2024-08-16 07:40:37 -0600

    neovim disabled.lua added akinsho/bufferline.nvim as I don't need tabs

--- neovim/neobean/lua/plugins/disabled.lua
+++ neovim/neobean/lua/plugins/disabled.lua
@@ -4,4 +4,6 @@
 return {
   -- If your search ends with an 'a' you enter insert mode and start modifying shit in the file, it sucks
   { "folke/flash.nvim", enabled = false },
+  -- This is the plugin that shows tabs, I don't need it as I use BufExplorer and snipe
+  { "akinsho/bufferline.nvim", enabled = false },
 }

64707e247c71a2536327e33effab57641c54d001
Linkarzu <linkarzu@gmail.com>
2024-08-17 05:11:01 -0600

    neovim mini-files.lua updated comments on how to get to the deleted data dir

--- neovim/neobean/lua/plugins/mini-files.lua
+++ neovim/neobean/lua/plugins/mini-files.lua
@@ -16,14 +16,19 @@ return {
       close = "q",
       -- Use this if you want to open several files
       go_in = "l",
-      -- This opens the file, but quits out of mini.files
+      -- This opens the file, but quits out of mini.files (default L)
       go_in_plus = "<CR>",
-      -- I swapped the following 2 to test
+      -- I swapped the following 2 (default go_out: h)
+      -- go_out_plus: when you go out, it shows you only 1 item to the right
+      -- go_out: shows you all the items to the right
       go_out = "H",
       go_out_plus = "h",
+      -- Default <BS>
       reset = ",",
+      -- Default @
       reveal_cwd = ".",
       show_help = "g?",
+      -- Default =
       synchronize = "s",
       trim_left = "<",
       trim_right = ">",
@@ -37,8 +42,9 @@ return {
       -- Whether to use for editing directories
       -- Disabled by default in LazyVim because neo-tree is used for that
       use_as_default_explorer = true,
-      -- If set to false, files are moved to
-      -- ~/.local/share/nvim/mini.files/trash
+      -- If set to false, files are moved to the trash directory
+      -- To get this dir run :echo stdpath('data')
+      -- ~/.local/share/neobean/mini.files/trash
       permanent_delete = false,
     },
   },

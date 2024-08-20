735159787a08497caf8ffc946ced38893d385361
Linkarzu <linkarzu@gmail.com>
2024-08-20 11:22:11 -0600

    neobean render-markdown.lua move highlight creation out of opts as suggested by maintainer

--- neovim/neobean/lua/plugins/render-markdown.lua
+++ neovim/neobean/lua/plugins/render-markdown.lua
@@ -10,41 +10,45 @@
 -- so the inly way is the only way I was able to make it work was loading it
 -- after the config.lazy in the init.lua file lamw25wmal
 
--- Define color variables
--- These are the colors for the eldritch colorscheme
-local color1_bg = "#f265b5"
-local color2_bg = "#37f499"
-local color3_bg = "#04d1f9"
-local color4_bg = "#a48cf2"
-local color5_bg = "#f1fc79"
-local color6_bg = "#f7c67f"
-local color_fg = "#323449"
--- local color_sign = "#ebfafa"
-
 return {
   "MeanderingProgrammer/render-markdown.nvim",
+  -- Moved highlight creation out of opts as suggested by plugin maintainer
+  -- There was no issue, but it was creating unnecessary noise when ran
+  -- :checkhealth render-markdown
+  -- https://github.com/MeanderingProgrammer/render-markdown.nvim/issues/138#issuecomment-2295422741
+  init = function()
+    -- Define color variables
+    -- These are the colors for the eldritch colorscheme
+    local color1_bg = "#f265b5"
+    local color2_bg = "#37f499"
+    local color3_bg = "#04d1f9"
+    local color4_bg = "#a48cf2"
+    local color5_bg = "#f1fc79"
+    local color6_bg = "#f7c67f"
+    local color_fg = "#323449"
+    -- local color_sign = "#ebfafa"
+
+    -- Heading colors (when not hovered over), extends through the entire line
+    vim.cmd(string.format([[highlight Headline1Bg guifg=%s guibg=%s]], color_fg, color1_bg))
+    vim.cmd(string.format([[highlight Headline2Bg guifg=%s guibg=%s]], color_fg, color2_bg))
+    vim.cmd(string.format([[highlight Headline3Bg guifg=%s guibg=%s]], color_fg, color3_bg))
+    vim.cmd(string.format([[highlight Headline4Bg guifg=%s guibg=%s]], color_fg, color4_bg))
+    vim.cmd(string.format([[highlight Headline5Bg guifg=%s guibg=%s]], color_fg, color5_bg))
+    vim.cmd(string.format([[highlight Headline6Bg guifg=%s guibg=%s]], color_fg, color6_bg))
+
+    -- Highlight for the heading and sign icons (symbol on the left)
+    -- I have the sign disabled for now, so this makes no effect
+    vim.cmd(string.format([[highlight Headline1Fg cterm=bold gui=bold guifg=%s]], color1_bg))
+    vim.cmd(string.format([[highlight Headline2Fg cterm=bold gui=bold guifg=%s]], color2_bg))
+    vim.cmd(string.format([[highlight Headline3Fg cterm=bold gui=bold guifg=%s]], color3_bg))
+    vim.cmd(string.format([[highlight Headline4Fg cterm=bold gui=bold guifg=%s]], color4_bg))
+    vim.cmd(string.format([[highlight Headline5Fg cterm=bold gui=bold guifg=%s]], color5_bg))
+    vim.cmd(string.format([[highlight Headline6Fg cterm=bold gui=bold guifg=%s]], color6_bg))
+  end,
   opts = {
     heading = {
       sign = false,
       icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
-
-      -- Heading colors (when not hovered over), extends through the entire line
-      vim.cmd(string.format([[highlight Headline1Bg guifg=%s guibg=%s]], color_fg, color1_bg)),
-      vim.cmd(string.format([[highlight Headline2Bg guifg=%s guibg=%s]], color_fg, color2_bg)),
-      vim.cmd(string.format([[highlight Headline3Bg guifg=%s guibg=%s]], color_fg, color3_bg)),
-      vim.cmd(string.format([[highlight Headline4Bg guifg=%s guibg=%s]], color_fg, color4_bg)),
-      vim.cmd(string.format([[highlight Headline5Bg guifg=%s guibg=%s]], color_fg, color5_bg)),
-      vim.cmd(string.format([[highlight Headline6Bg guifg=%s guibg=%s]], color_fg, color6_bg)),
-
-      -- Highlight for the heading and sign icons (symbol on the left)
-      -- I have the sign disabled for now, so this makes no effect
-      vim.cmd(string.format([[highlight Headline1Fg cterm=bold gui=bold guifg=%s]], color1_bg)),
-      vim.cmd(string.format([[highlight Headline2Fg cterm=bold gui=bold guifg=%s]], color2_bg)),
-      vim.cmd(string.format([[highlight Headline3Fg cterm=bold gui=bold guifg=%s]], color3_bg)),
-      vim.cmd(string.format([[highlight Headline4Fg cterm=bold gui=bold guifg=%s]], color4_bg)),
-      vim.cmd(string.format([[highlight Headline5Fg cterm=bold gui=bold guifg=%s]], color5_bg)),
-      vim.cmd(string.format([[highlight Headline6Fg cterm=bold gui=bold guifg=%s]], color6_bg)),
-
       backgrounds = {
         "Headline1Bg",
         "Headline2Bg",

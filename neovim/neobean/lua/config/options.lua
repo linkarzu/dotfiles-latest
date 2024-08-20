a987b6566a90e7ea33d3cb048e0bb5e49aa3e871
Linkarzu <linkarzu@gmail.com>
2024-08-20 11:16:33 -0600

    neovim options.lua winbar show amount of buffers open

--- neovim/neobean/lua/config/options.lua
+++ neovim/neobean/lua/config/options.lua
@@ -21,10 +21,30 @@ local function get_winbar_path()
   local full_path = vim.fn.expand("%:p")
   return full_path:gsub(vim.fn.expand("$HOME"), "~")
 end
+-- Function to get the number of open buffers using the :ls command
+local function get_buffer_count()
+  local buffers = vim.fn.execute("ls")
+  local count = 0
+  -- Match only lines that represent buffers, typically starting with a number followed by a space
+  for line in string.gmatch(buffers, "[^\r\n]+") do
+    if string.match(line, "^%s*%d+") then
+      count = count + 1
+    end
+  end
+  return count
+end
 -- Function to update the winbar
 local function update_winbar()
   local home_replaced = get_winbar_path()
-  vim.opt.winbar = "%#WinBar1#%m " .. home_replaced .. "%*%=%#WinBar2#" .. vim.fn.systemlist("hostname")[1]
+  local buffer_count = get_buffer_count()
+  vim.opt.winbar = "%#WinBar1#%m "
+    .. "%#WinBar2#("
+    .. buffer_count
+    .. ") "
+    .. "%#WinBar1#"
+    .. home_replaced
+    .. "%*%=%#WinBar2#"
+    .. vim.fn.systemlist("hostname")[1]
 end
 -- Autocmd to update the winbar on BufEnter and WinEnter events
 vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter" }, {

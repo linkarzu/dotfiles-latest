cbcc071db5710ed929612d3be0f6e5912269180d
Linkarzu <linkarzu@gmail.com>
2024-09-04 14:08:29 -0600

    karabiner rules.ts restart yabai with hyper+u+o

--- karabiner/mxstbr/rules.ts
+++ karabiner/mxstbr/rules.ts
@@ -587,9 +587,17 @@ const rules: KarabinerRules[] = [
         ],
       },
       // Restart yabai
-      0: open(
-        "btt://execute_assigned_actions_for_trigger/?uuid=5EC0D2D3-869C-4284-B063-B53A17BF7C4C"
-      ),
+      // 0: open(
+      //   "btt://execute_assigned_actions_for_trigger/?uuid=5EC0D2D3-869C-4284-B063-B53A17BF7C4C"
+      // ),
+      // Restart yabai
+      o: {
+        to: [
+          {
+            shell_command: `/opt/homebrew/bin/yabai --restart-service`,
+          },
+        ],
+      },
     },
 
     // For betterTouchTool

d4063ec28d63999d66ff7c1d145a9e5729dd4d51
Linkarzu <linkarzu@gmail.com>
2024-09-21 06:54:12 -0600

    sketchybar generate colors.sh dynamically from colorscheme

--- sketchybar/felixkratz-linkarzu/colors.sh
+++ sketchybar/felixkratz-linkarzu/colors.sh
@@ -2,6 +2,9 @@
 
 # Filename: ~/github/dotfiles-latest/sketchybar/felixkratz/colors.sh
 
+# Source the colorscheme file
+source "$HOME/github/dotfiles-latest/colorscheme/active/active-colorscheme.sh"
+
 ### Sonokai
 # export BLACK=0xff181819
 # export WHITE=0xffe2e2e3
@@ -37,32 +40,58 @@
 #export BG1=0x603c3e4f
 #export BG2=0x60494d64
 
-# Eldritch Theme
-# https://github.com/eldritch-theme
-export BLACK=0xff181926
-export WHITE=0xffebfafa
-export RED=0xfff16c75
-export GREEN=0xff37f499
-export BLUE=0xff04d1f9
-export YELLOW=0xfff1fc79
-export ORANGE=0xfff7c67f
-export MAGENTA=0xffa48cf2
-export GREY=0xff323449
+# # Eldritch Theme
+# # https://github.com/eldritch-theme
+# export BLACK=0xff181926
+# export WHITE=0xffebfafa
+# export RED=0xfff16c75
+# export GREEN=0xff37f499
+# export BLUE=0xff04d1f9
+# export YELLOW=0xfff1fc79
+# export ORANGE=0xfff7c67f
+# export MAGENTA=0xffa48cf2
+# export GREY=0xff323449
+# export TRANSPARENT=0x00000000
+# export BG0=0xff1e1e2e
+# export BG0O50=0x801e1e2e
+# export BG0O60=0x991e1e2e
+# export BG0O70=0xB21e1e2e
+# export BG0O80=0xCC1e1e2e
+# # export BG0O85=0xD91e1e2e
+# # export BG0O85=0xD9212337
+# # This sets the color of the bar
+# # Eldritch dark
+# export BG0O85=0xCF0D1116
+# # Eldritch light
+# # export BG0O85=0xCF212337
+# export BG1=0x603c3e4f
+# export BG2=0x60494d64
+
+# Linkarzu Theme
+export BLACK=0xff${linkarzu_color10#\#}
+export WHITE=0xff${linkarzu_color14#\#}
+export RED=0xff${linkarzu_color08#\#}
+export GREEN=0xff${linkarzu_color02#\#}
+export BLUE=0xff${linkarzu_color03#\#}
+export YELLOW=0xff${linkarzu_color05#\#}
+export ORANGE=0xff${linkarzu_color06#\#}
+export MAGENTA=0xff${linkarzu_color04#\#}
+export GREY=0xff${linkarzu_color09#\#}
 export TRANSPARENT=0x00000000
-export BG0=0xff1e1e2e
-export BG0O50=0x801e1e2e
-export BG0O60=0x991e1e2e
-export BG0O70=0xB21e1e2e
-export BG0O80=0xCC1e1e2e
+export BG0=0xff${linkarzu_color10#\#}
+export BG0O50=0x80${linkarzu_color10#\#}
+export BG0O60=0x99${linkarzu_color10#\#}
+export BG0O70=0xb2${linkarzu_color10#\#}
+export BG0O80=0xcc${linkarzu_color10#\#}
 # export BG0O85=0xD91e1e2e
 # export BG0O85=0xD9212337
 # This sets the color of the bar
 # Eldritch dark
-export BG0O85=0xCF0D1116
+export BG0O85=0xcf${linkarzu_color10#\#}
 # Eldritch light
 # export BG0O85=0xCF212337
-export BG1=0x603c3e4f
-export BG2=0x60494d64
+export BG1=0x60${linkarzu_color13#\#}
+export BG2=0x60${linkarzu_color07#\#}
 
 # General bar colors
 export BAR_COLOR=$BG0O85

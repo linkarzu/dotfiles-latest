dfb79c38a1f1e23090c22f48c10a9981ec56c012
Linkarzu <linkarzu@gmail.com>
2024-09-21 07:08:06 -0600

    colorscheme changed active theme

--- colorscheme/active/active-colorscheme.sh
+++ colorscheme/active/active-colorscheme.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-# Filename: ~/github/dotfiles-latest/colorscheme/list/linkarzu-colors.sh
-# ~/github/dotfiles-latest/colorscheme/list/linkarzu-colors.sh
+# Filename: ~/github/dotfiles-latest/colorscheme/list/eldritch-colors.sh
+# ~/github/dotfiles-latest/colorscheme/list/eldritch-colors.sh
 
-linkarzu_color01="#949ae5"
+linkarzu_color01="#f265b5"
 linkarzu_color02="#37f499"
 linkarzu_color03="#04d1f9"
-linkarzu_color04="#987afb"
-linkarzu_color05="#19dfcf"
-linkarzu_color06="#12f24e"
+linkarzu_color04="#a48cf2"
+linkarzu_color05="#f1fc79"
+linkarzu_color06="#f7c67f"
 # Markdown codeblock
-linkarzu_color07="#002931"
-linkarzu_color08="#5fa9f4"
+linkarzu_color07="#314154"
+linkarzu_color08="#f16c75"
 # Comments
 linkarzu_color09="#a5afc2"
 # Terminal and neovim background
-linkarzu_color10="#0D1116"
+linkarzu_color10="#212337"
 # Underline spellbad
 linkarzu_color11="#f16c75"
 # Underline spellcap
@@ -27,4 +27,4 @@ linkarzu_color14="#ebfafa"
 # Tmux inactive windows
 linkarzu_color15="#013e4a"
 # Selected text
-linkarzu_color16="#e9b3fd"
+linkarzu_color16="#d99ffd"

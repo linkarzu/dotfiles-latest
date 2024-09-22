3aaa2d53c435fe96aabcc5ebe2a67f0d0e4e0d44
Linkarzu <linkarzu@gmail.com>
2024-09-21 20:18:39 -0600

    colorscheme catppuccin-mocha initial commit

---
+++ colorscheme/list/catppuccin-mocha.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Filename: ~/github/dotfiles-latest/colorscheme/list/catppuccin-mocha.sh
+# ~/github/dotfiles-latest/colorscheme/list/catppuccin-mocha.sh
+
+# Maroon
+linkarzu_color01="#eba0ac"
+# Blue
+linkarzu_color02="#89b4fa"
+# Green
+linkarzu_color03="#a6e3a1"
+# Mauve
+linkarzu_color04="#cba6f7"
+# Lavender
+linkarzu_color05="#b4befe"
+# Sapphire
+linkarzu_color06="#74c7ec"
+# Surface 0
+# Markdown codeblock
+linkarzu_color07="#313244"
+# Peach
+linkarzu_color08="#fab387"
+# Subtext0
+# Comments
+linkarzu_color09="#a6adc8"
+# Base
+# Terminal and neovim background
+linkarzu_color10="#1e1e2e"
+# Red
+# Underline spellbad
+linkarzu_color11="#f38ba8"
+# Yellow
+# Underline spellcap
+linkarzu_color12="#f9e2af"
+# Surface2
+# line across cursor
+linkarzu_color13="#585b70"
+# Text
+# Cursor and tmux windows text
+linkarzu_color14="#cdd6f4"
+# Surface1
+# Tmux inactive windows
+linkarzu_color15="#45475a"
+# Flamingo
+# Selected text
+linkarzu_color16="#f2cdcd"
+# Lualine across
+linkarzu_color17="#313244"

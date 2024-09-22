5fac151bb6a9fef330f2aac89e9d3286aa66870e
Linkarzu <linkarzu@gmail.com>
2024-09-21 20:18:23 -0600

    colorscheme catppuccin-macchiato initial commit

---
+++ colorscheme/list/catppuccin-macchiato.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Filename: ~/github/dotfiles-latest/colorscheme/list/catppuccin-macchiato.sh
+# ~/github/dotfiles-latest/colorscheme/list/catppuccin-macchiato.sh
+
+# Maroon
+linkarzu_color01="#ee99a0"
+# Blue
+linkarzu_color02="#8aadf4"
+# Green
+linkarzu_color03="#a6da95"
+# Mauve
+linkarzu_color04="#c6a0f6"
+# Lavender
+linkarzu_color05="#b7bdf8"
+# Sapphire
+linkarzu_color06="#7dc4e4"
+# Surface 0
+# Markdown codeblock, lualine color across
+linkarzu_color07="#363a4f"
+# Peach
+linkarzu_color08="#f5a97f"
+# Subtext0
+# Comments
+linkarzu_color09="#a5adcb"
+# Base
+# Terminal and neovim background
+linkarzu_color10="#24273a"
+# Red
+# Underline spellbad
+linkarzu_color11="#ed8796"
+# Yellow
+# Underline spellcap
+linkarzu_color12="#eed49f"
+# Surface2
+# line across cursor
+linkarzu_color13="#5b6078"
+# Text
+# Cursor and tmux windows text
+linkarzu_color14="#cad3f5"
+# Surface1
+# Tmux inactive windows
+linkarzu_color15="#494d64"
+# Flamingo
+# Selected text
+linkarzu_color16="#f0c6c6"
+# Lualine across
+linkarzu_color17="#363a4f"

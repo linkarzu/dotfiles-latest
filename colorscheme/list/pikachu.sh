155483087b1ad9e66f116bbadd3f18b95dc13899
Linkarzu <linkarzu@gmail.com>
2024-09-27 12:39:48 -0600

    colorscheme list added pikachu.sh

---
+++ colorscheme/list/pikachu.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Filename: ~/github/dotfiles-latest/colorscheme/list/linkarzu-colors.sh
+# ~/github/dotfiles-latest/colorscheme/list/linkarzu-colors.sh
+
+linkarzu_color01="#fbe260"
+linkarzu_color02="#fad61d"
+linkarzu_color03="#fdf2bb"
+linkarzu_color04="#e19720"
+linkarzu_color05="#ebfafa"
+linkarzu_color06="#f62d14"
+# Markdown codeblock
+linkarzu_color07="#1c242f"
+linkarzu_color08="#811e09"
+# Comments
+linkarzu_color09="#a5afc2"
+# Terminal and neovim background
+linkarzu_color10="#0D1116"
+# Underline spellbad
+linkarzu_color11="#f62d14"
+# Underline spellcap
+linkarzu_color12="#fad61d"
+# line across cursor
+linkarzu_color13="#314154"
+# Cursor and tmux windows text
+linkarzu_color14="#ebfafa"
+# Tmux inactive windows
+linkarzu_color15="#013e4a"
+# Selected text
+linkarzu_color16="#e9b3fd"
+# Lualine across
+linkarzu_color17="#141b22"

7a788b255f8de1edd58954812597671d1edfacb7
Linkarzu <linkarzu@gmail.com>
2024-10-05 06:30:40 -0600

    zshrc zshrc-file.sh install kitty.terminfo if not present

--- zshrc/zshrc-file.sh
+++ zshrc/zshrc-file.sh
@@ -252,6 +252,31 @@ esac
 # macOS-specific configurations
 if [ "$OS" = 'Mac' ]; then
 
+  install_xterm_kitty_terminfo() {
+    # Attempt to get terminfo for xterm-kitty
+    if ! infocmp xterm-kitty &>/dev/null; then
+      echo "xterm-kitty terminfo not found. Installing..."
+      # Create a temp file
+      tempfile=$(mktemp)
+      # Download the kitty.terminfo file
+      # https://github.com/kovidgoyal/kitty/blob/master/terminfo/kitty.terminfo
+      if curl -o "$tempfile" https://raw.githubusercontent.com/kovidgoyal/kitty/master/terminfo/kitty.terminfo; then
+        echo "Downloaded kitty.terminfo successfully."
+        # Compile and install the terminfo entry for my current user
+        if tic -x -o ~/.terminfo "$tempfile"; then
+          echo "xterm-kitty terminfo installed successfully."
+        else
+          echo "Failed to compile and install xterm-kitty terminfo."
+        fi
+      else
+        echo "Failed to download kitty.terminfo."
+      fi
+      # Remove the temporary file
+      rm "$tempfile"
+    fi
+  }
+  install_xterm_kitty_terminfo
+
   #############################################################################
   #                        Cursor configuration
   #############################################################################

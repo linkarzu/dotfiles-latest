f42a305dd1d09fcda1ec0d24eaa1e8c23cec5a00
Linkarzu <linkarzu@gmail.com>
2024-10-08 07:45:49 -0600

    neobean bullets.lua added options to plugin

--- neovim/neobean/lua/plugins/bullets.lua
+++ neovim/neobean/lua/plugins/bullets.lua
@@ -14,4 +14,13 @@
 
 return {
   "bullets-vim/bullets.vim",
+  config = function()
+    -- Disable deleting the last empty bullet when pressing <cr> or 'o'
+    -- default = 1
+    vim.g.bullets_delete_last_bullet_if_empty = 1
+
+    -- (Optional) Add other configurations here
+    -- For example, enabling/disabling mappings
+    -- vim.g.bullets_set_mappings = 1
+  end,
 }

9b2c0114fbb9b73ea8dda94dfc16bdeeda35abe8
Linkarzu <linkarzu@gmail.com>
2024-10-28 14:54:33 -0600

    neobean ghostty.lua adds syntax highlighting for the ghostty config file

---
+++ neovim/neobean/lua/plugins/ghostty.lua
@@ -0,0 +1,10 @@
+-- Provides syntax highlighting for the ghostty config file
+-- No additional dependencies required, Zilchmasta was kind enough to let me
+-- know about this in discord
+-- https://discord.com/channels/1005603569187160125/1300462095946485790/1300534513788653630
+
+return {
+  "ghostty",
+  dir = "/Applications/Ghostty.app/Contents/Resources/vim/vimfiles/",
+  lazy = false,
+}

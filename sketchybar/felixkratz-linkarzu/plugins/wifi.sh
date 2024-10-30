807c848526f879a517c64bd40277554d436601ce
Linkarzu <linkarzu@gmail.com>
2024-10-30 17:02:25 -0600

    sketchybar wifi.sh set icon to ethernet or wifi depending on what I'm using to go out
    This helps me understand if there's a problem with ethernet, as it's
    what I always want to use, and only use wifi as a backup

--- sketchybar/felixkratz-linkarzu/plugins/wifi.sh
+++ sketchybar/felixkratz-linkarzu/plugins/wifi.sh
@@ -3,32 +3,40 @@
 # Filename: ~/github/dotfiles-latest/sketchybar/felixkratz-linkarzu/plugins/wifi.sh
 
 update() {
-	source "$CONFIG_DIR/icons.sh"
-	SSID="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F ' SSID: ' '/ SSID: / {print $2}')"
-	IP="$(ipconfig getifaddr en0)"
+  source "$CONFIG_DIR/icons.sh"
+  SSID="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F ' SSID: ' '/ SSID: / {print $2}')"
+  INTERFACE="$(route get default | grep interface | awk '{print $2}')"
 
-	ICON="$([ -n "$IP" ] && echo "$WIFI_CONNECTED" || echo "$WIFI_DISCONNECTED")"
-	LABEL="$([ -n "$IP" ] && echo "$SSID ($IP)" || echo "Disconnected")"
+  # Adjust IP assignment based on the active interface
+  if [ "$INTERFACE" = "en0" ]; then
+    IP="$(ipconfig getifaddr en0)"
+    ICON="$ETHERNET_CONNECTED"
+  else
+    IP="$(ipconfig getifaddr en1)" # Assuming en1 is WiFi
+    ICON="$([ -n "$IP" ] && echo "$WIFI_CONNECTED" || echo "$WIFI_DISCONNECTED")"
+  fi
 
-	sketchybar --set $NAME icon="$ICON" label="$LABEL"
+  LABEL="$([ -n "$IP" ] && echo "$SSID ($IP)" || echo "Disconnected")"
+
+  sketchybar --set $NAME icon="$ICON" label="$LABEL"
 }
 
 click() {
-	CURRENT_WIDTH="$(sketchybar --query $NAME | jq -r .label.width)"
+  CURRENT_WIDTH="$(sketchybar --query $NAME | jq -r .label.width)"
 
-	WIDTH=0
-	if [ "$CURRENT_WIDTH" -eq "0" ]; then
-		WIDTH=dynamic
-	fi
+  WIDTH=0
+  if [ "$CURRENT_WIDTH" -eq "0" ]; then
+    WIDTH=dynamic
+  fi
 
-	sketchybar --animate sin 20 --set $NAME label.width="$WIDTH"
+  sketchybar --animate sin 20 --set $NAME label.width="$WIDTH"
 }
 
 case "$SENDER" in
 "wifi_change")
-	update
-	;;
+  update
+  ;;
 "mouse.clicked")
-	click
-	;;
+  click
+  ;;
 esac

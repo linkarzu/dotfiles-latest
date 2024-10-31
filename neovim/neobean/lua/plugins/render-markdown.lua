1b9fb9aa99592d35768e49894172a0fd97100084
Linkarzu <linkarzu@gmail.com>
2024-10-30 21:16:22 -0600

    neobean render-markdown.lua aligned indentation on multiline tasks or checkboxes

--- neovim/neobean/lua/plugins/render-markdown.lua
+++ neovim/neobean/lua/plugins/render-markdown.lua
@@ -52,6 +52,30 @@ return {
       -- Turn on / off list bullet rendering
       enabled = true,
     },
+    checkbox = {
+      -- Turn on / off checkbox state rendering
+      enabled = true,
+      -- Determines how icons fill the available space:
+      --  inline:  underlying text is concealed resulting in a left aligned icon
+      --  overlay: result is left padded with spaces to hide any additional text
+      position = "inline",
+      unchecked = {
+        -- Replaces '[ ]' of 'task_list_marker_unchecked'
+        icon = "   󰄱 ",
+        -- Highlight for the unchecked icon
+        highlight = "RenderMarkdownUnchecked",
+        -- Highlight for item associated with unchecked checkbox
+        scope_highlight = nil,
+      },
+      checked = {
+        -- Replaces '[x]' of 'task_list_marker_checked'
+        icon = "   󰱒 ",
+        -- Highlight for the checked icon
+        highlight = "RenderMarkdownChecked",
+        -- Highlight for item associated with checked checkbox
+        scope_highlight = nil,
+      },
+    },
     heading = {
       sign = false,
       icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },

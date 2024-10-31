d14dcdff9d346994e4d6bd660a3956d5eb0bf16e
linkarzu <test@test.com>
2024-10-31 14:05:47 -0600

    scripts 300-dailyNote.sh set neovim to start with the NVIM_APPNAME='neobean' command
    Before doing this, the correct neobean version was started, but not sure
    why it always had updates to install, even though my main neobean
    outside had no updates. After this change, this was fixed

--- scripts/macos/mac/300-dailyNote.sh
+++ scripts/macos/mac/300-dailyNote.sh
@@ -57,7 +57,7 @@ if ! tmux has-session -t="$tmux_session_name" 2>/dev/null; then
   # Create a new tmux session with the note name in detached mode and start
   # neovim with the daily note, cursor at the last line
   # + tells neovim to execute a command after opening and G goes to last line
-  tmux new-session -d -s "$tmux_session_name" -c "$note_dir" "nvim +norm\ G $full_path"
+  tmux new-session -d -s "$tmux_session_name" -c "$note_dir" "export NVIM_APPNAME='neobean' && /opt/homebrew/bin/nvim +norm\ Go +startinsert $full_path"
   # tmux new-session -d -s "$tmux_session_name" "nvim +norm\ G $full_path"
 
   # Create a new tmux session with the note name in detached mode and start neovim with the daily note

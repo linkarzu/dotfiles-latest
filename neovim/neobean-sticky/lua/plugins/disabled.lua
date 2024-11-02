4625e14ed8f002715c302d7e4ffdf9438d49d71e
Linkarzu <linkarzu@gmail.com>
2024-11-02 06:07:40 -0600

    neobean-sticky enabled noice, otherwise I have to press enter when notifications show up

--- neovim/neobean-sticky/lua/plugins/disabled.lua
+++ neovim/neobean-sticky/lua/plugins/disabled.lua
@@ -15,11 +15,12 @@ local is_neovide = vim.g.neovide or false
 return {
   -- If your search ends with an 'a' you enter insert mode and start modifying shit in the file, it sucks
   { "folke/flash.nvim", enabled = false },
-  { "folke/noice.nvim", enabled = false },
+  -- I do want to have noice enabled, otherwise the notifications don't go away
+  -- until I press enter
+  -- { "folke/noice.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
   { "EdenEast/nightfox.nvim", enabled = false },
   { "ellisonleao/gruvbox.nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "nvim-lualine/lualine.nvim", enabled = false },
   -- This is the plugin that shows tabs, I don't need it as I use BufExplorer and snipe
---
+++ neovim/neobean-sticky/lua/plugins/noice.lua
@@ -0,0 +1,62 @@
+-- Filename: /home/krishna/github/dotfiles-latest/nvim-lazyvim/lua/plugins/noice.lua
+-- I want to change the default notifications to be less obtrussive (if that's even a word)
+-- https://github.com/folke/noice.nvim
+
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      presets = {
+        -- This is the search bar or popup that shows up when you press /
+        -- Setting this to false makes it a popup and true the search bar at the bottom
+        bottom_search = false,
+      },
+      messages = {
+        -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+        -- This is a current Neovim limitation.
+        enabled = true, -- enables the Noice messages UI
+        view = "mini", -- default view for messages
+        view_error = "mini", -- view for errors
+        view_warn = "mini", -- view for warnings
+        view_history = "mini", -- view for :messages
+        view_search = "mini", -- view for search count messages. Set to `false` to disable
+      },
+      notify = {
+        -- Noice can be used as `vim.notify` so you can route any notification like other messages
+        -- Notification messages have their level and other properties set.
+        -- event is always "notify" and kind can be any log level as a string
+        -- The default routes will forward notifications to nvim-notify
+        -- Benefit of using Noice for this is the routing and consistent history view
+        enabled = true,
+        view = "mini",
+      },
+      lsp = {
+        message = {
+          -- Messages shown by lsp servers
+          enabled = true,
+          view = "mini",
+        },
+      },
+      views = {
+        -- This sets the position for the search popup that shows up with / or with :
+        cmdline_popup = {
+          position = {
+            row = "40%",
+            col = "50%",
+          },
+        },
+        mini = {
+          timeout = 1500, -- timeout in milliseconds
+          align = "center",
+          position = {
+            -- Centers messages top to bottom
+            row = "95%",
+            -- Aligns messages to the far right
+            col = "100%",
+          },
+        },
+      },
+    },
+  },
+}

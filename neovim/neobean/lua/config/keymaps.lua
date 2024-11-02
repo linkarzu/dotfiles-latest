c8d283c785ca87b93f574fa1fe323558f78f4f6b
Linkarzu <linkarzu@gmail.com>
2024-11-02 05:07:15 -0600

    neobean keymaps.lua added gss in normal mode to inline the current word

--- neovim/neobean/lua/config/keymaps.lua
+++ neovim/neobean/lua/config/keymaps.lua
@@ -919,6 +919,56 @@ vim.keymap.set("n", "<leader>msr", function()
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(":spellr\n", true, false, true), "m", true)
 end, { desc = "[P]Spelling repeat" })
 
+-- Surround the http:// url that the cursor is currently in with ``
+vim.keymap.set("n", "gsu", function()
+  local line = vim.api.nvim_get_current_line()
+  local col = vim.api.nvim_win_get_cursor(0)[2] + 1 -- Adjust for 0-index in Lua
+  -- This makes the `s` optional so it matches both http and https
+  local pattern = "https?://[^ ,;'\"<>%s)]*"
+  -- Find the starting and ending positions of the URL
+  local s, e = string.find(line, pattern)
+  while s and e do
+    if s <= col and e >= col then
+      -- When the cursor is within the URL
+      local url = string.sub(line, s, e)
+      -- Update the line with backticks around the URL
+      local new_line = string.sub(line, 1, s - 1) .. "`" .. url .. "`" .. string.sub(line, e + 1)
+      vim.api.nvim_set_current_line(new_line)
+      vim.cmd("silent write")
+      return
+    end
+    -- Find the next URL in the line
+    s, e = string.find(line, pattern, e + 1)
+    -- Save the file to update trouble list
+  end
+  print("No URL found under cursor")
+end, { desc = "[P]Add surrounding to URL" })
+
+-- Remap 'gss' to 'gsa`' in visual mode
+-- This surrounds with inline code, that I use a lot lamw25wmal
+vim.keymap.set("v", "gss", function()
+  -- Use nvim_replace_termcodes to handle special characters like backticks
+  local keys = vim.api.nvim_replace_termcodes("gsa`", true, false, true)
+  -- Feed the keys in visual mode ('x' for visual mode)
+  vim.api.nvim_feedkeys(keys, "x", false)
+  -- I tried these 3, but they didn't work, I assume because of the backtick character
+  -- vim.cmd("normal! gsa`")
+  -- vim.cmd([[normal! gsa`]])
+  -- vim.cmd("normal! gsa\\`")
+end, { desc = "[P] Surround selection with backticks (inline code)" })
+
+-- This surrounds CURRENT WORD with inline code in NORMAL MODE lamw25wmal
+vim.keymap.set("n", "gss", function()
+  -- Use nvim_replace_termcodes to handle special characters like backticks
+  local keys = vim.api.nvim_replace_termcodes("gsaiw`", true, false, true)
+  -- Feed the keys in visual mode ('x' for visual mode)
+  vim.api.nvim_feedkeys(keys, "x", false)
+  -- I tried these 3, but they didn't work, I assume because of the backtick character
+  -- vim.cmd("normal! gsa`")
+  -- vim.cmd([[normal! gsa`]])
+  -- vim.cmd("normal! gsa\\`")
+end, { desc = "[P] Surround selection with backticks (inline code)" })
+
 -- In visual mode, check if the selected text is already bold and show a message if it is
 -- If not, surround it with double asterisks for bold
 vim.keymap.set("v", "<leader>mb", function()
@@ -1653,44 +1703,6 @@ vim.keymap.set("n", "<leader>fC", function()
   local current_line = vim.api.nvim_get_current_line()
   create_daily_note(current_line)
 end, { desc = "[P]Create daily note" })
-
--- Remap 'gss' to 'gsa`' in visual mode
--- This surrounds with inline code, that I use a lot lamw25wmal
-vim.keymap.set("v", "gss", function()
-  -- Use nvim_replace_termcodes to handle special characters like backticks
-  local keys = vim.api.nvim_replace_termcodes("gsa`", true, false, true)
-  -- Feed the keys in visual mode ('x' for visual mode)
-  vim.api.nvim_feedkeys(keys, "x", false)
-  -- I tried these 3, but they didn't work, I assume because of the backtick character
-  -- vim.cmd("normal! gsa`")
-  -- vim.cmd([[normal! gsa`]])
-  -- vim.cmd("normal! gsa\\`")
-end, { desc = "[P] Surround selection with backticks (inline code)" })
-
--- Surround the http:// url that the cursor is currently in with ``
-vim.keymap.set("n", "gsu", function()
-  local line = vim.api.nvim_get_current_line()
-  local col = vim.api.nvim_win_get_cursor(0)[2] + 1 -- Adjust for 0-index in Lua
-  -- This makes the `s` optional so it matches both http and https
-  local pattern = "https?://[^ ,;'\"<>%s)]*"
-  -- Find the starting and ending positions of the URL
-  local s, e = string.find(line, pattern)
-  while s and e do
-    if s <= col and e >= col then
-      -- When the cursor is within the URL
-      local url = string.sub(line, s, e)
-      -- Update the line with backticks around the URL
-      local new_line = string.sub(line, 1, s - 1) .. "`" .. url .. "`" .. string.sub(line, e + 1)
-      vim.api.nvim_set_current_line(new_line)
-      vim.cmd("silent write")
-      return
-    end
-    -- Find the next URL in the line
-    s, e = string.find(line, pattern, e + 1)
-    -- Save the file to update trouble list
-  end
-  print("No URL found under cursor")
-end, { desc = "[P]Add surrounding to URL" })
 
 -- - I have several `.md` documents that do not follow markdown guidelines
 -- - There are some old ones that have more than one H1 heading in them, so when I

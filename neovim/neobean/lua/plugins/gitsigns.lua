f250f158f83fed220cfa438f701dfe6eba2b4790
Linkarzu <linkarzu@gmail.com>
2024-11-02 18:56:45 -0600

    neobean gitsigns.lua added wrapping to ghd for diffs

---
+++ neovim/neobean/lua/plugins/gitsigns.lua
@@ -0,0 +1,70 @@
+-- Filename: ~/github/dotfiles-latest/neovim/neobean/lua/plugins/gitsigns.lua
+-- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/gitsigns.lua
+
+-- Plugin that shows git changes in the left sign column
+-- I also use this plugin to run ghd and see the diff in the current buffer, I
+-- like having wrap on the vim splits, so that's why I added folke's config here
+-- https://www.lazyvim.org/plugins/editor#gitsignsnvim
+
+return {
+  "lewis6991/gitsigns.nvim",
+  event = "LazyFile",
+  opts = {
+    signs = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+      untracked = { text = "▎" },
+    },
+    signs_staged = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+    },
+    on_attach = function(buffer)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+      end
+
+      -- stylua: ignore start
+      map("n", "]h", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "]c", bang = true })
+        else
+          gs.nav_hunk("next")
+        end
+      end, "Next Hunk")
+      map("n", "[h", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "[c", bang = true })
+        else
+          gs.nav_hunk("prev")
+        end
+      end, "Prev Hunk")
+      map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
+      map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
+      map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+      map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+      map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+      map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+      map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+      map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
+      map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+      map("n", "<leader>ghB", function() gs.blame() end, "Blame Buffer")
+      -- map("n", "<leader>ghd", gs.diffthis, "Diff This")
+      -- Enable wrap in both diff windows
+      map("n", "<leader>ghd", function()
+        gs.diffthis()
+        vim.cmd("windo set wrap")
+      end, "Diff This")
+      map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
+      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+    end,
+  },
+}

b0d32ee5e332795b9d9d7809ec686f44a1186cff
Linkarzu <linkarzu@gmail.com>
2024-11-02 18:58:42 -0600

    yabai yabai_env.sh make cleanshot always visible and added elmedia player to non managed apps

--- yabai/yabai_env.sh
+++ yabai/yabai_env.sh
@@ -5,10 +5,10 @@
 
 # apps_transparent="(Spotify|kitty|Neovide|Google Chrome|Code|WezTerm|Ghostty)"
 apps_transparent="(Spotify|Neovide|Code|WezTerm|Ghostty)"
-apps_transp_ignore="(kitty|CleanShot X)"
+apps_transp_ignore="(kitty|'CleanShot X')"
 # Apps excluded from window management, so you can resize them and move them around
 # This is basically the ignore list
-apps_mgoff_normal="(Calculator|iStat Menus|Hammerspoon|BetterDisplay|GIMP|Notes|System Preferences|System Settings|Activity Monitor|App StoreSoftware Update|CleanShot X|Karabiner-Elements|TestRig|Gemini|Raycast|OBS Studio|QuickTime Player|Cisco Packet Tracer 8.2.2|Cisco Packet Tracer|Stickies|kitty)"
+apps_mgoff_normal="(Calculator|iStat Menus|Hammerspoon|BetterDisplay|GIMP|Notes|System Preferences|System Settings|Activity Monitor|App StoreSoftware Update|CleanShot X|Karabiner-Elements|TestRig|Gemini|Raycast|OBS Studio|QuickTime Player|Cisco Packet Tracer 8.2.2|Cisco Packet Tracer|Stickies|kitty|Elmedia Player)"
 # This keeps apps always below
 apps_mgoff_below="()"
 # This keeps apps always on the top

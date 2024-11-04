1050fbb45478ac28477deca90f93c40e8c91c90d
Linkarzu <linkarzu@gmail.com>
2024-11-04 08:23:54 -0600

    neobean autocmds.lua quit some menus with <esc> instead of q

--- neovim/neobean/lua/config/autocmds.lua
+++ neovim/neobean/lua/config/autocmds.lua
@@ -6,6 +6,10 @@
 
 -- config/autocmds.lua
 
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
 -- This is for dadbod-ui auto completion
 -- https://github.com/kristijanhusak/vim-dadbod-completion/issues/53#issuecomment-1705335855
 local cmp = require("cmp")
@@ -22,4 +26,40 @@ vim.api.nvim_create_autocmd("FileType", {
     })
   end,
   group = autocomplete_group,
+})
+
+-- close some filetypes with <esc>
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("close_with_q"),
+  pattern = {
+    "PlenaryTestPopup",
+    "grug-far",
+    "help",
+    "lspinfo",
+    "notify",
+    "qf",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "neotest-output",
+    "checkhealth",
+    "neotest-summary",
+    "neotest-output-panel",
+    "dbout",
+    "gitsigns-blame",
+    "Lazy",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.schedule(function()
+      vim.keymap.set("n", "<esc>", function()
+        vim.cmd("close")
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = "Quit buffer",
+      })
+    end)
+  end,
 })

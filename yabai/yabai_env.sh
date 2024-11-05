c34081d21231a0c9227b948a82911c24702ca2cd
Linkarzu <linkarzu@gmail.com>
2024-11-05 07:38:40 -0600

    yabai yabai_env.sh set unmanaged apps from normal to below

--- yabai/yabai_env.sh
+++ yabai/yabai_env.sh
@@ -3,13 +3,25 @@
 # Filename: ~/github/dotfiles-latest/yabai/yabai_env.sh
 # ~/github/dotfiles-latest/yabai/yabai_env.sh
 
+# To get the names of all the running applications
+# yabai -m query --windows | jq -r '.[].app'
+
 # apps_transparent="(Spotify|kitty|Neovide|Google Chrome|Code|WezTerm|Ghostty)"
 apps_transparent="(Spotify|Neovide|Code|WezTerm|Ghostty)"
-apps_transp_ignore="(kitty|'CleanShot X')"
+
+# Apps that I want to always show, even when I have a transparent app focused
+apps_transp_ignore="(kitty|CleanShot X)"
+
 # Apps excluded from window management, so you can resize them and move them around
 # This is basically the ignore list
-apps_mgoff_normal="(Calculator|iStat Menus|Hammerspoon|BetterDisplay|GIMP|Notes|System Preferences|System Settings|Activity Monitor|App StoreSoftware Update|CleanShot X|Karabiner-Elements|TestRig|Gemini|Raycast|OBS Studio|QuickTime Player|Cisco Packet Tracer 8.2.2|Cisco Packet Tracer|Stickies|kitty|Elmedia Player)"
-# This keeps apps always below
-apps_mgoff_below="()"
+
+# I had to move them away from normal, because all these apps would stay on top
+# of other apps
+# apps_mgoff_normal="()"
+
+# This keeps apps always below, seems to be working fine when I switch to other
+# apps
+apps_mgoff_below="(Calculator|iStat Menus|Hammerspoon|BetterDisplay|GIMP|Notes|System Preferences|System Settings|Activity Monitor|App StoreSoftware Update|CleanShot X|Karabiner-Elements|TestRig|Gemini|Raycast|OBS Studio|QuickTime Player|Cisco Packet Tracer|Stickies|kitty|Elmedia Player|ProLevel|Resolve|DaVinci Resolve)"
+
 # This keeps apps always on the top
 apps_mgoff_above="()"

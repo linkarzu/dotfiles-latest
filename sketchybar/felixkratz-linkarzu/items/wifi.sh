b080593e9143e7cb133bedce828eb2706cdaf283
Linkarzu <linkarzu@gmail.com>
2024-11-05 18:53:05 -0600

    sketchybar volume.sh wifi.sh fixed paddings

--- sketchybar/felixkratz-linkarzu/items/wifi.sh
+++ sketchybar/felixkratz-linkarzu/items/wifi.sh
@@ -5,12 +5,13 @@
 source "$CONFIG_DIR/icons.sh"
 
 wifi=(
-	padding_right=4
-	label.width=0
-	icon="$WIFI_DISCONNECTED"
-	script="$PLUGIN_DIR/wifi.sh"
+  padding_right=6
+  padding_left=1
+  label.width=0
+  icon="$WIFI_DISCONNECTED"
+  script="$PLUGIN_DIR/wifi.sh"
 )
 
 sketchybar --add item wifi right \
-	--set wifi "${wifi[@]}" \
-	--subscribe wifi wifi_change mouse.clicked
+  --set wifi "${wifi[@]}" \
+  --subscribe wifi wifi_change mouse.clicked

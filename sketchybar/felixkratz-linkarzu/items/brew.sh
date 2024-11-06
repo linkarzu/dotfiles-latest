6eede302b3413ec1800ecac3b63ca3692a5137a5
Linkarzu <linkarzu@gmail.com>
2024-11-05 18:38:25 -0600

    sketchybar brew.sh set update frequency to 1 min

--- sketchybar/felixkratz-linkarzu/items/brew.sh
+++ sketchybar/felixkratz-linkarzu/items/brew.sh
@@ -6,14 +6,15 @@
 # e.g. via function in .zshrc
 
 brew=(
-	icon=􀐛
-	label=?
-	padding_right=12
-	label.padding_left=3
-	script="$PLUGIN_DIR/brew.sh"
+  icon=􀐛
+  label=?
+  update_freq=60
+  padding_right=12
+  label.padding_left=3
+  script="$PLUGIN_DIR/brew.sh"
 )
 
 sketchybar --add event brew_update \
-	--add item brew right \
-	--set brew "${brew[@]}" \
-	--subscribe brew brew_update
+  --add item brew right \
+  --set brew "${brew[@]}" \
+  --subscribe brew brew_update

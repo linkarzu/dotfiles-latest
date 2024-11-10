dd2c6da898ead632b395365c56e96b29c4833efe
Linkarzu <linkarzu@gmail.com>
2024-11-10 05:17:10 -0600

    scripts 200-micMute.sh if volume gt 0 set it to 0, else to 60

--- scripts/macos/mac/200-micMute.sh
+++ scripts/macos/mac/200-micMute.sh
@@ -5,8 +5,8 @@ MIC_VOLUME=$(osascript -e 'input volume of (get volume settings)')
 
 # If volume is less than 60 (including 0), set it to 60
 # Otherwise set it to 0
-if [[ $MIC_VOLUME -lt 60 ]]; then
-  osascript -e 'set volume input volume 60'
-else
+if [[ $MIC_VOLUME -gt 0 ]]; then
   osascript -e 'set volume input volume 0'
+else
+  osascript -e 'set volume input volume 60'
 fi

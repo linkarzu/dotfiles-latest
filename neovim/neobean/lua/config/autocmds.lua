c714d4fbacb175a129952715a5bd18b6a8ccc773
Linkarzu <linkarzu@gmail.com>
2024-11-15 14:18:14 -0600

    neobean autocmds.lua tried adding debounce delay but not detected when switching vim splits

--- neovim/neobean/lua/config/autocmds.lua
+++ neovim/neobean/lua/config/autocmds.lua
@@ -89,3 +89,56 @@ vim.api.nvim_create_autocmd({ "FocusGained", "FocusLost", "WinEnter", "WinLeave"
     end
   end,
 })
+
+-- -- Debounce wrapper using vim.defer_fn
+-- -- This debounce prevents to see the color switch when switching betweeen 2
+-- -- buffers, it doesn't kick in if I switch between 2 different panes until I
+-- -- switch to another tmux session
+-- local function debounce(func, timeout)
+--   local timer = nil
+--   return function(...)
+--     local args = { ... }
+--     if timer then
+--       -- Cancel any existing timer
+--       timer:close()
+--       timer = nil
+--     end
+--     timer = vim.defer_fn(function()
+--       func(unpack(args))
+--       timer = nil
+--     end, timeout)
+--   end
+-- end
+-- -- Callback function to handle background color change
+-- local function update_background(ev)
+--   local active_bg = colors.linkarzu_color10 -- darker background
+--   local inactive_bg = colors.linkarzu_color07 -- brighter background
+--   if
+--     ev.event == "FocusGained"
+--     or ev.event == "WinEnter"
+--     or ev.event == "BufEnter"
+--     or ev.event == "TabEnter"
+--     or ev.event == "BufWinEnter"
+--   then
+--     -- Active window or tab - darker background
+--     vim.cmd("hi Normal guibg=" .. active_bg)
+--     vim.cmd("hi NormalFloat guibg=" .. active_bg)
+--     vim.cmd("hi TreesitterContext guibg=" .. active_bg)
+--     vim.cmd("hi TreesitterContextLineNumber guibg=" .. active_bg)
+--   else
+--     -- Inactive window or tab - brighter background
+--     vim.cmd("hi Normal guibg=" .. inactive_bg)
+--     vim.cmd("hi NormalNC guibg=" .. inactive_bg)
+--     vim.cmd("hi NormalFloat guibg=" .. inactive_bg)
+--     vim.cmd("hi NormalFloatNC guibg=" .. inactive_bg)
+--     vim.cmd("hi TreesitterContext guibg=" .. inactive_bg)
+--     vim.cmd("hi TreesitterContextLineNumber guibg=" .. inactive_bg)
+--   end
+-- end
+-- -- Create autocmd with debounce
+-- vim.api.nvim_create_autocmd(
+--   { "FocusGained", "FocusLost", "WinEnter", "WinLeave", "BufEnter", "BufLeave", "TabEnter", "TabLeave", "BufWinEnter" },
+--   {
+--     callback = debounce(update_background, 500), -- 500 ms debounce delay
+--   }
+-- )

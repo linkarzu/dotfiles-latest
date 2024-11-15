5359e675e27cbae425d622c9c3a7db1538c36efc
Linkarzu <linkarzu@gmail.com>
2024-11-15 07:11:20 -0600

    tmux tmux.conf.sh updated pane-focus-out to color25

--- tmux/tmux.conf.sh
+++ tmux/tmux.conf.sh
@@ -85,8 +85,9 @@ set-option -g focus-events on
 
 # Change color of focused and unfocused panes, it helps me easily identify where
 # my cursor is
+# Make sure these colors also match your neovim autocmd if you change them
 set-hook -g pane-focus-in 'select-pane -P "bg=$linkarzu_color10,fg=white"'
-set-hook -g pane-focus-out 'select-pane -P "bg=$linkarzu_color13,fg=default"'
+set-hook -g pane-focus-out 'select-pane -P "bg=$linkarzu_color25,fg=default"'
 
 # This changes the colors of visual selection and choose-tree
 # https://unix.stackexchange.com/questions/141311/tmux-hightlight-colour-setting

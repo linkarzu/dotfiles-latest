b457bc67d4ba3d06c7cf01957ef8ab7ff3acd9b2
Linkarzu <linkarzu@gmail.com>
2024-11-16 20:19:15 -0600

    neobean mini-files respect lazyvim merging rules

--- neovim/neobean/lua/plugins/mini-files.lua
+++ neovim/neobean/lua/plugins/mini-files.lua
@@ -8,11 +8,11 @@
 
 return {
   "echasnovski/mini.files",
-  opts = {
+  opts = function(_, opts)
     -- I didn't like the default mappings, so I modified them
     -- Module mappings created only inside explorer.
     -- Use `''` (empty string) to not create one.
-    mappings = {
+    opts.mappings = vim.tbl_deep_extend("force", opts.mappings or {}, {
       close = "<esc>",
       -- Use this if you want to open several files
       go_in = "l",
@@ -32,13 +32,15 @@ return {
       synchronize = "s",
       trim_left = "<",
       trim_right = ">",
-    },
-    windows = {
+    })
+
+    opts.windows = vim.tbl_deep_extend("force", opts.windows or {}, {
       preview = true,
       width_focus = 30,
       width_preview = 50,
-    },
-    options = {
+    })
+
+    opts.options = vim.tbl_deep_extend("force", opts.options or {}, {
       -- Whether to use for editing directories
       -- Disabled by default in LazyVim because neo-tree is used for that
       use_as_default_explorer = true,
@@ -46,8 +48,10 @@ return {
       -- To get this dir run :echo stdpath('data')
       -- ~/.local/share/neobean/mini.files/trash
       permanent_delete = false,
-    },
-  },
+    })
+    return opts
+  end,
+
   keys = {
     {
       "<leader>e",
@@ -64,86 +68,9 @@ return {
       desc = "Open mini.files (cwd)",
     },
   },
+
   config = function(_, opts)
     require("mini.files").setup(opts)
-
-    local show_dotfiles = true
-    local filter_show = function(fs_entry)
-      return true
-    end
-    local filter_hide = function(fs_entry)
-      return not vim.startswith(fs_entry.name, ".")
-    end
-
-    local toggle_dotfiles = function()
-      show_dotfiles = not show_dotfiles
-      local new_filter = show_dotfiles and filter_show or filter_hide
-      require("mini.files").refresh({ content = { filter = new_filter } })
-    end
-
-    local map_split = function(buf_id, lhs, direction, close_on_file)
-      local rhs = function()
-        local new_target_window
-        local cur_target_window = require("mini.files").get_target_window()
-        if cur_target_window ~= nil then
-          vim.api.nvim_win_call(cur_target_window, function()
-            vim.cmd("belowright " .. direction .. " split")
-            new_target_window = vim.api.nvim_get_current_win()
-          end)
-
-          require("mini.files").set_target_window(new_target_window)
-          require("mini.files").go_in({ close_on_file = close_on_file })
-        end
-      end
-
-      local desc = "Open in " .. direction .. " split"
-      if close_on_file then
-        desc = desc .. " and close"
-      end
-      vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
-    end
-
-    local files_set_cwd = function()
-      local cur_entry_path = MiniFiles.get_fs_entry().path
-      local cur_directory = vim.fs.dirname(cur_entry_path)
-      if cur_directory ~= nil then
-        vim.fn.chdir(cur_directory)
-      end
-    end
-
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "MiniFilesBufferCreate",
-      callback = function(args)
-        local buf_id = args.data.buf_id
-
-        vim.keymap.set(
-          "n",
-          opts.mappings and opts.mappings.toggle_hidden or "g.",
-          toggle_dotfiles,
-          { buffer = buf_id, desc = "Toggle hidden files" }
-        )
-
-        vim.keymap.set(
-          "n",
-          opts.mappings and opts.mappings.change_cwd or "gc",
-          files_set_cwd,
-          { buffer = args.data.buf_id, desc = "Set cwd" }
-        )
-
-        map_split(buf_id, opts.mappings and opts.mappings.go_in_horizontal or "<C-w>s", "horizontal", false)
-        map_split(buf_id, opts.mappings and opts.mappings.go_in_vertical or "<C-w>v", "vertical", false)
-        map_split(buf_id, opts.mappings and opts.mappings.go_in_horizontal_plus or "<C-w>S", "horizontal", true)
-        map_split(buf_id, opts.mappings and opts.mappings.go_in_vertical_plus or "<C-w>V", "vertical", true)
-      end,
-    })
-
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "MiniFilesActionRename",
-      callback = function(event)
-        LazyVim.lsp.on_rename(event.data.from, event.data.to)
-      end,
-    })
-
     ---------------------------------------------------------------------------
     ---------------------------------------------------------------------------
 

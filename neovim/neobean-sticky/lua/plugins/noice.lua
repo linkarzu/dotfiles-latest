f75b9e122f07b8a6542c602eef3e7ed3872751f8
Linkarzu <linkarzu@gmail.com>
2024-11-22 06:24:18 -0600

    neobean-sticky noice.lua increated timeout from 1500 to 5000

--- neovim/neobean-sticky/lua/plugins/noice.lua
+++ neovim/neobean-sticky/lua/plugins/noice.lua
@@ -47,7 +47,7 @@ return {
           },
         },
         mini = {
-          timeout = 1500, -- timeout in milliseconds
+          timeout = 5000, -- timeout in milliseconds
           align = "center",
           position = {
             -- Centers messages top to bottom

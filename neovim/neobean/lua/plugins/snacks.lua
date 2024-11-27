0fa2bc50cb9846e6f7ea09e09126557e2621f55d
Linkarzu <linkarzu@gmail.com>
2024-11-27 06:03:50 -0600

    neobean snacks.lua changed dashboard back to neobean

--- neovim/neobean/lua/plugins/snacks.lua
+++ neovim/neobean/lua/plugins/snacks.lua
@@ -19,6 +19,35 @@ return {
         enabled = true,
         top_down = false, -- place notifications from top to bottom
       },
+      dashboard = {
+        preset = {
+          keys = {
+            -- { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            -- { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            -- { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            -- { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            -- {
+            --   icon = " ",
+            --   key = "c",
+            --   desc = "Config",
+            --   action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+            -- },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            -- { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+            { icon = " ", key = "<esc>", desc = "Quit", action = ":qa" },
+          },
+          header = [[
+███╗   ██╗███████╗ ██████╗ ██████╗ ███████╗ █████╗ ███╗   ██╗
+████╗  ██║██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔══██╗████╗  ██║
+██╔██╗ ██║█████╗  ██║   ██║██████╔╝█████╗  ███████║██╔██╗ ██║
+██║╚██╗██║██╔══╝  ██║   ██║██╔══██╗██╔══╝  ██╔══██║██║╚██╗██║
+██║ ╚████║███████╗╚██████╔╝██████╔╝███████╗██║  ██║██║ ╚████║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝
+
+[Linkarzu.com]
+        ]],
+        },
+      },
     },
   },
 }

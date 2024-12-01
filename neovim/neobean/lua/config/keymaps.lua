34884491c3580dd1bbb48f4fc54bdc3700218939
Linkarzu <linkarzu@gmail.com>
2024-12-01 06:34:52 -0600

    neobean keymaps.lua open daily note with <leader>fd instead of <leader>gd

--- neovim/neobean/lua/config/keymaps.lua
+++ neovim/neobean/lua/config/keymaps.lua
@@ -2260,7 +2260,7 @@ local function switch_to_daily_note(date_line)
 end
 
 -- Keymap to switch to the daily note or create it if it does not exist
-vim.keymap.set("n", "<leader>gd", function()
+vim.keymap.set("n", "<leader>fd", function()
   local current_line = vim.api.nvim_get_current_line()
   local date_line = current_line:match("%[%[%d+%-%d+%-%d+%-%w+%]%]") or ("[[" .. os.date("%Y-%m-%d-%A") .. "]]")
   switch_to_daily_note(date_line)

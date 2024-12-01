781bc2bc749bc3024cb0fc49a4bf9d0ac566f304
Linkarzu <linkarzu@gmail.com>
2024-12-01 16:12:38 -0600

    neobean added char mode comments, its on by default

--- neovim/neobean/lua/plugins/flash.lua
+++ neovim/neobean/lua/plugins/flash.lua
@@ -13,5 +13,18 @@ return {
       -- doesn't exit
       mode = "search",
     },
+    modes = {
+      char = {
+        -- f, t, F, T motions:
+        -- After typing f{char} or F{char}, you can repeat the motion with f or go to the previous match with F to undo a jump.
+        -- Similarly, after typing t{char} or T{char}, you can repeat the motion with t or go to the previous match with T.
+        -- You can also go to the next match with ; or previous match with ,
+        -- Any highlights clear automatically when moving, changing buffers, or pressing <esc>.
+        --
+        -- Useful if you do `vtf` or `vff` and then keep pressing f to jump to
+        -- the next `f`s
+        enabled = true,
+      },
+    },
   },
 }

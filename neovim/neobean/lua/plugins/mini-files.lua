aef3eee705a72635685e78d41a48ce416fb239a9
Linkarzu <linkarzu@gmail.com>
2024-12-05 14:03:45 -0600

    neobean mini-files.lua fix <leader>e wasn't opening cwd when file does not exist

--- neovim/neobean/lua/plugins/mini-files.lua
+++ neovim/neobean/lua/plugins/mini-files.lua
@@ -84,9 +84,15 @@ return {
       "<leader>e",
       function()
         local buf_name = vim.api.nvim_buf_get_name(0)
-        if vim.fn.filereadable(buf_name) == 1 or vim.fn.isdirectory(vim.fn.fnamemodify(buf_name, ":p:h")) == 1 then
+        local dir_name = vim.fn.fnamemodify(buf_name, ":p:h")
+        if vim.fn.filereadable(buf_name) == 1 then
+          -- Pass the full file path to highlight the file
           require("mini.files").open(buf_name, true)
+        elseif vim.fn.isdirectory(dir_name) == 1 then
+          -- If the directory exists but the file doesn't, open the directory
+          require("mini.files").open(dir_name, true)
         else
+          -- If neither exists, fallback to the current working directory
           require("mini.files").open(vim.uv.cwd(), true)
         end
       end,

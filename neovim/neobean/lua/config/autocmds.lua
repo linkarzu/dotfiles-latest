00b354d4f21ca590ad063a51a5b89a17eedf9095
Linkarzu <linkarzu@gmail.com>
2024-12-10 06:03:17 -0600

    neobean autocmds.lua set vim.opt_local.wrap = true and now decide that in the options.lua file

--- neovim/neobean/lua/config/autocmds.lua
+++ neovim/neobean/lua/config/autocmds.lua
@@ -176,3 +176,16 @@ vim.api.nvim_create_autocmd({ "WinEnter", "WinLeave" }, {
 vim.api.nvim_create_autocmd({ "FocusGained", "FocusLost" }, {
   callback = debounced_update_background,
 })
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    -- -- By default wrap is set to true regardless of what I chose in my options.lua file,
+    -- -- This sets wrapping for my skitty-notes and I don't want to have
+    -- -- wrapping there, I wanto to decide this in the options.lua file
+    -- vim.opt_local.wrap = false
+    vim.opt_local.spell = true
+  end,
+})

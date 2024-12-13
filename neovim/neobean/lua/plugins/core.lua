b6123168ebfa508b6250a18b7dafb76c516775e0
Linkarzu <linkarzu@gmail.com>
2024-12-13 13:01:20 -0600

    neobean core.lua version 14.x introduces breaking changes, pinning to 13.9.1

---
+++ neovim/neobean/lua/plugins/core.lua
@@ -0,0 +1,9 @@
+return {
+  -- Version 14.x introduces several breaking changes, and I'm not ready for
+  -- them, so pinning to 13.9.1 for now
+  -- https://www.lazyvim.org/news
+  --
+  -- To see the releases, go here
+  -- https://github.com/LazyVim/LazyVim/releases
+  { "LazyVim/LazyVim", version = "v13.9.1" },
+}

5fbad2f4bceb9c84f9008bb48dfa0763e3ea76b1
Linkarzu <linkarzu@gmail.com>
2024-12-16 07:07:05 -0600

    neobean vim-tmux-navigator.lua switch from neovim to tmux panes also when in insert and visual mode

--- neovim/neobean/lua/plugins/vim-tmux-navigator.lua
+++ neovim/neobean/lua/plugins/vim-tmux-navigator.lua
@@ -10,19 +10,14 @@
 return {
   {
     "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-    },
     keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+      -- Switch to other tmux panes not only when in normal mode, but also
+      -- insert and visual mode
+      { "<C-h>", '<cmd>TmuxNavigateLeft("n")<cr>', mode = { "n", "v", "i" }, desc = "TmuxNavigateLeft" },
+      { "<C-j>", "<cmd>TmuxNavigateDown<cr>", mode = { "n", "v", "i" }, desc = "TmuxNavigateDown" },
+      { "<C-k>", "<cmd>TmuxNavigateUp<cr>", mode = { "n", "v", "i" }, desc = "TmuxNavigateUp" },
+      { "<C-l>", "<cmd>TmuxNavigateRight<cr>", mode = { "n", "v", "i" }, desc = "TmuxNavigateRight" },
+      { "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", mode = { "n", "v", "i" }, desc = "TmuxNavigatePrevious" },
     },
   },
 }

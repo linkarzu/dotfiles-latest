232e33233223bddfbe39dfc5dc473ba15f62ab75
Linkarzu <linkarzu@gmail.com>
2024-12-19 06:37:58 -0600

    neobean disabled.lua disable image.nvim for neovide

--- neovim/neobean/lua/plugins/disabled.lua
+++ neovim/neobean/lua/plugins/disabled.lua
@@ -1,22 +1,27 @@
 -- Filename: ~/github/dotfiles-latest/neovim/neobean/lua/plugins/disabled.lua
 -- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/disabled.lua
 
--- return {
---   -- If your search ends with an 'a' you enter insert mode and start modifying shit in the file, it sucks
---   { "folke/flash.nvim", enabled = false },
---   -- This is the plugin that shows tabs, I don't need it as I use BufExplorer and snipe
---   { "akinsho/bufferline.nvim", enabled = false },
--- }
-
 -- is_neovide is set to "true" if running inside neovide because vim.g.neovide is set to true
 -- Otherwise, default to "false"
 local is_neovide = vim.g.neovide or false
 
 return {
-  -- If your learch ends with an 'a' you enter insert mode and start modifying shit in the file, it sucks
+  -- -- If your search ends with an 'a' you enter insert mode and start modifying shit in the file, it sucks
+  -- -- I'm giving flash a 2nd chance, I use my j and k too much
   -- { "folke/flash.nvim", enabled = false },
+
   -- This is the plugin that shows tabs, I don't need it as I use BufExplorer and snipe
   -- I enable the plugin only if the is_neovide variable is set to true
   { "akinsho/bufferline.nvim", enabled = is_neovide },
   { "Rics-Dev/project-explorer.nvim", enabled = is_neovide },
+
+  -- -- Disable '3rd/image.nvim' if running Neovide, or you will get the error:
+  -- -- Failed to run `config` for image.nvim
+  -- -- .../lazy/image.nvim/lua/image/utils/term.lua:34: Failed to get terminal size
+  {
+    "3rd/image.nvim",
+    cond = function()
+      return not (vim.g.neovide == true)
+    end,
+  },
 }

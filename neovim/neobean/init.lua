30426b94e769cd5cc671d3d573d17c8b88207db6
Linkarzu <linkarzu@gmail.com>
2024-12-28 07:04:54 -0600

    neobean init.lua added delay for skitty configuration

--- neovim/neobean/init.lua
+++ neovim/neobean/init.lua
@@ -17,3 +17,12 @@ require("config.lazy")
 -- Require the colors.lua module and access the colors directly without
 -- additional file reads
 require("config.highlights")
+
+-- Delay for `skitty` configuration
+-- If I don't add this delay, I get the message
+-- "Press ENTER or type command to continue"
+if vim.g.neovim_mode == "skitty" then
+  vim.wait(500, function()
+    return false
+  end) -- Wait for X miliseconds without doing anything
+end

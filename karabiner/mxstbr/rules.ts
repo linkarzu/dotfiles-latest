0247a1d2691356692a8f62dc9e4070a3e593842f
Linkarzu <linkarzu@gmail.com>
2024-12-29 05:26:37 -0600

    karabiner rules.ts added hyper+r+l for "Switch Windows"

--- karabiner/mxstbr/rules.ts
+++ karabiner/mxstbr/rules.ts
@@ -665,7 +665,7 @@ const rules: KarabinerRules[] = [
     r: {
       j: open("raycast://extensions/lardissone/raindrop-io/search"),
       k: open("raycast://extensions/mblode/google-search/index"),
-      // l: open("raycast://extensions/KevinBatdorf/obsidian/searchNoteCommand"),
+      l: open("raycast://extensions/raycast/navigation/switch-windows"),
       semicolon: open("raycast://extensions/vishaltelangre/google-drive/index"),
       quote: open("raycast://extensions/mathieudutour/wolfram-alpha/index"),
       y: open(

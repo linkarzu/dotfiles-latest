7c1a54e98639cfe5ae3d0625dc319e5e8bbbf09f
Linkarzu <linkarzu@gmail.com>
2024-12-30 10:14:01 -0600

    neobean stay-centered.lua toggle with <leader>US as <leader>uS is used by lazyvim

--- neovim/neobean/lua/plugins/stay-centered.lua
+++ neovim/neobean/lua/plugins/stay-centered.lua
@@ -10,7 +10,7 @@ return {
       -- Define the keymap to toggle the stay-centered plugin
       -- I had to move this keymap here inside, otherwise the plugin started
       -- disabled if I set the keymap outside under "keys"
-      vim.keymap.set("n", "<leader>uS", function()
+      vim.keymap.set("n", "<leader>US", function()
         require("stay-centered").toggle()
         vim.notify("Toggled stay-centered", vim.log.levels.INFO)
       end, { desc = "[P]Toggle stay-centered.nvim" })

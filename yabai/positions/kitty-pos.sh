a4b5f5e88defba72cfed01921544dcbd07b05a77
Linkarzu <linkarzu@gmail.com>
2025-01-10 11:13:28 -0600

    skitty made skitty-notes smaller

--- yabai/positions/kitty-pos.sh
+++ yabai/positions/kitty-pos.sh
@@ -17,13 +17,13 @@ export PATH="/opt/homebrew/bin:$PATH"
 display_resolution=$(system_profiler SPDisplaysDataType | grep Resolution)
 # First condition is to match my macbook pro, the * are used as wildcards
 if [[ "$display_resolution" == *"3456 x 2234"* ]]; then
-  yabai -m window --focus "$(yabai -m query --windows | jq '.[] | select(.app == "kitty") | .id')" --move abs:1203:45
-  yabai -m window --focus "$(yabai -m query --windows | jq '.[] | select(.app == "kitty") | .id')" --resize abs:290:400
+  yabai -m window --focus "$(yabai -m query --windows | jq '.[] | select(.app == "kitty") | .id')" --move abs:1,139:41
+  yabai -m window --focus "$(yabai -m query --windows | jq '.[] | select(.app == "kitty") | .id')" --resize abs:231:400
 elif [[ "$display_resolution" == *"1536 x 2048"* ]]; then
   yabai -m window --focus "$(yabai -m query --windows | jq '.[] | select(.app == "kitty") | .id')" --move abs:4:835
   yabai -m window --focus "$(yabai -m query --windows | jq '.[] | select(.app == "kitty") | .id')" --resize abs:290:186
   # Else below will match my 27 inch monitor
 else
-  yabai -m window --focus "$(yabai -m query --windows | jq '.[] | select(.app == "kitty") | .id')" --move abs:1305:43
-  yabai -m window --focus "$(yabai -m query --windows | jq '.[] | select(.app == "kitty") | .id')" --resize abs:290:400
+  yabai -m window --focus "$(yabai -m query --windows | jq '.[] | select(.app == "kitty") | .id')" --move abs:1369:39
+  yabai -m window --focus "$(yabai -m query --windows | jq '.[] | select(.app == "kitty") | .id')" --resize abs:231:400
 fi

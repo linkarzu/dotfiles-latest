18761622ac19fffb1ca43bd8e63409a51b4bfd65
Linkarzu <linkarzu@gmail.com>
2025-01-25 11:43:04 -0600

    neobean conform.lua fixed autocmd, added delay for BufLeave

--- neovim/neobean/lua/plugins/conform.lua
+++ neovim/neobean/lua/plugins/conform.lua
@@ -4,15 +4,32 @@
 -- https://github.com/stevearc/conform.nvim
 
 -- Auto-format when focus is lost or I leave the buffer
--- Useful if on skitty-notes or a regular buffer and I am on a
+-- Useful if on skitty-notes or a regular buffer and switch somewhere else the
+-- formatting doesn't stay all messed up
 -- I found this autocmd example in the readme
 -- https://github.com/stevearc/conform.nvim/blob/master/README.md#setup
--- vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "QuitPre", "VimSuspend" }, {
---   pattern = "*",
---   callback = function(args)
---     require("conform").format({ bufnr = args.buf })
---   end,
--- })
+-- "FocusLost" used when switching from skitty-notes
+-- "BufLeave" is used when switching between 2 buffers
+vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave" }, {
+  pattern = "*",
+  callback = function(args)
+    local buf = args.buf or vim.api.nvim_get_current_buf()
+    -- Only format if the current mode is normal mode
+    -- Only format if autoformat is enabled for the current buffer (if
+    -- autoformat disabled globally the buffers inherits it, see :LazyFormatInfo)
+    if LazyVim.format.enabled(buf) and vim.fn.mode() == "n" then
+      -- Add a small delay to the formatting so it doesn’t interfere with
+      -- CopilotChat’s or grug-far buffer initialization, this helps me to not
+      -- get errors when using the "BufLeave" event above, if not using
+      -- "BufLeave" the delay is not needed
+      vim.defer_fn(function()
+        if vim.api.nvim_buf_is_valid(buf) then
+          require("conform").format({ bufnr = buf })
+        end
+      end, 100)
+    end
+  end,
+})
 
 return {
   "stevearc/conform.nvim",

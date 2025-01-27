59a072bcf1dc2f984a8ef16965bacee1a3c24e8c
Linkarzu <linkarzu@gmail.com>
2025-01-26 20:17:55 -0600

    neobean auto-save.lua added flash events

--- neovim/neobean/lua/plugins/auto-save.lua
+++ neovim/neobean/lua/plugins/auto-save.lua
@@ -28,7 +28,10 @@ vim.api.nvim_create_autocmd("User", {
   end,
 })
 
--- I got this from https://github.com/okuuva/auto-save.nvim/issues/67#issuecomment-2597631756
+-- I do not want to save when I'm in visual mode because I'm usually moving
+-- stuff from one place to another, or deleting it
+-- I got this suggestion from the plugin maintainers
+-- https://github.com/okuuva/auto-save.nvim/issues/67#issuecomment-2597631756
 local visual_event_group = vim.api.nvim_create_augroup("visual_event", { clear = true })
 
 vim.api.nvim_create_autocmd("ModeChanged", {
@@ -49,6 +52,20 @@ vim.api.nvim_create_autocmd("ModeChanged", {
   end,
 })
 
+-- Override the `flash.jump` function to detect start and end
+local flash = require("flash")
+local original_jump = flash.jump
+
+flash.jump = function(opts)
+  vim.api.nvim_exec_autocmds("User", { pattern = "FlashJumpStart" })
+  -- print("flash.nvim enter")
+
+  original_jump(opts)
+
+  vim.api.nvim_exec_autocmds("User", { pattern = "FlashJumpEnd" })
+  -- print("flash.nvim leave")
+end
+
 return {
   {
     "okuuva/auto-save.nvim",
@@ -64,10 +81,17 @@ return {
         -- -- Re-enabling this to only save if NOT in insert mode in the condition below
         -- immediate_save = { nil },
         immediate_save = { "BufLeave", "FocusLost", "QuitPre", "VimSuspend" }, -- vim events that trigger an immediate save
-        defer_save = { "InsertLeave", "TextChanged", { "User", pattern = "VisualLeave" } }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+        -- vim events that trigger a deferred save (saves after `debounce_delay`)
+        defer_save = {
+          "InsertLeave",
+          "TextChanged",
+          { "User", pattern = "VisualLeave" },
+          { "User", pattern = "FlashJumpEnd" },
+        },
         cancel_deferred_save = {
           "InsertEnter",
           { "User", pattern = "VisualEnter" },
+          { "User", pattern = "FlashJumpStart" },
         },
       },
       -- function that takes the buffer handle and determines whether to save the current buffer or not

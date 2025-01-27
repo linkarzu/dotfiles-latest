7763411bd266719f5da2b8fbba86a6b39f92158c
linkarzu <test@test.com>
2025-01-27 07:26:36 -0600

    sketchybar battery.sh added colors

--- sketchybar/felixkratz-linkarzu/plugins/battery.sh
+++ sketchybar/felixkratz-linkarzu/plugins/battery.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+source "$CONFIG_DIR/colors.sh"
 
 BATTERY_INFO="$(pmset -g batt)"
 PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
@@ -8,35 +10,38 @@ CHARGING="$(pmset -g batt | grep 'AC Power')"
 # My mac mini doesn't have a battery, and it was leaving a blank space where the
 # battery was supposed to be
 if ! echo "$BATTERY_INFO" | grep -q "InternalBattery"; then
-	sketchybar --set "$NAME" drawing=off
+  sketchybar --set "$NAME" drawing=off
 else
-	sketchybar --set "$NAME" drawing=on
+  sketchybar --set "$NAME" drawing=on
 fi
 
 if [ "$PERCENTAGE" = "" ]; then
-	exit 0
+  exit 0
 fi
 
 case "${PERCENTAGE}" in
 9[0-9] | 100)
-	ICON=""
-	;;
-[6-8][0-9])
-	ICON=""
-	;;
-[3-5][0-9])
-	ICON=""
-	;;
+  ICON=""
+  ;;
+[3-7][0-9])
+  ICON=""
+  COLOR=$YELLOW
+  ;;
+# [3-5][0-9])
+#   ICON=""
+#   COLOR=$YELLOW
+#   ;;
 [1-2][0-9])
-	ICON=""
-	;;
+  ICON=""
+  COLOR=$RED
+  ;;
 *) ICON="" ;;
 esac
 
 if [[ "$CHARGING" != "" ]]; then
-	ICON=""
+  ICON=""
 fi
 
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
-sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}"
+sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}" icon.color=$COLOR

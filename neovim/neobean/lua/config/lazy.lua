034d824e0285b11fc4839d78105685af04523633
Linkarzu <linkarzu@gmail.com>
2025-01-31 20:35:53 -0600

    neobean removed snacks_picker

--- neovim/neobean/lua/config/lazy.lua
+++ neovim/neobean/lua/config/lazy.lua
@@ -24,7 +24,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.editor.mini-diff" },
-    { import = "lazyvim.plugins.extras.editor.snacks_picker" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },

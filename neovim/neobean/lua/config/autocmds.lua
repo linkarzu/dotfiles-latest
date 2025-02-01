6d644d9601eb51ac1260a1a915cda5b0519f7a18
Linkarzu <linkarzu@gmail.com>
2025-02-01 10:32:40 -0600

    neobean autocmds.lua when open markdown file, fold headings

--- neovim/neobean/lua/config/autocmds.lua
+++ neovim/neobean/lua/config/autocmds.lua
@@ -189,3 +189,26 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = true
   end,
 })
+
+-- When I open markdown files I want to fold the markdown headings
+-- Originally I thought about using it only for skitty-notes, but I think I want
+-- it in all markdown files
+--
+-- if vim.g.neovim_mode == "skitty" then
+vim.api.nvim_create_autocmd("BufRead", {
+  pattern = "markdown",
+  callback = function()
+    -- Avoid running zk multiple times for the same buffer
+    if vim.b.zk_executed then
+      return
+    end
+    vim.b.zk_executed = true -- Mark as executed
+    -- Use `vim.defer_fn` to add a slight delay before executing `zk`
+    vim.defer_fn(function()
+      vim.cmd("normal zk")
+      vim.cmd("silent write")
+      vim.notify("Folded keymaps", vim.log.levels.INFO)
+    end, 100) -- Delay in milliseconds (100ms should be enough)
+  end,
+})
+-- end

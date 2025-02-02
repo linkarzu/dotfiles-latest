24e8c5be7d309660ccebd295854119493714dd26
Linkarzu <linkarzu@gmail.com>
2025-02-01 18:29:50 -0600

    neobean snacks.lua configured scrolling the same way as in lazygit

--- neovim/neobean/lua/plugins/snacks.lua
+++ neovim/neobean/lua/plugins/snacks.lua
@@ -208,6 +208,11 @@ return {
               -- to close the picker on ESC instead of going to normal mode,
               -- add the following keymap to your config
               ["<Esc>"] = { "close", mode = { "n", "i" } },
+              -- I'm used to scrolling like this in LazyGit
+              ["J"] = { "preview_scroll_down", mode = { "i", "n" } },
+              ["K"] = { "preview_scroll_up", mode = { "i", "n" } },
+              ["H"] = { "preview_scroll_left", mode = { "i", "n" } },
+              ["L"] = { "preview_scroll_right", mode = { "i", "n" } },
             },
           },
         },

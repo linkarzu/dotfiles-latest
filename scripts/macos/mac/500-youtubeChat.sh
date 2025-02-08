a2309b2227b425a4efb5b9893bd7b062db9a6592
Linkarzu <linkarzu@gmail.com>
2025-02-08 15:09:27 -0600

    scripts mac 500-youtubeChat.sh ask for stream URL after script is ran

--- scripts/macos/mac/500-youtubeChat.sh
+++ scripts/macos/mac/500-youtubeChat.sh
@@ -3,9 +3,10 @@
 # Filename: ~/github/dotfiles-latest/scripts/macos/mac/500-youtubeChat.sh
 # ~/github/dotfiles-latest/scripts/macos/mac/500-youtubeChat.sh
 
-# Extract the stream ID from the given YouTube URL
-stream_url="$1"
+# Prompt the user for the YouTube Live URL
+read -rp "Enter the YouTube Live URL: " stream_url
 
+# Extract the stream ID from the given YouTube URL
 if [[ ! "$stream_url" =~ youtube\.com/live/([a-zA-Z0-9_-]+) ]]; then
   echo "Invalid YouTube Live URL"
   exit 1

d8edb1a9c3c6426a7cc8cedf8761cb7809200475
Linkarzu <linkarzu@gmail.com>
2025-02-08 15:08:41 -0600

    neobean luasnip.lua added 1password bottom banner

--- neovim/neobean/lua/plugins/luasnip.lua
+++ neovim/neobean/lua/plugins/luasnip.lua
@@ -442,6 +442,12 @@ return {
           "",
           '[![Image](../../assets/img/imgs/250124-1password-banner.avif){: width="300" }](https://www.dpbolvw.net/click-101327218-15917064){:target="_blank"}',
           "",
+          "---",
+          "",
+          '[Start your 14 day FREE trial](https://www.dpbolvw.net/click-101327218-15917064){:target="_blank"}',
+          "",
+          '[![Image](../../assets/img/imgs/250124-1password-banner-bottom.avif){: width="300" }](https://www.dpbolvw.net/click-101327218-15917064){:target="_blank"}',
+          "",
         }),
       })
     )

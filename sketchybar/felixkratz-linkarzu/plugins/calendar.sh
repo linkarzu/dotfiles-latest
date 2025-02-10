d9281a0983d059901a28ca60a088b21253dbad15
Linkarzu <linkarzu@gmail.com>
2025-02-10 11:49:23 -0600

    sketchybar calendar.sh add time to sketchybar

--- sketchybar/felixkratz-linkarzu/plugins/calendar.sh
+++ sketchybar/felixkratz-linkarzu/plugins/calendar.sh
@@ -2,6 +2,9 @@
 
 # If you cant to show the time
 # sketchybar --set $NAME icon="$(date '+%a %d. %b')" label="$(date '+%H:%M')"
+# sketchybar --set $NAME icon="$(date '+%a %d. %b %Y %H:%M')"
+# sketchybar --set $NAME icon="$(date '+%a %d%b%y %H:%M')"
+sketchybar --set $NAME icon="$(date '+%a %y/%m/%d %H:%M')"
 
 # In case you don't want to show the time
-sketchybar --set $NAME icon="$(date '+%a %d. %b %Y')"
+# sketchybar --set $NAME icon="$(date '+%a %d. %b %Y')"

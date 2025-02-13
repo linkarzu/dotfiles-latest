0bc4869ff956de18d26e1f9eb4688b6fe82c53ac
Linkarzu <linkarzu@gmail.com>
2025-02-13 17:03:01 -0600

    colorscheme switched back to linkarzu-colors

--- neovim/neobean/lua/config/active-colorscheme.sh
+++ neovim/neobean/lua/config/active-colorscheme.sh
@@ -14,54 +14,54 @@
 # C (#37f499) becomes when darkened 4 steps to the right.
 #
 # Markdown heading 1 - color04
-linkarzu_color18=#027C94
+linkarzu_color18="#5b4996"
 # Markdown heading 2 - color02
-linkarzu_color19=#2b4b90
+linkarzu_color19="#21925b"
 # Markdown heading 3 - color03
-linkarzu_color20=#566a93
+linkarzu_color20="#027d95"
 # Markdown heading 4 - color01
-linkarzu_color21=#999999
+linkarzu_color21="#585c89"
 # Markdown heading 5 - color05
-linkarzu_color22=#912e2f
+linkarzu_color22="#0f857c"
 # Markdown heading 6 - color08
-linkarzu_color23=#645206
+linkarzu_color23="#396592"
 # Markdown heading foreground
 # usually set to color10 which is the terminal background
-linkarzu_color26=#00040b
+linkarzu_color26=#0D1116
 
-linkarzu_color04=#04d1f9
-linkarzu_color02=#487ef0
-linkarzu_color03=#90b1f6
-linkarzu_color01=#FFFFFF
-linkarzu_color05=#f34d4f
-linkarzu_color08=#FACF11
-linkarzu_color06=#97956F
+linkarzu_color04="#987afb"
+linkarzu_color02="#37f499"
+linkarzu_color03="#04d1f9"
+linkarzu_color01="#949ae5"
+linkarzu_color05="#19dfcf"
+linkarzu_color08="#5fa9f4"
+linkarzu_color06="#1682ef"
 # Markdown codeblock, 2 to the right of background
 # https://www.colorhexa.com
-linkarzu_color07=#001232
+linkarzu_color07="#1c242f"
 # Comments
-linkarzu_color09=#999999
+linkarzu_color09="#a5afc2"
 # Terminal and neovim background
-linkarzu_color10=#00040b
+linkarzu_color10="#0D1116"
 # Underline spellbad
-linkarzu_color11=#f34d4f
+linkarzu_color11="#f16c75"
 # Underline spellcap
-linkarzu_color12=#FACF11
+linkarzu_color12="#f1fc79"
 # line across cursor, 5 to the right of background
 # https://www.colorhexa.com
-linkarzu_color13=#001946
+linkarzu_color13="#314154"
 # Cursor and tmux windows text
-linkarzu_color14=#FFFFFF
+linkarzu_color14="#ebfafa"
 # Tmux inactive windows, 7 colors to the right of background
 # https://www.colorhexa.com
-linkarzu_color15=#003694
+linkarzu_color15="#013e4a"
 # Selected text
-linkarzu_color16=#00ffff
+linkarzu_color16="#e9b3fd"
 # Lualine across, 1 color to the right of background
 # https://www.colorhexa.com/0d1116
-linkarzu_color17=#000b1f
+linkarzu_color17="#141b22"
 # Cursor color
-linkarzu_color24=#e93f92
+linkarzu_color24="#F712FF"
 # Background of inactive, 3 to the right of background
 # https://www.colorhexa.come tmux pane
-linkarzu_color25=#001946
+linkarzu_color25=#232e3b

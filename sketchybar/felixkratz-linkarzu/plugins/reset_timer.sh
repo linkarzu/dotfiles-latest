a2c13d00ec88cc2e98742330c97116d2c5022131
Linkarzu <linkarzu@gmail.com>
2025-02-17 04:44:14 -0600

    sketchybar reset_timer.sh fixed process to grep

--- sketchybar/felixkratz-linkarzu/plugins/reset_timer.sh
+++ sketchybar/felixkratz-linkarzu/plugins/reset_timer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pid=$(ps -A | grep -v 'grep' | grep -v 'sh' | grep ~/github/dotfiles-latest/sketchybar/felixkratz-linkarzu/plugins/timer.py | cut -d ' ' -f 1)
+pid=$(ps -A | grep -v 'grep' | grep -v 'sh' | grep ~/.config/sketchybar/plugins/timer.py | cut -d ' ' -f 1)
 kill ${pid}
 
 sketchybar --set timer label=""

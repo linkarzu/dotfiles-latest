bb84641584c88a18d243df682a9dd23fca1e40be
Linkarzu <linkarzu@gmail.com>
2025-03-07 05:35:00 -0600

    neobean noice.lua added search middle on bottom_search

--- neovim/neobean/lua/plugins/noice.lua
+++ neovim/neobean/lua/plugins/noice.lua
@@ -12,6 +12,7 @@ return {
       presets = {
         -- This is the search bar or popup that shows up when you press /
         -- Setting this to false makes it a popup and true the search bar at the bottom
+        -- search middle
         bottom_search = false,
       },
       messages = {

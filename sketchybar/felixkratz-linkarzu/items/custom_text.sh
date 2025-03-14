7eb88e1f7193ddb2bad8df0893a7125d5228471c
Linkarzu <linkarzu@gmail.com>
2025-03-14 07:40:54 -0600

    sketchybar custom_text.sh padding_right=3

--- sketchybar/felixkratz-linkarzu/items/custom_text.sh
+++ sketchybar/felixkratz-linkarzu/items/custom_text.sh
@@ -3,13 +3,11 @@
 # Filename: ~/github/dotfiles-latest/sketchybar/felixkratz-linkarzu/items/custom_text.sh
 # ~/github/dotfiles-latest/sketchybar/felixkratz-linkarzu/items/custom_text.sh
 
-youtube_banner="$HOME/github/dotfiles-latest/youtube-banner.txt"
-
 custom_text=(
   updates=on
   update_freq=10
   label.drawing=on
-  padding_right=15
+  padding_right=3
   label.font="$FONT:Regular:16.0"
   # label.padding_left=3
   script="$PLUGIN_DIR/custom_text.sh"

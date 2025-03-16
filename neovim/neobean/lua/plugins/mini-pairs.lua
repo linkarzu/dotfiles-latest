02345960bbbc9a40e2fd25b88477d340b5979d76
Linkarzu <linkarzu@gmail.com>
2025-03-16 11:08:37 -0600

    neobean mini-pairs.lua disabled automatic pair for backtics ` for inline calculator

---
+++ neovim/neobean/lua/plugins/mini-pairs.lua
@@ -0,0 +1,22 @@
+return {
+  "echasnovski/mini.pairs",
+  event = "VeryLazy",
+  opts = {
+    modes = { insert = true, command = true, terminal = false },
+    -- skip autopair when next character is one of these
+    skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+    -- skip autopair when the cursor is inside these treesitter nodes
+    skip_ts = { "string" },
+    -- skip autopair when next character is closing pair
+    -- and there are more closing pairs than opening pairs
+    skip_unbalanced = true,
+    -- better deal with markdown code blocks
+    markdown = true,
+    mappings = {
+      ["`"] = false,
+    },
+  },
+  config = function(_, opts)
+    LazyVim.mini.pairs(opts)
+  end,
+}

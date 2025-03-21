86f50f332acaa45c29d6c414f32fb1bf81cabf9c
Linkarzu <linkarzu@gmail.com>
2025-03-21 15:06:29 -0600

    neobean blink-cmp.lua added back transform_items function, wasn't able to add ; to friendly-snippets

--- neovim/neobean/lua/plugins/blink-cmp.lua
+++ neovim/neobean/lua/plugins/blink-cmp.lua
@@ -103,6 +103,37 @@ return {
             -- NOTE: remember that `trigger_text` is modified at the top of the file
             return before_cursor:match(trigger_text .. "%w*$") ~= nil
           end,
+          -- After accepting the completion, delete the trigger_text characters
+          -- from the final inserted text
+          -- Modified transform_items function based on suggestion by `synic` so
+          -- that the luasnip source is not reloaded after each transformation
+          -- https://github.com/linkarzu/dotfiles-latest/discussions/7#discussion-7849902
+          -- NOTE: I also tried to add the ";" prefix to all of the snippets loaded from
+          -- friendly-snippets in the luasnip.lua file, but I was unable to do
+          -- so, so I still have to use the transform_items here
+          -- This removes the ";" only for the friendly-snippets snippets
+          transform_items = function(_, items)
+            local line = vim.api.nvim_get_current_line()
+            local col = vim.api.nvim_win_get_cursor(0)[2]
+            local before_cursor = line:sub(1, col)
+            local start_pos, end_pos = before_cursor:find(trigger_text .. "[^" .. trigger_text .. "]*$")
+            if start_pos then
+              for _, item in ipairs(items) do
+                if not item.trigger_text_modified then
+                  ---@diagnostic disable-next-line: inject-field
+                  item.trigger_text_modified = true
+                  item.textEdit = {
+                    newText = item.insertText or item.label,
+                    range = {
+                      start = { line = vim.fn.line(".") - 1, character = start_pos - 1 },
+                      ["end"] = { line = vim.fn.line(".") - 1, character = end_pos },
+                    },
+                  }
+                end
+              end
+            end
+            return items
+          end,
         },
         -- Example on how to configure dadbod found in the main repo
         -- https://github.com/kristijanhusak/vim-dadbod-completion

4e5e81532cec8a15a89d1d18eac6cdb0ffae5a8d
Linkarzu <linkarzu@gmail.com>
2025-03-21 13:30:49 -0600

    neobean keymaps.lua "yd" to yank diagnostics to system clipboard

--- neovim/neobean/lua/config/keymaps.lua
+++ neovim/neobean/lua/config/keymaps.lua
@@ -311,6 +311,29 @@ vim.keymap.set("v", "gl", "$h", { desc = "[P]Go to the end of the line" })
 -- -- The `"+` register represents the system clipboard.
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "[P]Yank to system clipboard" })
 
+-- Copy the current line and all diagnostics on that line to system clipboard
+vim.keymap.set("n", "yd", function()
+  local pos = vim.api.nvim_win_get_cursor(0)
+  local line_num = pos[1] - 1 -- 0-indexed
+  local line_text = vim.api.nvim_buf_get_lines(0, line_num, line_num + 1, false)[1]
+  local diagnostics = vim.diagnostic.get(0, { lnum = line_num })
+  if #diagnostics == 0 then
+    vim.notify("No diagnostic found on this line", vim.log.levels.WARN)
+    return
+  end
+  local message_lines = {}
+  for _, d in ipairs(diagnostics) do
+    for msg_line in d.message:gmatch("[^\n]+") do
+      table.insert(message_lines, msg_line)
+    end
+  end
+  local formatted = {}
+  table.insert(formatted, "Line:\n" .. line_text .. "\n")
+  table.insert(formatted, "Diagnostic on that line:\n" .. table.concat(message_lines, "\n"))
+  vim.fn.setreg("+", table.concat(formatted, "\n\n"))
+  vim.notify("Line and diagnostic copied to clipboard", vim.log.levels.INFO)
+end, { desc = "[P]Yank line and diagnostic to system clipboard" })
+
 -- See the comment in the keymap
 local function md_inline_calculator(auto_trigger)
   local line = vim.api.nvim_get_current_line()

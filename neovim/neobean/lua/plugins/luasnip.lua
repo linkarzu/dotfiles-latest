30ec24971f9a3f7c608d760597802b1950f86c2d
Linkarzu <linkarzu@gmail.com>
2025-03-21 15:11:28 -0600

    neobean luasnip.lua added comment explaining that ```bash arent having the ; removed

--- neovim/neobean/lua/plugins/luasnip.lua
+++ neovim/neobean/lua/plugins/luasnip.lua
@@ -17,7 +17,8 @@ return {
     -- Add prefix ";" to each one of my snippets using the extend_decorator
     -- I use this in combination with blink.cmp. This way I don't have to use
     -- the transform_items function in blink.cmp that removes the ";" at the
-    -- beginning of each snippet
+    -- beginning of each snippet. I added this because snippets that start with
+    -- a symbol like ```bash aren't having their ";" removed
     -- https://github.com/L3MON4D3/LuaSnip/discussions/895
     -- NOTE: THis extend_decorator works great, but I also tried to add the ";"
     -- prefix to all of the snippets loaded from friendly-snippets, but I was
@@ -37,9 +38,9 @@ return {
         return auto_semicolon(original)
       end,
     })
+    local s = extend_decorator.apply(ls.s, {})
 
     -- local s = ls.snippet
-    local s = extend_decorator.apply(ls.s, {})
     local t = ls.text_node
     local i = ls.insert_node
     local f = ls.function_node

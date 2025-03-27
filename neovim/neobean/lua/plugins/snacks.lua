d280f00da3f4d32a407015515e5ebba9bc4c3114
Linkarzu <linkarzu@gmail.com>
2025-03-27 04:24:38 -0600

    neobean snacks.lua snacks image added smaller images option for skitty-notes tier list

--- neovim/neobean/lua/plugins/snacks.lua
+++ neovim/neobean/lua/plugins/snacks.lua
@@ -315,8 +315,10 @@ return {
           float = true,
           -- Sets the size of the image
           -- max_width = 60,
-          max_width = vim.g.neovim_mode == "skitty" and 20 or 60,
-          max_height = vim.g.neovim_mode == "skitty" and 10 or 30,
+          -- max_width = vim.g.neovim_mode == "skitty" and 20 or 60,
+          -- max_height = vim.g.neovim_mode == "skitty" and 10 or 30,
+          max_width = vim.g.neovim_mode == "skitty" and 5 or 60,
+          max_height = vim.g.neovim_mode == "skitty" and 2.5 or 30,
           -- max_height = 30,
           -- Apparently, all the images that you preview in neovim are converted
           -- to .png and they're cached, original image remains the same, but

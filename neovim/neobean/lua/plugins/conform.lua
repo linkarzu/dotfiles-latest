211abe24ecbd9b07f8d9face1bb7569173119d9b
Linkarzu <linkarzu@gmail.com>
2025-03-28 15:50:53 -0600

    neobean conform.lua issues with ruff, so using ruff_format

--- neovim/neobean/lua/plugins/conform.lua
+++ neovim/neobean/lua/plugins/conform.lua
@@ -45,6 +45,10 @@ return {
       -- "LSP: html, templ"
       -- "templ ready (templ) /Users/linkarzu/.local/share/neobean/mason/bin/templ"
       templ = { "templ" },
+      -- Not sure why I couldn't make ruff work, so I'll use ruff_format instead
+      -- it didn't work even if I added the pyproject.toml in the project or
+      -- root of my dots, I was getting the error [LSP][ruff] timeout
+      python = { "ruff_format" },
       -- php = { nil },
     },
   },

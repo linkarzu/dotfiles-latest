8cc7c2e078fb2332c65ebb96994b0d175b0001c7
Linkarzu <linkarzu@gmail.com>
2025-03-29 07:25:29 -0600

    sketchybar custom_text.sh set dynamic banner from text in file

--- sketchybar/felixkratz-linkarzu/plugins/custom_text.sh
+++ sketchybar/felixkratz-linkarzu/plugins/custom_text.sh
@@ -9,7 +9,8 @@ source "$CONFIG_DIR/colors.sh"
 youtube_banner="$HOME/github/dotfiles-latest/youtube-banner.txt"
 
 if [ -f "$youtube_banner" ]; then
-  sketchybar -m --set custom_text label="Linkarzu" icon="" icon.color=$BLUE label.color=$BLUE icon.drawing=on
+  banner_text=$(<"$youtube_banner")
+  sketchybar -m --set custom_text label="$banner_text" icon="" icon.color=$BLUE label.color=$BLUE icon.drawing=on
 else
   sketchybar -m --set custom_text label="" icon.drawing=off
 fi

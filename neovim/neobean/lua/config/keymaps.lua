04a934b4000c333ba05e60df0b00f8235231bf29
Linkarzu <linkarzu@gmail.com>
2025-04-01 10:42:48 -0600

    neobean keymaps.lua commented "gx" for markdown as it now works out of the box

--- neovim/neobean/lua/config/keymaps.lua
+++ neovim/neobean/lua/config/keymaps.lua
@@ -3604,51 +3604,53 @@ end, { desc = "Decrease headings in visual selection" })
 --   vim.cmd("nohlsearch")
 -- end, { desc = "[P]Decrease headings with confirmation" })
 
--- To open markdown links, the cursor usually has to be in this position for gx to
--- work [link text](https://test<cursor>site.com)
--- I want to open links if I run gx in the `link text` section too lamw26wmal
---
--- Switched this keymap to an autocmd as links in non markdown files were not
--- being called correctly
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function()
-    vim.keymap.set("n", "gx", function()
-      local line = vim.fn.getline(".")
-      local cursor_col = vim.fn.col(".")
-      local pos = 1
-      while pos <= #line do
-        local open_bracket = line:find("%[", pos)
-        if not open_bracket then
-          break
-        end
-        local close_bracket = line:find("%]", open_bracket + 1)
-        if not close_bracket then
-          break
-        end
-        local open_paren = line:find("%(", close_bracket + 1)
-        if not open_paren then
-          break
-        end
-        local close_paren = line:find("%)", open_paren + 1)
-        if not close_paren then
-          break
-        end
-        if
-          (cursor_col >= open_bracket and cursor_col <= close_bracket)
-          or (cursor_col >= open_paren and cursor_col <= close_paren)
-        then
-          local url = line:sub(open_paren + 1, close_paren - 1)
-          vim.ui.open(url)
-          return
-        end
-        pos = close_paren + 1
-      end
-      -- fallback to default gx behavior
-      vim.cmd("normal! gx")
-    end, { buffer = true, desc = "[P]Better URL opener for markdown" })
-  end,
-})
+-- -- NOTE: Ignore this, it works out of the box, see
+-- -- https://www.reddit.com/r/neovim/comments/1jozord/comment/mkvmp7s/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+-- -- To open markdown links, the cursor usually has to be in this position for gx
+-- -- to work [link text](https://test<cursor>site.com)
+-- -- I want to open links if I run gx in the `link text` section too lamw26wmal
+-- --
+-- -- Switched this keymap to an autocmd as links in non markdown files were not
+-- -- being called correctly
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "markdown",
+--   callback = function()
+--     vim.keymap.set("n", "gx", function()
+--       local line = vim.fn.getline(".")
+--       local cursor_col = vim.fn.col(".")
+--       local pos = 1
+--       while pos <= #line do
+--         local open_bracket = line:find("%[", pos)
+--         if not open_bracket then
+--           break
+--         end
+--         local close_bracket = line:find("%]", open_bracket + 1)
+--         if not close_bracket then
+--           break
+--         end
+--         local open_paren = line:find("%(", close_bracket + 1)
+--         if not open_paren then
+--           break
+--         end
+--         local close_paren = line:find("%)", open_paren + 1)
+--         if not close_paren then
+--           break
+--         end
+--         if
+--           (cursor_col >= open_bracket and cursor_col <= close_bracket)
+--           or (cursor_col >= open_paren and cursor_col <= close_paren)
+--         then
+--           local url = line:sub(open_paren + 1, close_paren - 1)
+--           vim.ui.open(url)
+--           return
+--         end
+--         pos = close_paren + 1
+--       end
+--       -- fallback to default gx behavior
+--       vim.cmd("normal! gx")
+--     end, { buffer = true, desc = "[P]Better URL opener for markdown" })
+--   end,
+-- })
 
 -- ############################################################################
 --                       End of markdown section

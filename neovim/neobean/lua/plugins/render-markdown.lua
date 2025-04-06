1254193a52f8e242784c7c9eeaf634a11c59a941
Linkarzu <linkarzu@gmail.com>
2025-04-06 06:11:29 -0600

    nebean render-markdown added vim.g.md_heading_bg condition

--- neovim/neobean/lua/plugins/render-markdown.lua
+++ neovim/neobean/lua/plugins/render-markdown.lua
@@ -22,52 +22,57 @@ return {
   -- :checkhealth render-markdown
   -- https://github.com/MeanderingProgrammer/render-markdown.nvim/issues/138#issuecomment-2295422741
   init = function()
-    -- Define color variables
-    -- local color1_bg = colors["linkarzu_color18"]
-    -- local color2_bg = colors["linkarzu_color19"]
-    -- local color3_bg = colors["linkarzu_color20"]
-    -- local color4_bg = colors["linkarzu_color21"]
-    -- local color5_bg = colors["linkarzu_color22"]
-    -- local color6_bg = colors["linkarzu_color23"]
-    -- local colorInline_bg = colors["linkarzu_color02"]
-    -- local color_fg = colors["linkarzu_color26"]
-    -- -- local color_sign = "#ebfafa"
-
-    local color1_bg = colors["linkarzu_color04"]
-    local color2_bg = colors["linkarzu_color02"]
-    local color3_bg = colors["linkarzu_color03"]
-    local color4_bg = colors["linkarzu_color01"]
-    local color5_bg = colors["linkarzu_color05"]
-    local color6_bg = colors["linkarzu_color08"]
     local colorInline_bg = colors["linkarzu_color02"]
     local color_fg = colors["linkarzu_color26"]
-    local color_fg1 = colors["linkarzu_color18"]
-    local color_fg2 = colors["linkarzu_color19"]
-    local color_fg3 = colors["linkarzu_color20"]
-    local color_fg4 = colors["linkarzu_color21"]
-    local color_fg5 = colors["linkarzu_color22"]
-    local color_fg6 = colors["linkarzu_color23"]
     -- local color_sign = "#ebfafa"
+    if vim.g.md_heading_bg == "transparent" then
+      -- Define color variables
+      local color1_bg = colors["linkarzu_color04"]
+      local color2_bg = colors["linkarzu_color02"]
+      local color3_bg = colors["linkarzu_color03"]
+      local color4_bg = colors["linkarzu_color01"]
+      local color5_bg = colors["linkarzu_color05"]
+      local color6_bg = colors["linkarzu_color08"]
+      local color_fg1 = colors["linkarzu_color18"]
+      local color_fg2 = colors["linkarzu_color19"]
+      local color_fg3 = colors["linkarzu_color20"]
+      local color_fg4 = colors["linkarzu_color21"]
+      local color_fg5 = colors["linkarzu_color22"]
+      local color_fg6 = colors["linkarzu_color23"]
 
-    -- Heading colors (when not hovered over), extends through the entire line
-    vim.cmd(string.format([[highlight Headline1Bg guibg=%s guifg=%s ]], color_fg1, color1_bg))
-    vim.cmd(string.format([[highlight Headline2Bg guibg=%s guifg=%s ]], color_fg2, color2_bg))
-    vim.cmd(string.format([[highlight Headline3Bg guibg=%s guifg=%s ]], color_fg3, color3_bg))
-    vim.cmd(string.format([[highlight Headline4Bg guibg=%s guifg=%s ]], color_fg4, color4_bg))
-    vim.cmd(string.format([[highlight Headline5Bg guibg=%s guifg=%s ]], color_fg5, color5_bg))
-    vim.cmd(string.format([[highlight Headline6Bg guibg=%s guifg=%s ]], color_fg6, color6_bg))
-    -- Define inline code highlight for markdown
-    vim.cmd(string.format([[highlight RenderMarkdownCodeInline guifg=%s guibg=%s]], colorInline_bg, color_fg))
-    -- vim.cmd(string.format([[highlight RenderMarkdownCodeInline guifg=%s]], colorInline_bg))
+      -- Heading colors (when not hovered over), extends through the entire line
+      vim.cmd(string.format([[highlight Headline1Bg guibg=%s guifg=%s ]], color_fg1, color1_bg))
+      vim.cmd(string.format([[highlight Headline2Bg guibg=%s guifg=%s ]], color_fg2, color2_bg))
+      vim.cmd(string.format([[highlight Headline3Bg guibg=%s guifg=%s ]], color_fg3, color3_bg))
+      vim.cmd(string.format([[highlight Headline4Bg guibg=%s guifg=%s ]], color_fg4, color4_bg))
+      vim.cmd(string.format([[highlight Headline5Bg guibg=%s guifg=%s ]], color_fg5, color5_bg))
+      vim.cmd(string.format([[highlight Headline6Bg guibg=%s guifg=%s ]], color_fg6, color6_bg))
+      -- Define inline code highlight for markdown
+      vim.cmd(string.format([[highlight RenderMarkdownCodeInline guifg=%s guibg=%s]], colorInline_bg, color_fg))
+      -- vim.cmd(string.format([[highlight RenderMarkdownCodeInline guifg=%s]], colorInline_bg))
 
-    -- Highlight for the heading and sign icons (symbol on the left)
-    -- I have the sign disabled for now, so this makes no effect
-    vim.cmd(string.format([[highlight Headline1Fg cterm=bold gui=bold guifg=%s]], color1_bg))
-    vim.cmd(string.format([[highlight Headline2Fg cterm=bold gui=bold guifg=%s]], color2_bg))
-    vim.cmd(string.format([[highlight Headline3Fg cterm=bold gui=bold guifg=%s]], color3_bg))
-    vim.cmd(string.format([[highlight Headline4Fg cterm=bold gui=bold guifg=%s]], color4_bg))
-    vim.cmd(string.format([[highlight Headline5Fg cterm=bold gui=bold guifg=%s]], color5_bg))
-    vim.cmd(string.format([[highlight Headline6Fg cterm=bold gui=bold guifg=%s]], color6_bg))
+      -- Highlight for the heading and sign icons (symbol on the left)
+      -- I have the sign disabled for now, so this makes no effect
+      vim.cmd(string.format([[highlight Headline1Fg cterm=bold gui=bold guifg=%s]], color1_bg))
+      vim.cmd(string.format([[highlight Headline2Fg cterm=bold gui=bold guifg=%s]], color2_bg))
+      vim.cmd(string.format([[highlight Headline3Fg cterm=bold gui=bold guifg=%s]], color3_bg))
+      vim.cmd(string.format([[highlight Headline4Fg cterm=bold gui=bold guifg=%s]], color4_bg))
+      vim.cmd(string.format([[highlight Headline5Fg cterm=bold gui=bold guifg=%s]], color5_bg))
+      vim.cmd(string.format([[highlight Headline6Fg cterm=bold gui=bold guifg=%s]], color6_bg))
+    else
+      local color1_bg = colors["linkarzu_color18"]
+      local color2_bg = colors["linkarzu_color19"]
+      local color3_bg = colors["linkarzu_color20"]
+      local color4_bg = colors["linkarzu_color21"]
+      local color5_bg = colors["linkarzu_color22"]
+      local color6_bg = colors["linkarzu_color23"]
+      vim.cmd(string.format([[highlight Headline1Bg guifg=%s guibg=%s]], color_fg, color1_bg))
+      vim.cmd(string.format([[highlight Headline2Bg guifg=%s guibg=%s]], color_fg, color2_bg))
+      vim.cmd(string.format([[highlight Headline3Bg guifg=%s guibg=%s]], color_fg, color3_bg))
+      vim.cmd(string.format([[highlight Headline4Bg guifg=%s guibg=%s]], color_fg, color4_bg))
+      vim.cmd(string.format([[highlight Headline5Bg guifg=%s guibg=%s]], color_fg, color5_bg))
+      vim.cmd(string.format([[highlight Headline6Bg guifg=%s guibg=%s]], color_fg, color6_bg))
+    end
   end,
   opts = {
     bullet = {

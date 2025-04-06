24260330606d9f7dfe7729824e4b108c9f8aa305
Linkarzu <linkarzu@gmail.com>
2025-04-06 06:09:46 -0600

    colorscheme colorscheme-vars.sh initial called by zshrc and bashrc files

---
+++ colorscheme/colorscheme-vars.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Filename: ~/github/dotfiles-latest/colorscheme/colorscheme-vars.sh
+# ~/github/dotfiles-latest/colorscheme/colorscheme-vars.sh
+
+# colorscheme_profile="minty-lemon.sh"
+colorscheme_profile="linkarzu-new-headings.sh"
+# colorscheme_profile="linkarzu-colors.sh"
+# colorscheme_profile="star-saber.sh"
+# colorscheme_profile="eldritch-colors.sh"
+# colorscheme_profile="darkppuccin.sh"
+# colorscheme_profile="catppuccin-mocha.sh"
+# colorscheme_profile="catppuccin-macchiato.sh"
+
+# # I have 2 Neovim markdown heading style options
+export MD_HEADING_BG=transparent
+# export MD_HEADING_BG=solid

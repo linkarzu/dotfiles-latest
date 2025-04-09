de8f240fc90dd8b938ff870b7bfeff10eaaeb79e
Linkarzu <linkarzu@gmail.com>
2025-04-09 06:00:19 -0600

    zshrc moved colors to colors.sh and added to zshrc-common.sh

--- zshrc/zshrc-common.sh
+++ zshrc/zshrc-common.sh
@@ -1,4 +1,5 @@
 # Run a clear command right after I log in to any host
 clear
 
+source ~/github/dotfiles-latest/zshrc/modules/colors.sh
 source ~/github/dotfiles-latest/zshrc/modules/autocompletion.sh
--- zshrc/zshrc-file.sh
+++ zshrc/zshrc-file.sh
@@ -6,13 +6,6 @@
 # UNIQUE_ID=do_not_delete_this_line
 # #############################################################################
 
-boldGreen="\033[1;32m"
-boldYellow="\033[1;33m"
-boldRed="\033[1;31m"
-boldPurple="\033[1;35m"
-boldBlue="\033[1;34m"
-noColor="\033[0m"
-
 source ~/github/dotfiles-latest/colorscheme/colorscheme-vars.sh
 
 # ~/.config is used by neovim, alacritty and karabiner

9451544c7467906a4582ac87eaf47090cb9d52b2
Linkarzu <linkarzu@gmail.com>
2025-04-09 07:47:19 -0600

    zshrc zshrc-common.sh removed clear command

--- zshrc/zshrc-common.sh
+++ zshrc/zshrc-common.sh
@@ -1,6 +1,3 @@
-# Run a clear command right after I log in to any host
-clear
-
 source ~/github/dotfiles-latest/zshrc/modules/colors.sh
 source ~/github/dotfiles-latest/colorscheme/colorscheme-vars.sh
 source ~/github/dotfiles-latest/zshrc/modules/symlinks.sh

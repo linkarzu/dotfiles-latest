5ea815bf1a1a3a9675e12fb51ef4197ac03e3ce4
Linkarzu <linkarzu@gmail.com>
2025-04-09 06:10:11 -0600

    zshrc moved history to history.sh

--- zshrc/zshrc-file.sh
+++ zshrc/zshrc-file.sh
@@ -130,34 +130,6 @@ create_symlink ~/github/dotfiles-latest/sesh ~/.config/sesh
 # ln -snf ~/github/dotfiles-latest/hammerspoon ~/.hammerspoon >/dev/null 2>&1
 # ln -snf ~/github/dotfiles-latest/karabiner/mxstbr ~/.config/karabiner >/dev/null 2>&1
 
-# Current number of entries Zsh is configured to store in memory (HISTSIZE)
-# How many commands Zsh is configured to save to the history file (SAVEHIST)
-# echo "HISTSIZE: $HISTSIZE"
-# echo "SAVEHIST: $SAVEHIST"
-# Store 10,000 entries in the command history
-HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=20000
-# Check if the history file exists, if not, create it
-if [[ ! -f $HISTFILE ]]; then
-  touch $HISTFILE
-  chmod 600 $HISTFILE
-fi
-# Append commands to the history file as they are entered
-setopt appendhistory
-# Record timestamp of each command (helpful for auditing)
-setopt extendedhistory
-# Share command history data between all sessions
-setopt sharehistory
-# Incrementally append to the history file, rather than waiting until the shell exits
-setopt incappendhistory
-# Ignore duplicate commands in a row
-setopt histignoredups
-# Exclude commands that start with a space
-setopt histignorespace
-# Custom list of commands to ignore (adjust as needed)
-# HISTIGNORE='ls*:bg*:fg*:exit*:ll*'
-
 # # Disabled this as I close the pane with `,` when in normal mode on zsh-vi-mode
 # #
 # # When on my right tmux pane if I type exit, it will not close the pane but

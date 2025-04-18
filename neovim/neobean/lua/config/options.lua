7ce9a231d42dd990541a966703b5180a755823e0
Linkarzu <linkarzu@gmail.com>
2025-04-18 08:31:41 -0600

    neobean options.lua commented get_computer_model as I don't use copilot chat anymore

--- neovim/neobean/lua/config/options.lua
+++ neovim/neobean/lua/config/options.lua
@@ -205,28 +205,28 @@ end
 -- Set it to 2 if using kitty or codeblocks will look weird
 vim.opt.conceallevel = 0
 
--- Function to get the model of my mac, can be used by copilot-chat plugin
-local function get_computer_model()
-  local ok, handle = pcall(io.popen, "sysctl -n hw.model")
-  if not ok or not handle then
-    return nil
-  end
-  local result = handle:read("*a")
-  handle:close()
-  if result then
-    return result:gsub("^%s*(.-)%s*$", "%1") -- Trim whitespace
-  end
-  return nil
-end
--- Store the computer model globally
-_G.COMPUTER_MODEL = get_computer_model()
--- Compute the Copilot model based on the computer model
-_G.COPILOT_MODEL = _G.COMPUTER_MODEL == "MacBookPro18,2" and "gpt-4o" or "claude-3.5-sonnet"
--- Optional: Create a command to show the computer model
-vim.api.nvim_create_user_command("ShowComputerModel", function()
-  local model = _G.COMPUTER_MODEL or "Unknown"
-  print("Computer Model: " .. model)
-end, {})
+-- -- Function to get the model of my mac, can be used by copilot-chat plugin
+-- local function get_computer_model()
+--   local ok, handle = pcall(io.popen, "sysctl -n hw.model")
+--   if not ok or not handle then
+--     return nil
+--   end
+--   local result = handle:read("*a")
+--   handle:close()
+--   if result then
+--     return result:gsub("^%s*(.-)%s*$", "%1") -- Trim whitespace
+--   end
+--   return nil
+-- end
+-- -- Store the computer model globally
+-- _G.COMPUTER_MODEL = get_computer_model()
+-- -- Compute the Copilot model based on the computer model
+-- _G.COPILOT_MODEL = _G.COMPUTER_MODEL == "MacBookPro18,2" and "gpt-4o" or "claude-3.5-sonnet"
+-- -- Optional: Create a command to show the computer model
+-- vim.api.nvim_create_user_command("ShowComputerModel", function()
+--   local model = _G.COMPUTER_MODEL or "Unknown"
+--   print("Computer Model: " .. model)
+-- end, {})
 
 -- Auto update plugins at startup
 -- Tried to add this vimenter autocmd in the autocmds.lua file but it was never

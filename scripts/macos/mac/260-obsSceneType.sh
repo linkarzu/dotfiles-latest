3a97bebd3d9ee0845e618f31210f0e4ee4a82ab8
Linkarzu <linkarzu@gmail.com>
2025-04-18 20:16:45 -0600

    scripts 260-obsSceneType.sh added 3 guest livestream

--- scripts/macos/mac/260-obsSceneType.sh
+++ scripts/macos/mac/260-obsSceneType.sh
@@ -11,6 +11,8 @@ echo "3 - 1 guest"
 echo "4 - 1 guest livestream"
 echo "5 - 2 guest"
 echo "6 - 2 guest livestream"
+echo "7 - 3 guest"
+echo "8 - 3 guest livestream"
 read -rp "Enter number: " choice
 
 target_file="$HOME/github/dotfiles-latest/karabiner/mxstbr/rules.ts"
@@ -26,6 +28,11 @@ case "$choice" in
   guest_scene="guests-all-notes-right"
   guest_1="guest1-1guest"
   ;;
+4)
+  main_scene="main-1-guest-live"
+  guest_scene="guests-all-notes-right-live"
+  guest_1="guest1-1guest-live"
+  ;;
 5)
   main_scene="main-2-guest"
   guest_scene="guests2-all-notes-right"
@@ -38,6 +45,20 @@ case "$choice" in
   guest_1="guest1-2guest-live"
   guest_2="guest2-2guest-live"
   ;;
+7)
+  main_scene="main-3-guest"
+  guest_scene="guests3-all-notes-right"
+  guest_1="guest1-3guest"
+  guest_2="guest2-3guest"
+  guest_3="guest3-3guest"
+  ;;
+8)
+  main_scene="main-3-guest-live"
+  guest_scene="guests3-all-notes-right-live"
+  guest_1="guest1-3guest-live"
+  guest_2="guest2-3guest-live"
+  guest_3="guest3-3guest-live"
+  ;;
 *)
   echo "Invalid choice. Only 1 and 3 are supported right now."
   exit 1
@@ -49,5 +70,6 @@ sed -i '' "/lineid_obs_switchscene_main/{n;s|switch_scene\.py [^\\\`]*|switch_sc
 sed -i '' "/lineid_obs_switchscene_guest/{n;s|switch_scene\.py [^\\\`]*|switch_scene.py $guest_scene|;}" "$target_file"
 sed -i '' "/lineid_obs_switchscene_guest1/{n;s|switch_scene\.py [^\\\`]*|switch_scene.py $guest_1|;}" "$target_file"
 sed -i '' "/lineid_obs_switchscene_guest2/{n;s|switch_scene\.py [^\\\`]*|switch_scene.py $guest_2|;}" "$target_file"
+sed -i '' "/lineid_obs_switchscene_guest3/{n;s|switch_scene\.py [^\\\`]*|switch_scene.py $guest_3|;}" "$target_file"
 
 echo "Replaced scene with '$main_scene' in $target_file"

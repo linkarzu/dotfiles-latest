100b912264a4b9d0e42e0f2d87f7792432d0a742
Linkarzu <linkarzu@gmail.com>
2025-05-01 04:20:56 -0600

    neobean plugins which-key.lua fixed delay paramenter

--- neovim/neobean/lua/plugins/which-key.lua
+++ neovim/neobean/lua/plugins/which-key.lua
@@ -4,10 +4,7 @@
 -- https://github.com/folke/which-key.nvim
 return {
   "folke/which-key.nvim",
-  config = function()
-    require("which-key").setup({
-      -- I want which key to only popup if I don't remember the key
-      delay = 1500,
-    })
-  end,
+  opts = {
+    delay = 1000,
+  },
 }

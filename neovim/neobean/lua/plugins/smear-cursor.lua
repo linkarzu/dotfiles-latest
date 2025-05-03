f866ac91785ca259fe8e9576f9cfbb57ef2d8f80
Linkarzu <linkarzu@gmail.com>
2025-05-02 20:50:34 -0600

    neobean smear-cursor.lua initial commit

---
+++ neovim/neobean/lua/plugins/smear-cursor.lua
@@ -0,0 +1,11 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  cond = vim.g.neovide == nil,
+  opts = {
+    stiffness = 0.8, -- 0.6      [0, 1]
+    trailing_stiffness = 0.4, -- 0.4      [0, 1]
+    stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
+    trailing_stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
+    distance_stop_animating = 0.5, -- 0.1      > 0
+  },
+}

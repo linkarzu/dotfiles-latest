0824c5e63151e7d9b8b9f43f6a09587dad7d9858
Linkarzu <linkarzu@gmail.com>
2025-05-16 14:34:51 -0600

    neobean hardtime.lua changed event from VeryLazy to BufEnter as advised by plugin creator

--- neovim/neobean/lua/plugins/hardtime.lua
+++ neovim/neobean/lua/plugins/hardtime.lua
@@ -1,7 +1,9 @@
 return {
   "m4xshen/hardtime.nvim",
   dependencies = { "MunifTanjim/nui.nvim" },
-  event = "VeryLazy",
+  -- Was told to use this event by m4xshen himself, in discord
+  -- https://discord.com/channels/1323810827220029441/1371572869838012487/1371660878344097832
+  event = "BufEnter",
   keys = {
     { "j", "v:count == 0 ? 'gj' : 'j'", mode = { "n", "x" }, expr = true, silent = true, desc = "Down" },
     { "k", "v:count == 0 ? 'gk' : 'k'", mode = { "n", "x" }, expr = true, silent = true, desc = "Up" },

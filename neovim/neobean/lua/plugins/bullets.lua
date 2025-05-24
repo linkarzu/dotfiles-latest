691d4bea36df58d1e79deab6306f928aaafc572f
Linkarzu <linkarzu@gmail.com>
2025-05-23 20:06:13 -0600

    https://github.com/bullets-vim/bullets.vim/pull/163

--- neovim/neobean/lua/plugins/bullets.lua
+++ neovim/neobean/lua/plugins/bullets.lua
@@ -25,7 +25,8 @@ return {
   config = function()
     -- Disable deleting the last empty bullet when pressing <cr> or 'o'
     -- default = 1
-    vim.g.bullets_delete_last_bullet_if_empty = 1
+    -- 2 works similar ot Obsidian https://github.com/bullets-vim/bullets.vim/pull/163
+    vim.g.bullets_delete_last_bullet_if_empty = 2
 
     -- (Optional) Add other configurations here
     -- For example, enabling/disabling mappings

bee22576a7ca7742ab3670f57cb4dce9175dcd37
Linkarzu <linkarzu@gmail.com>
2025-06-24 13:28:05 -0600

    yabai yabai-custom-pad.sh accept 4 arguments to define all 4 yabai paddings

--- yabai/positions/yabai-custom-pad.sh
+++ yabai/positions/yabai-custom-pad.sh
@@ -3,7 +3,7 @@
 # Filename: ~/github/dotfiles-latest/yabai/positions/yabai-custom-pad.sh
 # ~/github/dotfiles-latest/yabai/positions/yabai-custom-pad.sh
 
-padding="$1"
+top="$1" bottom="$2" left="$3" right="$4"
 target_file="$HOME/github/dotfiles-latest/yabai/yabairc"
 
-sed -E -i '' '/lineid_asus_monitor_bottom_pad/{n;s/([[:space:]]*yabai -m config bottom_padding )[0-9]+/\1'"$padding"'/;}' "$target_file"
+sed -E -i '' "/lineid_asus_monitor_top_pad/{n;s/([[:space:]]*yabai -m config top_padding )[0-9]+/\1${top}/;};/lineid_asus_monitor_bottom_pad/{n;s/([[:space:]]*yabai -m config bottom_padding )[0-9]+/\1${bottom}/;};/lineid_asus_monitor_left_pad/{n;s/([[:space:]]*yabai -m config left_padding )[0-9]+/\1${left}/;};/lineid_asus_monitor_right_pad/{n;s/([[:space:]]*yabai -m config right_padding )[0-9]+/\1${right}/;}" "$target_file"

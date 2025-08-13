a990b1cb5f73a1134261e8d9a153181d4355739c
Linkarzu <linkarzu@gmail.com>
2025-08-13 07:21:31 -0600

    neobean nvim-lint.lua make it compatible for other users

--- neovim/neobean/lua/plugins/nvim-lint.lua
+++ neovim/neobean/lua/plugins/nvim-lint.lua
@@ -16,14 +16,35 @@ https://youtu.be/c0cuvzK1SDo
 
 --]=====]
 
+-- Only load when the config file exists, I added this for users that don't
+-- have this file, so they don't get an error. They'll get a warning letting
+-- them know where this file is being looked for
+local cfg_path = os.getenv("HOME") .. "/github/dotfiles-latest/.markdownlint.yaml"
+
 return {
   "mfussenegger/nvim-lint",
   optional = true,
+  cond = function()
+    if vim.fn.filereadable(cfg_path) == 1 then
+      return true
+    else
+      vim.schedule(function()
+        vim.notify(
+          string.format(
+            "[nvim-lint] Skipping markdownlint-cli2 setup — expected config file not found at:\n%s",
+            cfg_path
+          ),
+          vim.log.levels.WARN
+        )
+      end)
+      return false
+    end
+  end,
   opts = {
     linters = {
       -- https://github.com/LazyVim/LazyVim/discussions/4094#discussioncomment-10178217
       ["markdownlint-cli2"] = {
-        args = { "--config", os.getenv("HOME") .. "/github/dotfiles-latest/.markdownlint.yaml", "--" },
+        args = { "--config", cfg_path, "--" },
       },
     },
   },

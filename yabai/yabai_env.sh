e5f3be6fa9c47b92eccfc20b331eded5c94ff18a
Linkarzu <linkarzu@gmail.com>
2025-08-22 18:45:06 -0600

    yabai yabai_env.sh disabled skitty-notes

--- yabai/yabai_env.sh
+++ yabai/yabai_env.sh
@@ -14,16 +14,17 @@ if [[ $(echo "$display_resolution" | grep -c "Resolution") -ge 2 ]]; then
   apps_stream="(Microsoft Edge|OBS Studio|Jitsi Meet|Discord)"
   # This keeps apps always below, seems to be working fine when I switch to other
   # apps
-  apps_mgoff_below="(Calculator|iStat Menus|Hammerspoon|BetterDisplay|GIMP|Notes|Activity Monitor|App StoreSoftware Update|TestRig|Gemini|Raycast|OBS Studio|Microsoft Edge|Cisco Packet Tracer|Stickies|kitty|ProLevel|Photo Booth|Hand Mirror|SteerMouse|remote-viewer|Jitsi Meet|DaVinci Resolve|Discord|Vivaldi)"
+  apps_mgoff_below="(Calculator|iStat Menus|Hammerspoon|BetterDisplay|GIMP|Notes|Activity Monitor|App StoreSoftware Update|TestRig|Gemini|Raycast|OBS Studio|Microsoft Edge|Cisco Packet Tracer|Stickies|ProLevel|Photo Booth|Hand Mirror|SteerMouse|remote-viewer|Jitsi Meet|DaVinci Resolve|Discord|Vivaldi)"
 else
   apps_stream="()"
   # This keeps apps always below, seems to be working fine when I switch to other
   # apps
-  apps_mgoff_below="(Calculator|iStat Menus|Hammerspoon|BetterDisplay|GIMP|Notes|Activity Monitor|App StoreSoftware Update|TestRig|Gemini|Raycast|OBS Studio|Microsoft Edge|Cisco Packet Tracer|Stickies|kitty|ProLevel|Photo Booth|Hand Mirror|SteerMouse|remote-viewer|Jitsi Meet|DaVinci Resolve)"
+  apps_mgoff_below="(Calculator|iStat Menus|Hammerspoon|BetterDisplay|GIMP|Notes|Activity Monitor|App StoreSoftware Update|TestRig|Gemini|Raycast|OBS Studio|Microsoft Edge|Cisco Packet Tracer|Stickies|ProLevel|Photo Booth|Hand Mirror|SteerMouse|remote-viewer|Jitsi Meet|DaVinci Resolve)"
 fi
 
 # Apps that I want to always show, even when I have a transparent app focused
-apps_transp_ignore="(kitty)"
+# apps_transp_ignore="(kitty)"
+apps_transp_ignore="()"
 apps_scratchpad="(Udemy|WezTerm)"
 # apps_transp_ignore="(kitty|CleanShot X)"
 

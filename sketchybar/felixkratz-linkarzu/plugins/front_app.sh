999413f24011c2e5cae53a20962339a50cad689d
Linkarzu <linkarzu@gmail.com>
2025-08-26 14:04:22 -0600

    kitty new_tab_with_cwd to open new tabs independently of each session https://github.com/kovidgoyal/kitty/issues/8911#issuecomment-3224876629

--- sketchybar/felixkratz-linkarzu/plugins/front_app.sh
+++ sketchybar/felixkratz-linkarzu/plugins/front_app.sh
@@ -3,9 +3,9 @@
 # Filename: ~/github/dotfiles-latest/sketchybar/felixkratz/plugins/front_app.sh
 
 if [ "$SENDER" = "front_app_switched" ]; then
-  if [ "$INFO" = "kitty" ]; then
-    ~/github/dotfiles-latest/sketchybar/felixkratz-linkarzu/plugins/kitty_name.sh
-    exit 0
-  fi
+  # if [ "$INFO" = "kitty" ]; then
+  #   ~/github/dotfiles-latest/sketchybar/felixkratz-linkarzu/plugins/kitty_name.sh
+  #   exit 0
+  # fi
   sketchybar --set $NAME label="$INFO" icon.background.image="app.$INFO"
 fi

e9e2ca4a8a791d599d88ab8a00caefa5580e1497
Linkarzu <linkarzu@gmail.com>
2025-09-08 07:48:05 -0600

    neobean lazy.lua added util.dot which adds bashls shellcheck

--- neovim/neobean/lua/config/lazy.lua
+++ neovim/neobean/lua/config/lazy.lua
@@ -37,6 +37,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    { import = "lazyvim.plugins.extras.util.dot" },
     -- { import = "lazyvim.plugins.extras.lang.php" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins

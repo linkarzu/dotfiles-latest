f3a2f6191c2850be7e7941b4311c87e15b66f249
Linkarzu <linkarzu@gmail.com>
2025-09-17 09:27:59 -0600

    neobean mason-old.lua removed unused

--- neovim/neobean/lua/plugins/mason-old.lua
+++
@@ -1,8 +0,0 @@
--- https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817
-
-return {
-  -- Mason is pinned to v1, as of May 25th 2025
-  -- https://github.com/LazyVim/LazyVim/blob/25abbf546d564dc484cf903804661ba12de45507/lua/lazyvim/plugins/lsp/init.lua#L296
-  -- { "mason-org/mason.nvim", version = "1.11.0" },
-  -- { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
-}

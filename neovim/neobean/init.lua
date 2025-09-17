8284b384c842f7dac1d96fda98e8e3a995ac5a6b
Linkarzu <linkarzu@gmail.com>
2025-09-17 08:46:17 -0600

    kitty-scrollback init.lua added the crollback_mode variable

--- neovim/neobean/init.lua
+++ neovim/neobean/init.lua
@@ -7,6 +7,7 @@
 --
 -- NOTE: To see all the files modified for skitty-notes just search for "neovim_mode"
 vim.g.neovim_mode = vim.env.NEOVIM_MODE or "default"
+vim.g.scrollback_mode = vim.env.SCROLLBACK_MODE or "default"
 -- vim.g.bullets_enable_in_empty_buffers = 0
 
 -- -- I have 2 style options "solid" and "transparent"

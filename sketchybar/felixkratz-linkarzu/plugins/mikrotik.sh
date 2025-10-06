0fb9d3122d0b9ab35a3da84b6a548e307f6ca304
Linkarzu <linkarzu@gmail.com>
2025-10-06 05:57:58 -0600

    sketchybar mikrotik.sh single ssh session to router, show backup ISP status

--- sketchybar/felixkratz-linkarzu/plugins/mikrotik.sh
+++ sketchybar/felixkratz-linkarzu/plugins/mikrotik.sh
@@ -7,33 +7,64 @@
 
 source "$HOME/github/dotfiles-latest/sketchybar/felixkratz-linkarzu/colors.sh"
 
-TRANSFER_SPEEDS=$(ssh mikrotik "/interface/monitor-traffic bridge once")
 # I'm monitoring the bridge interface, as I have 2 ISPs, from the bgridge
 # interface perspective the Tx and Rx are opposites, so that cause a bit of
 # confusion
-RECEIVE_SPEED=$(echo "$TRANSFER_SPEEDS" | awk '/ tx-bits-per-second/ {print $2}')
-TRANSMIT_SPEED=$(echo "$TRANSFER_SPEEDS" | awk '/ rx-bits-per-second/ {print $2}')
-# echo "tx=$RECEIVE_SPEED" >>/tmp/mikrotik.sh.log
-# echo "rx=$TRANSMIT_SPEED" >>/tmp/mikrotik.sh.log
+#
+# tr -d '\r' deletes carriage returns at the end of values
+ROUTER_OUTPUT=$(ssh mikrotik ":put (\"TRANSMITSPEED=\" . [/interface/monitor-traffic interface=bridge once as-value]->\"rx-bits-per-second\"); \
+:put (\"RECEIVESPEED=\" . [/interface/monitor-traffic interface=bridge once as-value]->\"tx-bits-per-second\"); \
+:put (\"MAINGATEWAY=\" . \$maingateway);
+:put (\"BACKGATEWAY=\" . \$backgateway);
+:if ([:len [/ip route find comment=\"mainisp\"]] > 0) do={:put \"MAINISP=UP\"} else={:put \"MAINISP=DOWN\"}; \
+:if ([:len [/ip route find comment=\"backupisp\"]] > 0) do={:put \"BACKUPISP=UP\"} else={:put \"BACKUPISP=DOWN\"}" | tr -d '\r')
+# echo "$ROUTER_OUTPUT" >>/tmp/mikrotik.sh.log
 
-RECEIVE_SPEED_HUMAN=$(echo "$RECEIVE_SPEED" | tr -d '\r' | sed -E 's/[[:space:]]+$//; s/\.\.\./0k/; s/kbps$/k/; s/Mbps$/M/; s/bps$/b/')
-TRANSMIT_SPEED_HUMAN=$(echo "$TRANSMIT_SPEED" | tr -d '\r' | sed -E 's/[[:space:]]+$//; s/\.\.\./0k/; s/kbps$/k/; s/Mbps$/M/; s/bps$/b/')
-# echo "txh=$RECEIVE_SPEED_HUMAN" >>/tmp/mikrotik.sh.log
-# echo "rxh=$TRANSMIT_SPEED_HUMAN" >>/tmp/mikrotik.sh.log
+# Parse values from KEY=VALUE lines
+TRANSMIT_SPEED=$(echo "$ROUTER_OUTPUT" | awk -F= '/^TRANSMITSPEED=/{print $2}')
+RECEIVE_SPEED=$(echo "$ROUTER_OUTPUT" | awk -F= '/^RECEIVESPEED=/{print $2}')
+MAINGATEWAY=$(echo "$ROUTER_OUTPUT" | awk -F= '/^MAINGATEWAY=/{print $2}')
+BACKGATEWAY=$(echo "$ROUTER_OUTPUT" | awk -F= '/^BACKGATEWAY=/{print $2}')
+MAINISP_STATUS=$(echo "$ROUTER_OUTPUT" | awk -F= '/^MAINISP=/{print $2}')
+BACKUPISP_STATUS=$(echo "$ROUTER_OUTPUT" | awk -F= '/^BACKUPISP=/{print $2}')
+# echo "TRANSMIT_SPEED=$TRANSMIT_SPEED" >>/tmp/mikrotik.sh.log
+# echo "RECEIVE_SPEED=$RECEIVE_SPEED" >>/tmp/mikrotik.sh.log
+# echo "MAINISP_NAME=$MAINISP_NAME" >>/tmp/mikrotik.sh.log
+# echo "BACKUPISP_NAME=$BACKUPISP_NAME" >>/tmp/mikrotik.sh.log
+# echo "MAINISP_STATUS=$MAINISP_STATUS" >>/tmp/mikrotik.sh.log
+# echo "BACKUPISP_STATUS=$BACKUPISP_STATUS" >>/tmp/mikrotik.sh.log
 
-CURRENT_ISP=$(ssh mikrotik "/ip route print where dst-address=0.0.0.0/0 and active")
-GATEWAY=$(echo "$CURRENT_ISP" | awk '/0\.0\.0\.0\/0/ {print $(NF-1)}')
-# echo "gateway=$GATEWAY" >>/tmp/mikrotik.sh.log
+RECEIVE_SPEED_HUMAN=$(
+  awk -v b="$RECEIVE_SPEED" 'BEGIN{ if(b>=1e6) printf("%.1fM", b/1e6); else if(b>=1e3) printf("%.1fk", b/1e3); else printf("%db", b) }'
+)
 
-if [ "$GATEWAY" == "1.0.0.1" ]; then
-  CURRENT_GATEWAY="CLARO"
+TRANSMIT_SPEED_HUMAN=$(
+  awk -v b="$TRANSMIT_SPEED" 'BEGIN{ if(b>=1e6) printf("%.1fM", b/1e6); else if(b>=1e3) printf("%.1fk", b/1e3); else printf("%db", b) }'
+)
+# echo "TRANSMIT_SPEED_HUMAN=$TRANSMIT_SPEED_HUMAN" >>/tmp/mikrotik.sh.log
+# echo "RECEIVE_SPEED_HUMAN=$RECEIVE_SPEED_HUMAN" >>/tmp/mikrotik.sh.log
+
+if [ "$MAINISP_STATUS" == "UP" ]; then
   COLOR="$GREEN"
+  if [ "$BACKUPISP_STATUS" = "UP" ]; then
+    BACKUPISP_ARROW=""
+  else
+    BACKUPISP_ARROW=""
+  fi
+  ISPS_STATUS="A:${MAINGATEWAY} B:${BACKGATEWAY}$BACKUPISP_ARROW"
 else
-  CURRENT_GATEWAY="SIST"
   COLOR="$RED"
+  if [ "$BACKUPISP_STATUS" = "UP" ]; then
+    BACKUPISP_ARROW=""
+  else
+    BACKUPISP_ARROW=""
+    TRANSMIT_SPEED_HUMAN=0
+    RECEIVE_SPEED_HUMAN=0
+  fi
+  ISPS_STATUS="B:${BACKGATEWAY}$BACKUPISP_ARROW"
 fi
 
 sketchybar -m --set mikrotik \
-  label="${CURRENT_GATEWAY}  ${RECEIVE_SPEED_HUMAN}  ${TRANSMIT_SPEED_HUMAN}" \
+  label="${ISPS_STATUS}  ${RECEIVE_SPEED_HUMAN}  ${TRANSMIT_SPEED_HUMAN}" \
   icon= icon.color="$COLOR" \
   label.color="$COLOR"

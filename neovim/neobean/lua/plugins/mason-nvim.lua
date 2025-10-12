52da1c3e84efaf9ed065f86491d6847aa3aa7f7d
Linkarzu <linkarzu@gmail.com>
2025-10-12 09:34:02 -0600

    neobean mason-nvim.lua disabled htmx-lsp, CAUSED ME A MAJOR PAIN IN THE DICK and couldn't figure it out

--- neovim/neobean/lua/plugins/mason-nvim.lua
+++ neovim/neobean/lua/plugins/mason-nvim.lua
@@ -4,15 +4,20 @@
 -- https://github.com/jonschlinkert/markdown-toc
 return {
   "mason-org/mason.nvim",
-  opts = {
-    ensure_installed = {
+  opts = function(_, opts)
+    vim.list_extend(opts.ensure_installed, {
       -- I installed "templ" and other LSPS, these requirements can be found in
       -- the TEMPL docs https://templ.guide/commands-and-tools/ide-support/#neovim--050
       -- The templ command must be in your system path for the LSP to be able to start
       -- vim.list_extend(opts.ensure_installed, { "templ", "html-lsp", "htmx-lsp", "tailwindcss-language-server" })
       "templ",
       "html-lsp",
-      "htmx-lsp",
+      -- htmx-lsp is the culprit for a bug that haunted me for fucking days.
+      -- only when in markdown files my blink completions would take like 5
+      -- seconds to load. You have no idea what I had to go through to figure
+      -- this out. It broke after some updates, not sure which one, if the LSP
+      -- itself, blink or what
+      -- "htmx-lsp",
       "tailwindcss-language-server",
       "harper-ls",
       -- Not installing the tree-sitter CLI through mason due do this
@@ -23,6 +28,6 @@ return {
       -- I installed markdown-toc as I use to to automatically create and upate
       -- the TOC at the top of each file
       -- vim.list_extend(opts.ensure_installed, { "markdownlint-cli2", "marksman", "markdown-toc" })
-    },
-  },
+    })
+  end,
 }

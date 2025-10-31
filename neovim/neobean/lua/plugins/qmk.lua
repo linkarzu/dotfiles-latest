3932e9cd8472b26e94fd5b41e8dd2d920b6d046a
Linkarzu <linkarzu@gmail.com>
2025-10-31 15:27:01 -0600

    neobean qmk.lua initial commit

---
+++ neovim/neobean/lua/plugins/qmk.lua
@@ -0,0 +1,18 @@
+return {
+  "codethread/qmk.nvim",
+  ft = "keymap",
+  enabled = true,
+  event = "VeryLazy",
+  opts = {
+    name = "LAYOUT_glove80",
+    variant = "zmk",
+    layout = {
+      "x x x x x _ _ _ _ _ _ _ _ _ x x x x x",
+      "x x x x x x _ _ _ _ _ _ _ x x x x x x",
+      "x x x x x x _ _ _ _ _ _ _ x x x x x x",
+      "x x x x x x _ _ _ _ _ _ _ x x x x x x",
+      "x x x x x x x x x _ x x x x x x x x x",
+      "x x x x x _ x x x _ x x x _ x x x x x",
+    },
+  },
+}

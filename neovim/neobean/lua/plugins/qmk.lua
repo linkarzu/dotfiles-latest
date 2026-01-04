8a38034f55a37e8b65c48649c5fcb3c293dd1253
Linkarzu <linkarzu@gmail.com>
2026-01-04 07:11:17 -0600

    neobean qmk.lua commented default layout, moved to autocmds file

--- neovim/neobean/lua/plugins/qmk.lua
+++ neovim/neobean/lua/plugins/qmk.lua
@@ -1,18 +1,23 @@
+-- https://github.com/codethread/qmk.nvim
+
 return {
   "codethread/qmk.nvim",
   ft = "keymap",
   enabled = true,
   event = "VeryLazy",
-  opts = {
-    name = "LAYOUT_glove80",
-    variant = "zmk",
-    layout = {
-      "x x x x x _ _ _ _ _ _ _ _ _ x x x x x",
-      "x x x x x x _ _ _ _ _ _ _ x x x x x x",
-      "x x x x x x _ _ _ _ _ _ _ x x x x x x",
-      "x x x x x x _ _ _ _ _ _ _ x x x x x x",
-      "x x x x x x x x x _ x x x x x x x x x",
-      "x x x x x _ x x x _ x x x _ x x x x x",
-    },
-  },
+  -- Commentikng the default layout, moving this config to the autocmds file to
+  -- have multiple keyboards configured
+  -- ~/github/dotfiles-latest/neovim/neobean/lua/config/autocmds.lua
+  -- opts = {
+  --   name = "LAYOUT_glove80",
+  --   variant = "zmk",
+  --   layout = {
+  --     "x x x x x _ _ _ _ _ _ _ _ _ x x x x x",
+  --     "x x x x x x _ _ _ _ _ _ _ x x x x x x",
+  --     "x x x x x x _ _ _ _ _ _ _ x x x x x x",
+  --     "x x x x x x _ _ _ _ _ _ _ x x x x x x",
+  --     "x x x x x x x x x _ x x x x x x x x x",
+  --     "x x x x x _ x x x _ x x x _ x x x x x",
+  --   },
+  -- },
 }

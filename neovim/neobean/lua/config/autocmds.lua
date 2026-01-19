51288ae532b0079983613c2ff4d6bd2a4049d546
Linkarzu <linkarzu@gmail.com>
2026-01-19 08:57:58 -0600

    neobean autocmds disabled close stuff with esc and comment on disable spell

--- neovim/neobean/lua/config/autocmds.lua
+++ neovim/neobean/lua/config/autocmds.lua
@@ -32,41 +32,41 @@ end
 --   group = autocomplete_group,
 -- })
 
--- close some filetypes with <esc>
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("close_with_q"),
-  pattern = {
-    "PlenaryTestPopup",
-    "grug-far",
-    "help",
-    "lspinfo",
-    "notify",
-    "qf",
-    "spectre_panel",
-    "startuptime",
-    "tsplayground",
-    "neotest-output",
-    "checkhealth",
-    "neotest-summary",
-    "neotest-output-panel",
-    "dbout",
-    "gitsigns-blame",
-    "Lazy",
-  },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
-    vim.schedule(function()
-      vim.keymap.set("n", "<esc>", function()
-        vim.cmd("close")
-        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
-      end, {
-        buffer = event.buf,
-        silent = true,
-        desc = "Quit buffer",
-      })
-    end)
-  end,
-})
+-- -- close some filetypes with <esc>
+-- vim.api.nvim_create_autocmd("FileType", {
+--   group = augroup("close_with_q"),
+--   pattern = {
+--     "PlenaryTestPopup",
+--     "grug-far",
+--     "help",
+--     "lspinfo",
+--     "notify",
+--     "qf",
+--     "spectre_panel",
+--     "startuptime",
+--     "tsplayground",
+--     "neotest-output",
+--     "checkhealth",
+--     "neotest-summary",
+--     "neotest-output-panel",
+--     "dbout",
+--     "gitsigns-blame",
+--     "Lazy",
+--   },
+--   callback = function(event)
+--     vim.bo[event.buf].buflisted = false
+--     vim.schedule(function()
+--       vim.keymap.set("n", "<esc>", function()
+--         vim.cmd("close")
+--         pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+--       end, {
+--         buffer = event.buf,
+--         silent = true,
+--         desc = "Quit buffer",
+--       })
+--     end)
+--   end,
+-- })
 
 -- -- This is used to switch between light and dark background colors when the
 -- -- focus is lost or gained, for example when I switch from neovim to a tmux
@@ -322,3 +322,12 @@ vim.api.nvim_create_autocmd("BufEnter", {
     })
   end,
 })
+
+-- -- In case you want to disable spell
+-- vim.api.nvim_create_autocmd("FileType", {
+--   group = augroup("wrap_spell"),
+--   pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+--   callback = function()
+--     vim.opt_local.spell = false
+--   end,
+-- })

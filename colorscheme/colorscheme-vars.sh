b7004d42d573f1342055d45a2397d03980f079d6
Linkarzu <linkarzu@gmail.com>
2026-02-12 05:58:45 -0600

    Colorscheme set to radioactive-fiambre again

--- colorscheme/colorscheme-vars.sh
+++ colorscheme/colorscheme-vars.sh
@@ -5,8 +5,8 @@
 
 # colorscheme_profile="minty-lemon.sh"
 # colorscheme_profile="linkarzu-new-headings.sh"
-# colorscheme_profile="radioactive-fiambre.sh"
-colorscheme_profile="batman.sh"
+colorscheme_profile="radioactive-fiambre.sh"
+# colorscheme_profile="batman.sh"
 # colorscheme_profile="linkarzu-colors.sh"
 # colorscheme_profile="star-saber.sh"
 # colorscheme_profile="eldritch-colors.sh"
--- neovim/neobean/lua/config/active-colorscheme.sh
+++ neovim/neobean/lua/config/active-colorscheme.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Filename: ~/github/dotfiles-latest/colorscheme/list/linkarzu-colors.sh
+# ~/github/dotfiles-latest/colorscheme/list/linkarzu-colors.sh
+
 # These files have to be executable
 
 # Lighter markdown headings
@@ -11,55 +14,55 @@
 # C (#37f499) becomes when darkened 4 steps to the right.
 #
 # Markdown heading 1 - color04
-linkarzu_color18=#494949
+linkarzu_color18=#2d244b
 # Markdown heading 2 - color02
-linkarzu_color19=#2a2a2a
+linkarzu_color19=#10492d
 # Markdown heading 3 - color03
-linkarzu_color20=#1a1a1a
+linkarzu_color20=#013e4a
 # Markdown heading 4 - color01
-linkarzu_color21=#7f6900
+linkarzu_color21=#4b314c
 # Markdown heading 5 - color05
-linkarzu_color22=#5b4c00
+linkarzu_color22=#1e2b00
 # Markdown heading 6 - color08
-linkarzu_color23=#999999
+linkarzu_color23=#2d1c08
 # Markdown heading foreground
 # usually set to color10 which is the terminal background
-linkarzu_color26=#000000
+linkarzu_color26=#0D1116
 
-linkarzu_color04=#b2b2b2
-linkarzu_color02=#939393
-linkarzu_color03=#d1d1d1
-linkarzu_color01=#d4b000
-linkarzu_color05=#997f00
-linkarzu_color08=#ffffff
-linkarzu_color06=#027085
+linkarzu_color04=#987afb
+linkarzu_color02=#37f499
+linkarzu_color03=#04d1f9
+linkarzu_color01=#fca6ff
+linkarzu_color05=#9ad900
+linkarzu_color08=#e58f2a
+linkarzu_color06=#05ff23
 
 # Colors to the right from https://www.colorhexa.com
 # Terminal and neovim background
-linkarzu_color10=#000000
+linkarzu_color10=#0D1116
 # Lualine across, 1 color to the right of background
-linkarzu_color17=#0a0a0a
+linkarzu_color17=#141b22
 # Markdown codeblock, 2 to the right of background
-linkarzu_color07=#141414
+linkarzu_color07=#141b22
 # Background of inactive tmux pane, 3 to the right of background
-linkarzu_color25=#1f1f1f
+linkarzu_color25=#232e3b
 # line across cursor, 5 to the right of background
-linkarzu_color13=#333333
+linkarzu_color13=#232e3b
 # Tmux inactive windows, 7 colors to the right of background
-linkarzu_color15=#474747
+linkarzu_color15=#013e4a
 
 # Comments
-linkarzu_color09=#5c5c5c
+linkarzu_color09=#b7bfce
 # Underline spellbad
-linkarzu_color11=#ff7676
+linkarzu_color11=#f16c75
 # Underline spellcap
-linkarzu_color12=#b7a54c
+linkarzu_color12=#f1fc79
 # Cursor and tmux windows text
 linkarzu_color14=#ffffff
 # Selected text
-linkarzu_color16=#0390ac
+linkarzu_color16=#e9b3fd
 # Cursor color
-linkarzu_color24=#04d1f9
+linkarzu_color24=#f94dff
 
 # Wallpaper for this colorscheme
-wallpaper="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Images/wallpapers/batman/batman7.png"
+wallpaper="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Images/wallpapers/official/skyrim-dragon-4.webp"

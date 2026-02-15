984d4f649ceba61adea01ce4b0afb67204bb8624
Linkarzu <linkarzu@gmail.com>
2026-02-15 12:02:57 -0600

    sketchybar items calendar.sh auto-formatted

--- sketchybar/felixkratz-linkarzu/items/calendar.sh
+++ sketchybar/felixkratz-linkarzu/items/calendar.sh
@@ -3,22 +3,22 @@
 #Filename: ~/github/dotfiles-latest/sketchybar/felixkratz/items/calendar.sh
 
 calendar=(
-	icon=cal
-	# Using "JetBrainsMono Nerd Font"
-	icon.font="$FONT:Black:14.0"
-	# Using default "SF Pro"
-	# icon.font="$FONT:Black:12.0"
-	icon.padding_right=0
-	# If showing the time, leave the width as 45
-	# label.width=45
-	label.width=5
-	label.align=right
-	padding_left=15
-	update_freq=30
-	script="$PLUGIN_DIR/calendar.sh"
-	click_script="$PLUGIN_DIR/zen.sh"
+  icon=cal
+  # Using "JetBrainsMono Nerd Font"
+  icon.font="$FONT:Black:14.0"
+  # Using default "SF Pro"
+  # icon.font="$FONT:Black:12.0"
+  icon.padding_right=0
+  # If showing the time, leave the width as 45
+  # label.width=45
+  label.width=5
+  label.align=right
+  padding_left=15
+  update_freq=30
+  script="$PLUGIN_DIR/calendar.sh"
+  click_script="$PLUGIN_DIR/zen.sh"
 )
 
 sketchybar --add item calendar right \
-	--set calendar "${calendar[@]}" \
-	--subscribe calendar system_woke
+  --set calendar "${calendar[@]}" \
+  --subscribe calendar system_woke

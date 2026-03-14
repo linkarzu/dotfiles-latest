4c2acdaeab15c7cf4f0025cd6368460b30fe492a
Linkarzu <linkarzu@gmail.com>
2026-03-14 09:56:31 -0600

    neobean hardtime.lua disabled

--- neovim/neobean/lua/plugins/hardtime.lua
+++ neovim/neobean/lua/plugins/hardtime.lua
@@ -1,6 +1,6 @@
 return {
   "m4xshen/hardtime.nvim",
-  enabled = true,
+  enabled = false,
   dependencies = { "MunifTanjim/nui.nvim" },
   -- Was told to use this event by m4xshen himself, in discord
   -- https://discord.com/channels/1323810827220029441/1371572869838012487/1371660878344097832

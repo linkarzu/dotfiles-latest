735c9cbf2138eec041c1f79d1868791866b08ebc
Linkarzu <linkarzu@gmail.com>
2026-03-23 05:15:23 -0600

    neobean snacks.lua changed ivy and ivy_split heights

--- neovim/neobean/lua/plugins/snacks.lua
+++ neovim/neobean/lua/plugins/snacks.lua
@@ -255,7 +255,7 @@ return {
               backdrop = false,
               row = -1,
               width = 0,
-              height = 0.5,
+              height = 0.7,
               border = "top",
               title = " {title} {live} {flags}",
               title_pos = "left",
@@ -267,8 +267,27 @@ return {
               },
             },
           },
+          -- I wanted to modify the layout height, I wanted it bigger (that's
+          -- what she said)
+          ivy_split = {
+            layout = {
+              box = "vertical",
+              backdrop = false,
+              width = 0,
+              height = 0.6,
+              position = "bottom",
+              border = "top",
+              title = " {title} {live} {flags}",
+              title_pos = "left",
+              { win = "input", height = 1, border = "bottom" },
+              {
+                box = "horizontal",
+                { win = "list", border = "none" },
+                { win = "preview", title = "{preview}", width = 0.6, border = "left" },
+              },
+            },
+          },
           -- I wanted to modify the layout width
-          --
           vertical = {
             layout = {
               backdrop = false,

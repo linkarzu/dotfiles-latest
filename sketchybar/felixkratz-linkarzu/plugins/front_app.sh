09ea4260df1fcb3197a18e81d40f9494939a9b9b
Linkarzu <linkarzu@gmail.com>
2026-07-04 04:43:22 -0600

    sketchybar front_app.sh add counter of instances of a same app when more than 1

--- sketchybar/felixkratz-linkarzu/plugins/front_app.sh
+++ sketchybar/felixkratz-linkarzu/plugins/front_app.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-# Filename: ~/github/dotfiles-latest/sketchybar/felixkratz/plugins/front_app.sh
+# Filename: ~/github/dotfiles-latest/sketchybar/felixkratz-linkarzu/plugins/front_app.sh
 
-if [ "$SENDER" = "front_app_switched" ]; then
+if [ "$SENDER" = "front_app_switched" ] || [ "$SENDER" = "front_app_windows_changed" ]; then
   # if [ "$INFO" = "kitty" ]; then
   #   ~/github/dotfiles-latest/sketchybar/felixkratz-linkarzu/plugins/kitty_name.sh
   #   exit 0
   # fi
-  # sketchybar --set $NAME label="$INFO" icon.background.image="app.$INFO"
-  sketchybar --set $NAME label="" label.drawing=off icon.background.image="app.$INFO"
+
+  APP="${INFO:-$(yabai -m query --windows --window 2>/dev/null | jq -r '.app // empty')}"
+
+  if [ -z "$APP" ]; then
+    exit 0
+  fi
+
+  COUNT=$(yabai -m query --windows 2>/dev/null | jq --arg app "$APP" '[.[] | select(.app == $app)] | length')
+
+  if [ "$COUNT" -gt 1 ]; then
+    sketchybar --set "$NAME" label="$COUNT" label.drawing=on icon.background.image="app.$APP"
+  else
+    sketchybar --set "$NAME" label="" label.drawing=off icon.background.image="app.$APP"
+  fi
 fi

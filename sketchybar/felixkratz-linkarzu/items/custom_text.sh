67e29c6af027645fb2405338cc37d216d56748d3
Linkarzu <linkarzu@gmail.com>
2026-07-09 07:42:35 -0600

    sketchybar custom_text.sh show for how long I've been streaming

--- sketchybar/felixkratz-linkarzu/items/custom_text.sh
+++ sketchybar/felixkratz-linkarzu/items/custom_text.sh
@@ -5,13 +5,22 @@
 
 custom_text=(
   updates=on
-  update_freq=10
+  update_freq=300
+  icon.drawing=on
+  icon.font="$FONT:Regular:8.0"
+  icon.y_offset=5
+  icon.padding_left=0
+  icon.padding_right=0
   label.drawing=on
   padding_right=3
-  label.font="$FONT:Bold:14.0"
-  # label.padding_left=3
+  label.font="$FONT:Bold:12.0"
+  label.y_offset=-6
+  label.padding_left=0
+  label.padding_right=0
   script="$PLUGIN_DIR/custom_text.sh"
 )
 
-sketchybar --add item custom_text right \
-  --set custom_text "${custom_text[@]}"
+sketchybar --add event custom_text_update \
+  --add item custom_text right \
+  --set custom_text "${custom_text[@]}" \
+  --subscribe custom_text custom_text_update
--- sketchybar/felixkratz-linkarzu/plugins/custom_text.sh
+++ sketchybar/felixkratz-linkarzu/plugins/custom_text.sh
@@ -7,9 +7,43 @@ source "$CONFIG_DIR/colors.sh"
 
 # File created by ~/github/scripts-public/macos/mac/305-bannerOn.sh
 youtube_banner="$HOME/github/dotfiles-latest/youtube-banner.txt"
+streaming_time_script="$HOME/github/dotfiles-private/scripts/macos/mac/obs/streaming-time/py/streaming-time.py"
+
+format_streaming_time() {
+  local minutes="$1"
+
+  if ! [[ "$minutes" =~ ^[0-9]+$ ]]; then
+    minutes=0
+  fi
+
+  printf "%d:%02d" "$((minutes / 60))" "$((minutes % 60))"
+}
+
+set_custom_text() {
+  local banner_text="$1"
+  local streaming_time="$2"
+  local color="$3"
+  local scene_label_width=$((${#banner_text} * 7))
+  local time_label_width=$((${#streaming_time} * 5))
+  local icon_padding_left=$((scene_label_width - time_label_width))
+  local icon_padding_right=$((-scene_label_width))
+
+  sketchybar -m --set custom_text \
+    label="$banner_text" \
+    icon="$streaming_time" \
+    icon.color=$BLUE \
+    label.color=$color \
+    icon.drawing=on \
+    label.drawing=on \
+    icon.padding_left="$icon_padding_left" \
+    icon.padding_right="$icon_padding_right" \
+    padding_right=3
+}
 
 if [ -f "$youtube_banner" ]; then
   banner_text=$(<"$youtube_banner")
+  streaming_minutes=$(zsh -lc "python3 '$streaming_time_script'" 2>/dev/null)
+  streaming_time=$(format_streaming_time "$streaming_minutes")
 
   # Choose color based on label value
   if [[ "$banner_text" == "main-screen" ]]; then
@@ -18,7 +52,7 @@ if [ -f "$youtube_banner" ]; then
     color=$RED
   fi
 
-  sketchybar -m --set custom_text label="$banner_text" icon="" icon.color=$color label.color=$color icon.drawing=on
+  set_custom_text "$banner_text" "$streaming_time" "$color"
 else
-  sketchybar -m --set custom_text label="" icon.drawing=off
+  sketchybar -m --set custom_text label="" icon="" icon.drawing=off
 fi

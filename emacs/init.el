a82f2d1d8adb4ec74fd90a12f9605e98366d85b9
Linkarzu <linkarzu@gmail.com>
2026-07-10 05:56:25 -0600

    emacs init.el changes after coaching session 1 with prot

--- emacs/init.el
+++ emacs/init.el
@@ -1,17 +1,45 @@
 ;;; -*- lexical-binding: t -*-
-
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file t)
+      
 (scroll-bar-mode -1)        ; Disable visible scrollbar
 (tool-bar-mode -1)          ; Disable the toolbar
 (tooltip-mode -1)           ; Disable tooltips
 (menu-bar-mode -1)            ; Disable the menu bar
 
-(setq visible-bell t) ;; Set up the visible bell
+(setq visible-bell nil) ;; Set up the visible bell
 
 (set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 150)
 
-; (load-theme 'modus-vivendi)
-(load-theme 'tango-dark)
+(load-theme 'modus-vivendi)
+;(load-theme 'tango-dark)
 
 (setq inhibit-startup-message t)
 
 (setq mouse-drag-copy-region t)
+
+(use-package vertico
+  :ensure t
+  :config
+  (vertico-mode 1))
+
+(use-package marginalia
+  :ensure t
+  :config
+  (marginalia-mode 1))
+
+(use-package savehist
+  :ensure nil
+  :config
+  (savehist-mode 1))
+
+(use-package orderless
+  :ensure t
+  :config
+  (setq completion-styles '(orderless basic))
+  (setq completion-category-defaults nil))
+
+(use-package delsel
+  :ensure nil
+  :config
+  (delete-selection-mode 1))
